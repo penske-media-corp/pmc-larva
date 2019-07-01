379734a115f8cc40740c30639671030466de2ed7
laras126 <laras126@gmail.com>
2019-07-01 14:19:16 -0400

    patterns-package: refactor: rename c-figure to o-figure

--- packages/patterns/src/components/c-figure/c-figure.prototype.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	"c_figure_permalink_url": "https://notlaura.com",
-	"c_figure_alt_attr": "Thumbnail image",
-	"c_figure_placeholder_image_url": "https://picsum.photos/300/300",
-	"c_figure_image_url": "https://picsum.photos/300/300",
-	"c_figure_caption_text": "Here is some caption text",
-	"c_figure_screen_reader_text": "Text if there is no caption"
-}
---
+++ packages/patterns/src/objects/o-figure/o-figure.prototype.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"o_figure_permalink_url": "https://notlaura.com",
+	"o_figure_alt_attr": "Thumbnail image",
+	"o_figure_placeholder_image_url": "https://picsum.photos/300/300",
+	"o_figure_image_url": "https://picsum.photos/300/300",
+	"o_figure_caption_text": "Here is some caption text",
+	"o_figure_screen_reader_text": "Text if there is no caption"
+}
