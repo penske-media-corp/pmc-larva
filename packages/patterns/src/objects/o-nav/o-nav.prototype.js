c47232ca2575425474e9b6003a277375bc73b029
laras126 <laras126@gmail.com>
2019-07-01 14:18:52 -0400

    patterns-package: refactor: update pattern data schema names, add templates

--- packages/patterns/src/objects/o-nav/o-nav.prototype.js
+++ packages/patterns/src/objects/o-nav/o-nav.prototype.js
@@ -9,7 +9,7 @@ const o_nav_list_items = [
 module.exports = {
 	"modifier_class": "",
 	"o_nav_data_attributes": "",
-	"o_nav_classes": "",
+	"o_nav_classes": "pmc-u-background-brand-red",
 	"o_nav_title_text": "",
 	"o_nav_title_classes": "",
 	"o_nav_list_data_attributes": "",
