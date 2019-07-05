1cd93d27a7cac534712878503d2eec09892bc123
laras126 <laras126@gmail.com>
2019-07-05 13:32:15 -0400

    css-algorithms-package: feat: update rules to test props according to string in selector

--- packages/css-algorithms/lib/a-space-children/rules.js
+++ packages/css-algorithms/lib/a-space-children/rules.js
@@ -5,31 +5,41 @@ var chalk = require( 'chalk' );
 var postscss = require( 'postcss-scss' );
 
 var processor = postcss();
+
 const source = fs.readFileSync( path.join( __dirname, './a-space-children.critical.scss' ) ).toString();
 const nodes =  processor.process( source, {
 	parser: postscss
 } ).root.nodes;
 
-const allowedProperties = [
-	'margin-left',
-	'margin-top',
-	'display',
-	'flex-wrap',
-	'--a-space-children-spacer'
-];
+const rules = {
+	algorithm: 'a-space-children',
+	allowedProperties: [
+		'margin-left',
+		'margin-top',
+		'display',
+		'flex-wrap',
+		'--a-space-children-spacer'
+	]
+};
 
 nodes.forEach( ( node ) => {
 	if ( 'rule' !== node.type ) {
 		return;
 	}
 
-	node.nodes.forEach( ( node ) => {
+	if ( -1 === node.selector.indexOf( rules.algorithm ) ) {
+		return;
+	}
+
+	let ruleNodes = node.nodes;
+
+	ruleNodes.forEach( ( node ) => {
 		if ( 'decl' !== node.type ) {
 			return;
 		}
 
-		if ( ! allowedProperties.includes( node.prop ) ) {
-			console.log( chalk.red( 'The use of ' + node.prop + ' is not permitted in this algorithm.' ) );
+		if ( ! rules.allowedProperties.includes( node.prop ) ) {
+			console.error( chalk.red( 'The use of ' + node.prop + ' is not permitted in the ' + rules.algorithm + ' algorithm.' ) );
 		}
 	});
 });
