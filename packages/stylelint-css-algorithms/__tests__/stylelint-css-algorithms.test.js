6dbe270e8ea496f81e2b3f31574403d9f57067bf
laras126 <laras126@gmail.com>
2019-07-22 18:09:53 -0400

    DOPS-1820: test: get basic test structure kinda in place

--- packages/stylelint-css-algorithms/__tests__/stylelint-css-algorithms.test.js
+++ packages/stylelint-css-algorithms/__tests__/stylelint-css-algorithms.test.js
@@ -1,18 +1,52 @@
 'use strict';
 
+const fs = require( 'fs' );
 const stylelint = require( 'stylelint' );
 const stylelintCssAlgorithms = require('..');
-var rule = require( "../lib/rule.js" );
+const rule = require( '../lib/rule.js' );
 
-const arrayCheck = ( processCss, context ) => {
-	console.log( processCss, context );
-	
+const equalityCheck = ( processCss, context ) => {
+	return processCss.then( ( results ) => {
+		results.forEach( result => {
+			expect( result.actual ).toBe( result.expected );
+		});
+	});
 };
 
-const testRule = stylelint.createRuleTester( arrayCheck );
+const testRule = stylelint.createRuleTester( equalityCheck );
 
 describe('@penskemediacorp/stylelint-css-algorithms', () => {
-	it( 'needs tests', () => {
-		testRule( rule.rule, 'test please' );
+	it( 'does a thing', () => {
+		return testRule( rule.rule, {
+			ruleName: rule.name,
+			config: {
+				'name': 'a-space-children',
+				'allowed-properties': [ 'margin-top', 'margin-left', 'display', 'flex-wrap', '--a-space-children-spacer' ]
+			},
+			accept: [
+				{
+					code: '.a-glue { margin-top: 0; }',
+					description: 'has a margin'
+				}
+			],
+			reject: [
+				{
+					code: '.a-glue { background: 0; }',
+					description: 'has an incorrect property'
+				},
+				{
+					code: '.a-glue { color: 0; }',
+					description: 'has an incorrect property'
+				}
+			]
+		});
 	});
+
+	// it( 'does another thing', () => {
+	// 	return testRule( rule.rule, {
+	// 		ruleName: rule.name,
+	// 		config: 'Throws an error if there additonal properties than those specified',
+	// 		code: '.a-glue { background: green; color: red; }'
+	// 	} );
+	// });
 });
