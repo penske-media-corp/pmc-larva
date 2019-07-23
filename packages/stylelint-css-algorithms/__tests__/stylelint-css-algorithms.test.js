3e622bf561568e6a18c2893708069c0b68b88faa
laras126 <laras126@gmail.com>
2019-07-23 17:44:20 -0400

    DOPS-1820: test: refactor test for testRule

--- packages/stylelint-css-algorithms/__tests__/stylelint-css-algorithms.test.js
+++ packages/stylelint-css-algorithms/__tests__/stylelint-css-algorithms.test.js
@@ -1,52 +1,35 @@
 'use strict';
 
-const fs = require( 'fs' );
+const path = require( 'path' );
 const stylelint = require( 'stylelint' );
-const stylelintCssAlgorithms = require('..');
 const rule = require( '../lib/rule.js' );
 
-const equalityCheck = ( processCss, context ) => {
-	return processCss.then( ( results ) => {
-		results.forEach( result => {
-			expect( result.actual ).toBe( result.expected );
-		});
-	});
-};
-
-const testRule = stylelint.createRuleTester( equalityCheck );
-
-describe('@penskemediacorp/stylelint-css-algorithms', () => {
-	it( 'does a thing', () => {
-		return testRule( rule.rule, {
-			ruleName: rule.name,
-			config: {
-				'name': 'a-space-children',
-				'allowed-properties': [ 'margin-top', 'margin-left', 'display', 'flex-wrap', '--a-space-children-spacer' ]
-			},
-			accept: [
-				{
-					code: '.a-glue { margin-top: 0; }',
-					description: 'has a margin'
-				}
-			],
-			reject: [
-				{
-					code: '.a-glue { background: 0; }',
-					description: 'has an incorrect property'
-				},
-				{
-					code: '.a-glue { color: 0; }',
-					description: 'has an incorrect property'
-				}
-			]
-		});
-	});
-
-	// it( 'does another thing', () => {
-	// 	return testRule( rule.rule, {
-	// 		ruleName: rule.name,
-	// 		config: 'Throws an error if there additonal properties than those specified',
-	// 		code: '.a-glue { background: green; color: red; }'
-	// 	} );
-	// });
+testRule( rule.rule, {
+	ruleName: rule.name,
+	skipBasicChecks: true,
+	config: {
+		'name': 'a-space-children',
+		'allowed-properties': [ 'margin-top', 'margin-left', 'display', 'flex-wrap', '--a-space-children-spacer' ]
+	},
+	accept: [
+		{
+			code: '.a-space-children { poop: 0; }',
+			description: 'passes with an allowed property'
+		},
+		{
+			code: '.a-space-children { flex-wrap: 0; }',
+			description: 'passes with an allowed property'
+		}
+	],
+	reject: [
+		{
+			code: '.a-space-children { background: 0; }',
+			message: rule.messages.rejected( 'background', 'a-space-children' ),
+			description: 'should flag a property that is not present in allowed properties'
+		},
+		{
+			code: '.a-space-children { color: 0; }',
+			message: rule.messages.rejected( 'color', 'a-space-children' )
+		}
+	]
 });
