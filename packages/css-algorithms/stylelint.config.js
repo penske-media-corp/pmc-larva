b14f026a29971b8bf3f848b376fd111fd0929db7
laras126 <laras126@gmail.com>
2019-07-24 12:35:00 -0400

    DOPS-1820: refactor: upate css algo stylint to new config

--- packages/css-algorithms/stylelint.config.js
+++ packages/css-algorithms/stylelint.config.js
@@ -5,32 +5,27 @@ const rules = {
 	'extends': '/Users/laraschenck/Sites/pmc/pmc-larva/packages/stylelint-config/index.js',
 	'plugins': path.resolve( __dirname, '../stylelint-css-algorithms/index.js' ),
 	'rules': {
-		'plugin/css-algorithms': [
-			{
-				'name': 'a-space-children',
-				'allowed-properties': [
-					'margin-top',
-					'margin-left',
-					'display',
-					'--a-space-children-spacer',
-					'flex-wrap'
-				]
-			},
-			{
-				'name': 'a-glue',
-				'allowed-properties': [
-					'position',
-					'top',
-					'right',
-					'bottom',
-					'left',
-					'--a-glue-bottom',
-					'--a-glue-top',
-					'--a-glue-right',
-					'--a-glue-left'
-				]
-			}
-		]
+		'plugin/css-algorithms': {
+			'a-space-children': [
+				'margin-top',
+				'margin-left',
+				'display',
+				'--a-space-children-spacer',
+				'flex-wrap'
+			],
+			'pmc-a-glue': [
+				'top',
+				'right',
+				'bottom',
+				'left',
+				'position',
+				'z-index',
+				'--a-glue-bottom',
+				'--a-glue-top',
+				'--a-glue-right',
+				'--a-glue-left'
+			]
+		}
 	}
 };
 
