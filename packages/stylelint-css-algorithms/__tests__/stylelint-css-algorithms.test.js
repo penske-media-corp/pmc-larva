4dfcc94a527e3d69201de9c79d8db493f2612de3
laras126 <laras126@gmail.com>
2019-07-24 09:10:02 -0400

    DOPS-1820: test: clean up and add more stylelint algo tests

--- packages/stylelint-css-algorithms/__tests__/stylelint-css-algorithms.test.js
+++ packages/stylelint-css-algorithms/__tests__/stylelint-css-algorithms.test.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const path = require( 'path' );
-const stylelint = require( 'stylelint' );
 const rule = require( '../lib/rule.js' );
 
 testRule( rule.rule, {
@@ -13,11 +11,7 @@ testRule( rule.rule, {
 	},
 	accept: [
 		{
-			code: '.a-space-children { poop: 0; }',
-			description: 'passes with an allowed property'
-		},
-		{
-			code: '.a-space-children { flex-wrap: 0; }',
+			code: '.a-space-children { margin-top: 0; }',
 			description: 'passes with an allowed property'
 		}
 	],
@@ -25,11 +19,62 @@ testRule( rule.rule, {
 		{
 			code: '.a-space-children { background: 0; }',
 			message: rule.messages.rejected( 'background', 'a-space-children' ),
-			description: 'should flag a property that is not present in allowed properties'
-		},
+			description: 'disallows a property missing from allowed properties'
+		}
+	]
+});
+
+testRule( rule.rule, {
+	ruleName: rule.name,
+	skipBasicChecks: true,
+	config: {
+		'name': 'pmc-a-glue',
+		'allowed-properties': [ 'margin-top' ]
+	},
+	reject: [
 		{
-			code: '.a-space-children { color: 0; }',
-			message: rule.messages.rejected( 'color', 'a-space-children' )
+			code: '.pmc-a-glue { background: red; }',
+			message: rule.messages.rejected( 'background', 'pmc-a-glue' ),
+			description: 'checks with a pmc- namespace'
+		}
+	]
+});
+
+testRule( rule.rule, {
+	ruleName: rule.name,
+	skipBasicChecks: true,
+	config: {
+		'name': 'card',
+		'allowed-properties': [ 'margin-top' ]
+	},
+	reject: [
+		{
+			code: '.card { background: red; }',
+			message: rule.messages.rejected( 'background', 'card' ),
+			description: 'works with a non a-* namespace'
+		}
+	]
+});
+
+testRule( rule.rule, {
+	ruleName: rule.name,
+	skipBasicChecks: true,
+	config: {
+		'name': 'a-icon',
+		'allowed-properties': [ 'height', 'width' ]
+	},
+	accept: [
+		{
+			code: '.a-icon::before { height: 1rem; }',
+			message: rule.messages.rejected( 'background', 'card' ),
+			description: 'works with a pseudo-element'
+		}
+	],
+	reject: [
+		{
+			code: '.a-icon::before { font-family: 1rem; }',
+			message: rule.messages.rejected( 'font-family', 'a-icon' ),
+			description: 'works with a pseudo-element'
 		}
 	]
 });
