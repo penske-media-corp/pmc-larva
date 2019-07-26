8bed254dc36c87a093dca09b2e3b2a0f0c1f3e17
laras126 <laras126@gmail.com>
2019-07-26 14:44:50 -0400

    DOPS-1823: fix: correct error throwing in pattern data path

--- packages/larva/lib/utils/getPatternDataPath.js
+++ packages/larva/lib/utils/getPatternDataPath.js
@@ -5,14 +5,11 @@ module.exports = function getPatternDataPath( patternsIndexPath, patternName ) {
 	const protoPath = buildPatternFilePath( patternsIndexPath, patternName, '.prototype.js' ).toString();
 	const jsonPath = buildPatternFilePath( patternsIndexPath, patternName, '.json' ).toString();
 	
-	try {
-		if ( fs.existsSync( protoPath ) ) {
-			return protoPath;
-		} else if ( fs.existsSync( jsonPath ) ) {
-			return jsonPath;
-		}
-	} catch ( error ) {
-		return error;
+	if ( fs.existsSync( protoPath ) ) {
+		return protoPath;
+	} else if ( fs.existsSync( jsonPath ) ) {
+		return jsonPath;
 	}
-	
+
+	throw new Error( `Couldn\'t find a .prototype.js or .json file for ${patternName}` );
 }
