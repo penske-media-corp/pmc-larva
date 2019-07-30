f8377b9664e82714c474f1778ac63881ecdfd85c
laras126 <laras126@gmail.com>
2019-07-30 13:45:20 -0400

    asset-build-refactor: feat: split sass path at src, then add build dir

--- packages/larva/__tests__/lib/utils/getScssResultFilePath.test.js
+++ packages/larva/__tests__/lib/utils/getScssResultFilePath.test.js
@@ -3,8 +3,7 @@ const path = require( 'path' );
 const fixture = path.join( __dirname, '../../fixtures' );
 const getScssResultFilePath = require( '../../../lib/utils/getScssResultFilePath' );
 
-const stubSassFilePath = '/path/to/css-utilities/src/u-display.common.inline.scss';
-//const stubSassFilePath = '/path/to/css-utilities/src/07-utilities/u-display.common.inline.scss'; - should be this once path is split at src
+const stubSassFilePath = '/path/to/css-utilities/src/07-utilities/u-thing/u-display.common.inline.scss';
 
 const expectedResultFilePath = '/path/to/css-utilities/build/css/utilities.common.inline.css';
 
--- packages/larva/lib/utils/getScssResultFilePath.js
+++ packages/larva/lib/utils/getScssResultFilePath.js
@@ -1,6 +1,11 @@
 const path = require( 'path' );
 
 module.exports = function getScssResultFilePath( sassFilePath, extension ) {
-	// TODO: need to split the path at src in case the sass file is nested, which it probably will be
-	return path.resolve( path.dirname( sassFilePath ), '../build/css/utilities.' + extension + '.css' )
+	let pathArr = path.dirname( sassFilePath ).split( '/' );
+	let index = pathArr.indexOf( 'src' );
+	let removeSrc = pathArr.slice( 0, index );
+	let buildPath = '';
+	let arrayWithPaths = removeSrc.filter( str => buildPath += (str + '/') );
+
+	return path.resolve( buildPath, './build/css/utilities.' + extension + '.css' )
 };
