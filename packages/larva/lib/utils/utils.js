a657da0231b5fdc9da6a9e6ab4eade18ad2d5a29
laras126 <laras126@gmail.com>
2019-07-30 12:52:38 -0400

    asset-build-refactor: feat: scaffold node modules script, add to utils.js

--- packages/larva/lib/utils/utils.js
+++ packages/larva/lib/utils/utils.js
@@ -11,4 +11,6 @@ module.exports = {
 	getScssPathsWithExtension: require( './getScssPathsWithExtension' ),
 	concatenateFileData: require( './concatenateFileData' ),
 	renderSass: require( './renderSass' ),
+	getSassResultFilePath: require( './getSassResultFilePath' ),
+	getProjectNodeModulesPath: require( './getProjectNodeModulesPath' ),
 }
