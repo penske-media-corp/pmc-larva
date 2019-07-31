2ad27b4db34fb4c26ae95f26d17bf0f3bf2bf2a8
laras126 <laras126@gmail.com>
2019-07-31 17:15:56 -0400

    DOPS-1831: feat: add c-icon to footer network

--- packages/patterns/modules/footer-pmc-network/footer-pmc-network.prototype.js
+++ packages/patterns/modules/footer-pmc-network/footer-pmc-network.prototype.js
@@ -1,11 +1,20 @@
 const clone = require( '@penskemediacorp/larva' ).clone;
 const path = require( 'path' );
-const o_nav = clone( path.resolve( __dirname, '../../objects/o-nav/o-nav.prototype' ) );
 const c_link_path = path.resolve( __dirname, '../../components/c-link/c-link.prototype' );
+const o_nav = clone( path.resolve( __dirname, '../../objects/o-nav/o-nav.prototype' ) );
+const c_icon = clone( path.resolve( __dirname, '../../components/c-icon/c-icon.full' ) );
+
 const data = require( '../../_data/brands.json' );
 
 o_nav.o_nav_title_text = 'Our Sites';
 o_nav.o_nav_list_items = [];
+
+c_icon.c_icon_link_classes = 'pmc-u-max-width-150';
+c_icon.c_icon_name = 'pmc-logo-black';
+c_icon.c_icon_url = 'https://pmc.com';
+c_icon.c_icon_rel_name = 'noopener noreferrer';
+
+// pmc-u-margin-r-2 pmc-u-margin-r-00@mobile-max pmc-u-margin-b-1@mobile-max u-flex-order-1@mobile-max
 
 data.brands.forEach( ( brand ) => {
 	let c_link = clone( c_link_path );
@@ -17,14 +26,10 @@ data.brands.forEach( ( brand ) => {
 	o_nav.o_nav_list_items.push( c_link );
 });
 
+// o_nav.o_nav_list_classes += ' ';
+
 module.exports = {
-	"c_icon": {
-		"modifier_class": "c-icon--full",
-		"c_icon_link_classes": "u-width-122 u-height-20 pmc-u-margin-r-2 pmc-u-margin-r-00@mobile-max pmc-u-margin-b-1@mobile-max u-flex-order-1@mobile-max",
-		"c_icon_name": "pmc-logo-black",
-		"c_icon_url": "#",
-		"c_icon_rel_name": "noopener noreferrer"
-	},
+	"c_icon": c_icon,
 	"c_tagline": {
 		"c_tagline_classes": "pmc-u-margin-a-00",
 		"c_tagline_text": "Copyright © 2018 Penske Business Media, LLC. All rights reserved."
