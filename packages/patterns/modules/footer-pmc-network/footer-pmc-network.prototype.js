6054b8795187fa4b5605951afdcdaed8b96a9e60
laras126 <laras126@gmail.com>
2019-07-31 11:43:31 -0400

    DOPS-1831: feat: loop through brand data

--- packages/patterns/modules/footer-pmc-network/footer-pmc-network.prototype.js
+++ packages/patterns/modules/footer-pmc-network/footer-pmc-network.prototype.js
@@ -1,17 +1,55 @@
+const clone = require( '@penskemediacorp/larva' ).clone;
+const path = require( 'path' );
+const o_nav = clone( path.resolve( __dirname, '../../objects/o-nav/o-nav.prototype' ) );
+const c_link_path = path.resolve( __dirname, '../../components/c-link/c-link.prototype' );
+
+// TODO: move this to a pmc plugin when it exists.
 const brands = [
 	{
-		"Variety": "https://variety.com/"
+		"name": "Variety",
+		"url": "https://variety.com/"
 	},
 	{
-		"Deadline": "https://deadline.com/"
-	},
-	{
-		"IndieWire": "https://indiewire.com/"
-	},
-	{
-		"RobbReport": "https://robbreport.com/"
+		"name": "Rolling Stone",
+		"url": "https://rollingstone.com/"
 	}
 ];
+
+// const c_tagline = 
+/**
+ * {
+		"o_nav_classes": "pmc-u-text-transform-uppercase u-width-300 u-max-width-100p u-flex-order-2@mobile-max u-display-none@mobile pmc-u-padding-a-1 u-border-a-1@mobile-max u-border-color-grey-medium-light pmc-u-margin-b-1@mobile-max",
+		"o_nav_title_classes": "pmc-u-font-size-12@mobile-max pmc-u-margin-b-050 is-active is-heading a-icon-after-down a-icon-after-down-r-0 a-icon-after-down-hide@mobile a-icon-bottom-arrow pmc-u-margin-a-00@mobile-max pmc-u-justify-content-center u-justify-content-left@mobile-max",
+		"o_nav_list_classes": "pmc-u-margin-a-00@mobile-max pmc-u-color-grey-medium-light u-color-black@mobile-max",
+		"o_nav_list_item_classes": "pmc-u-color-grey-medium pmc-u-font-size-12@mobile-max pmc-u-margin-t-1@mobile-max pmc-u-line-height-large",
+		"o_nav_title_text": "Our Sites",
+		"o_nav_list_items": [
+			{
+				"c_link_url": "https://variety.com/",
+				"c_link_text": "Variety"
+			},
+			{
+				"c_link_url": "https://deadline.com",
+				"c_link_text": "Deadline"
+			},
+		]
+	}
+ */
+
+o_nav.o_nav_list_classes = '';
+o_nav.o_nav_list_item_classes = '';
+o_nav.o_nav_title_text = 'Our Sites';
+o_nav.o_nav_list_items = [];
+
+brands.forEach( ( brand ) => {
+	let c_link = clone( c_link_path );
+
+	c_link.c_link_text = brand.name;
+	c_link.c_link_url = brand.url;
+	c_link.c_link_classes += ' pmc-u-text-transform-uppercase';
+
+	o_nav.o_nav_list_items.push( c_link );
+});
 
 module.exports = {
 	"c_icon": {
@@ -26,11 +64,12 @@ module.exports = {
 		"c_tagline_text": "Copyright © 2018 Penske Business Media, LLC. All rights reserved."
 	},
 	"c_link": {
-		"c_link_text": "WordPress.com VIP",
+		"c_link_classes": "pmc-a-unstyle-link",
+		"c_link_text": "Powered by WordPress.com VIP",
 		"c_link_url": "#"
 	},
 	"o_dropdown": {
-		"o_dropdown_classes": "pmc-u-font-size-12 pmc-u-font-weight-bold u-margin-l-auto u-width-300 u-max-width-100p u-flex-order-2@mobile-max u-display-none@mobile-max a-icon-after-down a-icon-after-down--has-hover u-display-block",
+		"o_dropdown_classes": "pmc-u-font-size-12 pmc-u-font-weight-bold pmc-u-margin-l-auto u-width-300 u-max-width-100p u-flex-order-2@mobile-max u-display-none@mobile-max a-icon-after-down a-icon-after-down--has-hover pmc-u-display-block",
 		"o_dropdown_heading_classes": "u-flex u-align-items-center pmc-u-padding-l-1 pmc-u-text-transform-uppercase",
 		"o_dropdown_heading_text": "Our Sites",
 		"o_dropdown_list_classes": "pmc-u-background-white pmc-u-margin-a-00",
@@ -40,149 +79,7 @@ module.exports = {
 				"c_link_url": "https://variety.com/",
 				"c_link_text": "Variety"
 			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://deadline.com",
-				"c_link_text": "Deadline"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://www.rollingstone.com/",
-				"c_link_text": "Rolling Stone"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://wwd.com/",
-				"c_link_text": "WWD"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://hollywoodlife.com/",
-				"c_link_text": "HollywoodLife"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://www.goldderby.com/",
-				"c_link_text": "Gold Derby"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://spy.com/",
-				"c_link_text": "spy"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://robbreport.com/",
-				"c_link_text": "Robb Report"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://footwearnews.com/",
-				"c_link_text": "Footwear News"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://bgr.com/",
-				"c_link_text": "BGR"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://www.indiewire.com/",
-				"c_link_text": "IndieWire"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://sourcingjournal.com/",
-				"c_link_text": "Sourcing Journal"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://tvline.com/",
-				"c_link_text": "TVLine"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://fairchildlive.com/",
-				"c_link_text": "Fairchild Media"
-			},
-			{
-				"c_link_classes": "pmc-u-padding-l-1 pmc-u-line-height-large pmc-u-text-transform-uppercase u-display-block u-color-white:hover pmc-u-background-brand-red:hover",
-				"c_link_url": "https://www.sheknows.com/",
-				"c_link_text": "She Knows"
-			}
 		]
 	},
-	"o_nav": {
-		"o_nav_classes": "pmc-u-text-transform-uppercase u-width-300 u-max-width-100p u-flex-order-2@mobile-max u-display-none@mobile pmc-u-padding-a-1 u-border-a-1@mobile-max u-border-color-grey-medium-light pmc-u-margin-b-1@mobile-max",
-		"o_nav_title_classes": "pmc-u-font-size-12@mobile-max pmc-u-margin-b-050 is-active is-heading a-icon-after-down a-icon-after-down-r-0 a-icon-after-down-hide@mobile a-icon-bottom-arrow pmc-u-margin-a-00@mobile-max u-justify-content-center u-justify-content-left@mobile-max",
-		"o_nav_list_classes": "pmc-u-margin-a-00@mobile-max pmc-u-color-grey-medium-light u-color-black@mobile-max",
-		"o_nav_list_item_classes": "pmc-u-color-grey-medium pmc-u-font-size-12@mobile-max  pmc-u-margin-t-1@mobile-max pmc-u-line-height-large",
-		"o_nav_title_text": "Our Sites",
-		"o_nav_data_attributes": "true",
-		"o_nav_title_data_attributes": "true",
-		"o_nav_list_data_attributes": "true",
-		"o_nav_list_item_data_attributes": "true",
-		"o_nav_list_items": [
-			{
-				"c_link_url": "https://variety.com/",
-				"c_link_text": "Variety"
-			},
-			{
-				"c_link_url": "https://deadline.com",
-				"c_link_text": "Deadline"
-			},
-			{
-				"c_link_url": "https://www.rollingstone.com/",
-				"c_link_text": "Rolling Stone"
-			},
-			{
-				"c_link_url": "https://wwd.com/",
-				"c_link_text": "WWD"
-			},
-			{
-				"c_nav_link_url": "https://hollywoodlife.com/",
-				"c_nav_link_text": "HollywoodLife"
-			},
-			{
-				"c_nav_link_url": "https://www.goldderby.com/",
-				"c_nav_link_text": "Gold Derby"
-			},
-			{
-				"c_nav_link_url": "https://spy.com/",
-				"c_nav_link_text": "spy"
-			},
-			{
-				"c_nav_link_url": "https://robbreport.com/",
-				"c_nav_link_text": "Robb Report"
-			},
-			{
-				"c_nav_link_url": "https://footwearnews.com/",
-				"c_nav_link_text": "Footwear News"
-			},
-			{
-				"c_nav_link_url": "https://bgr.com/",
-				"c_nav_link_text": "BGR"
-			},
-			{
-				"c_nav_link_url": "https://www.indiewire.com/",
-				"c_nav_link_text": "IndieWire"
-			},
-			{
-				"c_nav_link_url": "https://sourcingjournal.com/",
-				"c_nav_link_text": "Sourcing Journal"
-			},
-			{
-				"c_nav_link_url": "https://tvline.com/",
-				"c_nav_link_text": "TVLine"
-			},
-			{
-				"c_nav_link_url": "https://fairchildlive.com/",
-				"c_nav_link_text": "Fairchild Media"
-			},
-			{
-				"c_nav_link_url": "https://www.sheknows.com/",
-				"c_nav_link_text": "She Knows"
-			}
-		]
-	}
+	"o_nav": o_nav
 }
