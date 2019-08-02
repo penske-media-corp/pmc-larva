66f67afd2f240f9c7b9948dc7dcc1ed2aab0ff8e
laras126 <laras126@gmail.com>
2019-08-02 18:08:30 -0400

    DOPS-1823: chore: build assets

--- packages/js/build/common.js
+++ packages/js/build/common.js
@@ -94,31 +94,31 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _interface_Dropdown_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./interface/Dropdown/index */ \"./src/interface/Dropdown/index.js\");\n\n\nconst onSafeResize = function() {\n\tconst width = window.innerWidth;\n\n\tObject(_interface_Dropdown_index__WEBPACK_IMPORTED_MODULE_0__[\"default\"])( width );\n};\n\nwindow.addEventListener( 'load', () => {\n\tonSafeResize();\n});\n\n//# sourceURL=webpack:///./src/common.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _interface_MobileHeightToggle_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./interface/MobileHeightToggle/index */ \"./src/interface/MobileHeightToggle/index.js\");\n\n\nconst onSafeResize = function() {\n\tObject(_interface_MobileHeightToggle_index__WEBPACK_IMPORTED_MODULE_0__[\"default\"])( window.innerWidth );\n};\n\nwindow.addEventListener( 'load', () => {\n\tonSafeResize();\n});\n\nwindow.addEventListener( 'resize', () => {\n\tonSafeResize();\n});\n\n//# sourceURL=webpack:///./src/common.js?");
 
 /***/ }),
 
-/***/ "./src/interface/Dropdown/Dropdown.js":
-/*!********************************************!*\
-  !*** ./src/interface/Dropdown/Dropdown.js ***!
-  \********************************************/
+/***/ "./src/interface/MobileHeightToggle/MobileHeightToggle.js":
+/*!****************************************************************!*\
+  !*** ./src/interface/MobileHeightToggle/MobileHeightToggle.js ***!
+  \****************************************************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Dropdown; });\n/**\n * Add drop-down functionality for navigation component.\n */\nclass Dropdown {\n\tconstructor( el ) {\n\t\tthis.el = el;\n\t\tthis.el.pmcDropdown = this;\n\t\tthis.toggle = this.toggle.bind( this );\n\t\tthis.el.addEventListener( 'click', this.toggle );\n\t}\n\n\tdestroy() {\n\t\tdelete this.el.pmcDropdown;\n\t}\n\n\tget activeItem() {\n\t\tconsole.log( this.el );\n\t\t\n\t\treturn this.el.querySelector( '.is-active' );\n\t}\n\n\t// get items() {\n\t// \treturn [ ... this.el.querySelectorAll( '.pmc-js-Dropdown-list-item' ) ];\n\t// }\n\n\tget list() {\n\t\treturn this.el.querySelector( '.pmc-js-Dropdown-list' );\n\t}\n\n\ttoggle( e ) {\n\t\tconst parent     = this.el;\n\t\t\n\t\tif ( e.target.classList.contains( 'pmc-js-Dropdown-trigger' ) ) {\n\t\t\tif ( ! parent.classList.contains( 'is-expanded' ) ) {\n\t\t\t\te.preventDefault();\n\t\t\t\tparent.classList.add( 'is-expanded' );\n\t\t\t\treturn;\n\t\t\t} else {\n\t\t\t\tparent.classList.remove( 'is-expanded' );\n\t\t\t}\n\t\t}\n\t\tconsole.log( e.target );\n\t\t\n\t\t\t\n\t\t// Prevent default if active item was clicked again.\n\t\t// if ( this.isActive( parentItem ) ) {\n\t\t// \te.preventDefault();\n\t\t// } else {\n\t\t// \tthis.setActive( parentItem );\n\t\t// }\n\n\t\t// if ( this.isHeadingItem( parentItem ) ) {\n\t\t// \tparent.dataset.expanded = 'false';\n\t\t// }\n\t}\n\n\tisActive( item ) {\n\t\treturn item.classList.contains( 'is-active' );\n\t}\n\n\tsetActive( item ) {\n\t\tthis.activeItem.classList.remove( 'is-active' );\n\t\titem.classList.add( 'is-active' );\n\t}\n\n\tisItem( node ) {\n\t\treturn node.dataset.dropdownListItem ? true : false;\n\t}\n\n\tisHeadingItem( node ) {\n\t\treturn node.classList.contains( 'is-heading' );\n\t}\n\n\tfindParentItem( child ) {\n\t\tlet currentNode = child;\n\n\t\twhile ( ! this.isItem( currentNode ) ) {\n\t\t\tif ( currentNode === this.el ) {\n\t\t\t\treturn null;\n\t\t\t}\n\n\t\t\tcurrentNode = currentNode.parentNode;\n\t\t}\n\n\t\treturn currentNode;\n\t}\n}\n\n\n//# sourceURL=webpack:///./src/interface/Dropdown/Dropdown.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return MobileHeightToggle; });\n/**\n * Add drop-down functionality for navigation component.\n */\nclass MobileHeightToggle {\n\tconstructor( el ) {\n\t\tthis.el = el;\n\t\tthis.el.pmcMobileHeightToggle = this;\n\t\tthis.isExpanded = false;\n\t\tthis.toggle = this.toggle.bind( this );\n\t\tthis.el.addEventListener( 'click', this.toggle );\n\t}\n\t\n\tdestroy() {\n\t\tthis.el.classList.remove( 'is-expanded' );\n\t\tthis.isExpanded = false;\n\t\tdelete this.el.pmcMobileHeightToggle;\n\t}\n\n\ttoggle( e ) {\n\t\tif ( undefined !== this.el.pmcMobileHeightToggle ) {\n\n\t\t\t// Bubble event\n\t\t\tif ( e.target.classList.contains( 'pmc-js-MobileHeightToggle-trigger' ) ) {\n\t\t\t\tif ( ! this.isExpanded ) {\n\t\t\t\t\te.preventDefault();\n\t\t\t\t\tthis.el.classList.add( 'is-expanded' );\n\t\t\t\t\tthis.isExpanded = true;\n\t\t\t\t} else {\n\t\t\t\t\tthis.el.classList.remove( 'is-expanded' );\n\t\t\t\t\tthis.isExpanded = false;\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n\n}\n\n\n//# sourceURL=webpack:///./src/interface/MobileHeightToggle/MobileHeightToggle.js?");
 
 /***/ }),
 
-/***/ "./src/interface/Dropdown/index.js":
-/*!*****************************************!*\
-  !*** ./src/interface/Dropdown/index.js ***!
-  \*****************************************/
+/***/ "./src/interface/MobileHeightToggle/index.js":
+/*!***************************************************!*\
+  !*** ./src/interface/MobileHeightToggle/index.js ***!
+  \***************************************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Dropdown_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Dropdown.js */ \"./src/interface/Dropdown/Dropdown.js\");\n\n\n// Initialize all Dropdowns.\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (function() {\n\tconst dropdowns = [ ... document.querySelectorAll( '.pmc-js-Dropdown' ) ];\n\tconst width     = window.innerWidth;\n\t\n\tdropdowns.forEach( el => {\n\t\tif ( 768 > width && undefined === el.pmcDropdown ) {\n\t\t\tnew _Dropdown_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]( el );\n\t\t\tconsole.log( el );\n\t\t\t\n\t\t} else if ( 768 <= width && undefined !== el.pmcDropdown ) {\n\t\t\tel.pmcDropdown.destroy();\n\t\t}\n\t});\n});;\n\n\n//# sourceURL=webpack:///./src/interface/Dropdown/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _MobileHeightToggle__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./MobileHeightToggle */ \"./src/interface/MobileHeightToggle/MobileHeightToggle.js\");\n\n\n// Initialize all Dropdowns.\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (function( width ) {\n\tconst dropdowns = [ ... document.querySelectorAll( '.pmc-js-MobileHeightToggle' ) ];\n\n\tdropdowns.forEach( el => {\n\n\t\tif ( 768 > width && undefined === el.pmcMobileHeightToggle ) {\n\t\t\tnew _MobileHeightToggle__WEBPACK_IMPORTED_MODULE_0__[\"default\"]( el );\t\n\t\t}\n\t\t\n\t\tif ( 768 <= width && undefined !== el.pmcMobileHeightToggle ) {\n\t\t\tel.pmcMobileHeightToggle.destroy();\n\t\t}\n\t});\n});;\n\n\n//# sourceURL=webpack:///./src/interface/MobileHeightToggle/index.js?");
 
 /***/ })
 
