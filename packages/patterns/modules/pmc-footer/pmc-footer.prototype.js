7e2886592d24caee0a24745cca85a331bc72ed52
laras126 <laras126@gmail.com>
2019-08-05 14:04:31 -0400

    DOPS-1824: feat: add classes to prototype

--- packages/patterns/modules/pmc-footer/pmc-footer.prototype.js
+++ packages/patterns/modules/pmc-footer/pmc-footer.prototype.js
@@ -19,14 +19,15 @@ data.brands.forEach( ( brand ) => {
 
 	c_link.c_link_text = brand.name;
 	c_link.c_link_url = brand.url;
+	c_link.c_link_classes += ' pmc-u-display-block pmc-u-background-brand-red:hover pmc-u-color-white:hover pmc-u-font-weight-bold pmc-u-padding-lr-1 pmc-u-padding-tb-050';
 
 	o_nav.o_nav_list_items.push( c_link );
 });
 
 o_nav.o_nav_classes = 'pmc-u-text-transform-uppercase pmc-u-font-size-12  pmc-u-border-a-1 pmc-u-border-color-grey-medium-light pmc-u-flex@tablet pmc-js-MobileHeightToggle pmc-a-hover-dropup@tablet';
-o_nav.o_nav_title_classes = 'pmc-u-padding-lr-1 pmc-u-margin-tb-00 pmc-u-padding-t-1 pmc-u-padding-b-050 pmc-u-width-100p a-icon-after a-icon-arrow-down a-icon-after:margin-l-auto pmc-u-cursor-pointer pmc-js-MobileHeightToggle-trigger pmc-a-hover-dropup-trigger@tablet';
-o_nav.o_nav_list_classes = 'pmc-u-padding-lr-1 pmc-u-background-white pmc-u-padding-b-050 pmc-js-MobileHeightToggle-target pmc-a-hover-dropup-target@tablet';
-o_nav.o_nav_list_item_classes = 'pmc-u-padding-tb-050 pmc-u-color-grey-medium-dark';
+o_nav.o_nav_title_classes = 'pmc-u-padding-lr-1 pmc-u-margin-tb-00 pmc-u-padding-t-1 pmc-u-padding-t-050@tablet pmc-u-padding-b-050 pmc-u-width-100p a-icon-after a-icon-arrow-down a-icon-after:margin-l-auto pmc-u-cursor-pointer pmc-js-MobileHeightToggle-trigger pmc-a-hover-dropup-trigger@tablet';
+o_nav.o_nav_list_classes += ' pmc-u-background-white pmc-u-padding-b-050 pmc-js-MobileHeightToggle-target pmc-a-hover-dropup-target@tablet pmc-u-box-shadow-medium@tablet';
+o_nav.o_nav_list_item_classes = 'pmc-u-color-grey-medium-dark';
 
 module.exports = {
 	"c_icon": c_icon,
