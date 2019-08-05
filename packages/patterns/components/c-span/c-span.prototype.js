fb9ce2e226344d9d88274c51224af7c9debc71ae
laras126 <laras126@gmail.com>
2019-08-05 13:43:20 -0400

    DOPS-1824: chore: remove unused scss files from patterns

--- packages/patterns/components/c-span/c-span.prototype.js
+++ packages/patterns/components/c-span/c-span.prototype.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"modifier_class": "",
 	"c_span_data_attr": "",
-	"c_span_classes": "",
+	"c_span_classes": "pmc-u-whitespace-nowrap",
 	"c_span_text": "Label Text",
 	"c_span_url": "#",
 	"c_span_link_classes": ""
