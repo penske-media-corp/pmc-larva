d3b27ca1f08caf2949ce5a5df0f4b442a1a5cd37
laras126 <laras126@gmail.com>
2019-08-06 12:20:41 -0400

    docs: feat: keyboard functionality for mobile height toggle

--- packages/larva-js/src/interface/MobileHeightToggle/MobileHeightToggle.js
+++ packages/larva-js/src/interface/MobileHeightToggle/MobileHeightToggle.js
@@ -6,8 +6,16 @@ export default class MobileHeightToggle {
 		this.el = el;
 		this.el.pmcMobileHeightToggle = this;
 		this.isExpanded = false;
+		
 		this.toggle = this.toggle.bind( this );
-		this.el.addEventListener( 'click', this.toggle );
+		this.onClick = this.onClick.bind( this );
+		this.onFocus = this.onFocus.bind( this );
+		this.onBlur = this.onBlur.bind( this );
+		this.keyDown = this.keyDown.bind( this );
+		
+		this.el.addEventListener( 'click', this.onClick );
+		this.el.addEventListener( 'focus', this.onFocus );
+		this.el.addEventListener( 'blur', this.onBlur );
 	}
 	
 	destroy() {
@@ -16,20 +24,39 @@ export default class MobileHeightToggle {
 		delete this.el.pmcMobileHeightToggle;
 	}
 
-	toggle( e ) {
+	onBlur( e ) {
+		document.removeEventListener( 'keydown', this.keyDown );
+	}
+
+	keyDown( e ) {	
+		if ( 'Enter' === e.key ) {
+			this.toggle( e );
+		}
+	}
+
+	onFocus( e ) {
+		document.addEventListener( 'keydown', this.keyDown );
+	}
+
+	onClick( e ) {
+		
 		if ( undefined !== this.el.pmcMobileHeightToggle ) {
 
 			// Bubble event
 			if ( e.target.classList.contains( 'pmc-js-MobileHeightToggle-trigger' ) ) {
-				if ( ! this.isExpanded ) {
-					e.preventDefault();
-					this.el.classList.add( 'is-expanded' );
-					this.isExpanded = true;
-				} else {
-					this.el.classList.remove( 'is-expanded' );
-					this.isExpanded = false;
-				}
+				this.toggle( e );
 			}
+		}
+	}
+
+	toggle( e ) {
+		if ( ! this.isExpanded ) {
+			e.preventDefault();
+			this.el.classList.add( 'is-expanded' );
+			this.isExpanded = true;
+		} else {
+			this.el.classList.remove( 'is-expanded' );
+			this.isExpanded = false;
 		}
 	}
 
