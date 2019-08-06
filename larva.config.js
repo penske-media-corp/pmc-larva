10b07a2f7d08d036ea88499d4df0b6d4b60fe67b
laras126 <laras126@gmail.com>
2019-08-06 17:37:44 -0400

    DOPS-1824: uncomment project nav, set larva project dir to self

--- larva.config.js
+++ larva.config.js
@@ -3,6 +3,7 @@ const path = require( 'path' );
 module.exports = {
 
 	patterns: {
-		larvaPatternsDir: path.resolve( './packages/larva-patterns' )
+		larvaPatternsDir: path.resolve( './packages/larva-patterns' ),
+		projectPatternsDir: path.resolve( './packages/larva-patterns' ),
 	}
 };
