3a8c98c7a6faa1b6b2f57ed17f1447befff6a4af
laras126 <laras126@gmail.com>
2019-08-06 17:45:51 -0400

    DOPS-1824: feat: add source to server route

--- packages/larva/lib/server.js
+++ packages/larva/lib/server.js
@@ -11,18 +11,18 @@ const twigPaths = getPatternPathsToLoad( appConfiguration );
 
 let loader = new TwingLoaderFilesystem( twigPaths );
 
-// TODO: make this an array / iterator combo
-if ( appConfiguration.projectPatternsDir ) {
-	loader.addPath( path.resolve( appConfiguration.projectPatternsDir, './04-components' ), 'components' );
-	loader.addPath( path.resolve( appConfiguration.projectPatternsDir, './05-objects' ), 'objects' );
-	loader.addPath( path.resolve( appConfiguration.projectPatternsDir, './08-modules' ), 'modules' );
-}
-
+// TODO: Could be an array/iterator if the namespace can be extracted from the key, the larva.config API could
+// change to `patterns: { larva: /larva/path/here/, project: /project/path/here }`
 loader.addPath( appConfiguration.larvaPatternsDir, 'larva' );
+
+if( appConfiguration.projectPatternsDir ) {
+	loader.addPath( appConfiguration.projectPatternsDir, 'project' );
+}
 
 let twing = new TwingEnvironment( loader, { debug: true } );
 
 app.use( express.static( 'build' ) );
+
 // TODO: these will be updated to paths that point to a node module for use out of the mono-repo
 app.use( '/svgs' , express.static( path.join( appConfiguration.larvaPatternsDir, '../larva-svg/build' ) ) );
 app.use( '/js' , express.static( path.join( appConfiguration.larvaPatternsDir, '../larva-js/build' ) ) );
@@ -34,8 +34,8 @@ app.get( '/', function (req, res) {
 	res.end( twing.render( 'index.html', { name: 'Welcome' } ) );
 });
 
-app.get( '/:type/:name', function (req, res) {
-	let patternsPath = getPatternsIndexPath( appConfiguration );
+app.get( '/:source/:type/:name', function (req, res) {
+	let patternsPath = 'larva' === req.params.source ? appConfiguration.larvaPatternsDir : appConfiguration.projectPatternsDir;
 	req.params[ 'data' ] = getPatternData( patternsPath, req.params );
 	req.params[ 'json_pretty' ] = JSON.stringify( req.params[ 'data' ], null, '\t' );
 	req.params[ 'sprite_data' ] = fs.readFileSync( path.join( __dirname, '../../larva-svg/build/defs/svg/sprite.defs.svg' ) );
