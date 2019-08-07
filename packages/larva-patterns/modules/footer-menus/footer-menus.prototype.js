c4a5e05404db98957afc1bc3dcd5638a1aaf8bb6
laras126 <laras126@gmail.com>
2019-08-07 13:20:50 -0400

    DOPS-1824: refactor: rename footer-links to footer-menus

--- packages/larva-patterns/modules/footer-menus/footer-menus.prototype.js
+++ packages/larva-patterns/modules/footer-menus/footer-menus.prototype.js
@@ -2,7 +2,7 @@ const path = require( 'path' );
 const clone = require( '@penskemediacorp/larva' ).clone;// This should be in this repo, probably
 const o_nav = clone( path.resolve( __dirname, '../../objects/o-nav/o-nav.prototype' ) );
 
-o_nav.o_nav_title_text = 'Footer Links';
+o_nav.o_nav_title_text = 'Footer Menu';
 o_nav.o_nav_title_id_attr = 'pmc-footer';
 o_nav.o_nav_classes = 'pmc-js-MobileHeightToggle pmc-u-color-white pmc-u-text-align-center@tablet pmc-u-border-a-1@mobile-max pmc-u-border-color-white';
 o_nav.o_nav_title_classes = 'pmc-js-MobileHeightToggle-trigger pmc-u-padding-lr-1 pmc-u-margin-tb-00 pmc-u-padding-t-1 pmc-u-padding-t-050@tablet pmc-u-padding-b-050 pmc-u-width-100p@mobile-max a-icon-after a-icon-arrow-down a-icon-after:margin-l-auto a-icon-after-remove@tablet a-icon-invert pmc-u-cursor-pointer pmc-u-font-family-primary pmc-u-font-weight-bold pmc-u-font-size-20';
