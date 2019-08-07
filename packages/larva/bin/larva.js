e4b671d97da3720ab50f1e963ebb914082fa7927
laras126 <laras126@gmail.com>
2019-08-07 17:01:38 -0400

    DOPS-1824: todo: add notes in larva bin

--- packages/larva/bin/larva.js
+++ packages/larva/bin/larva.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 'use strict';
 
+// TODO: add switch statement here for
+/**
+ * 1. server
+ * 2. json - should write patterns JSON to build/json using lib/writeData.js
+ */
+
 require( '../lib/server' );
