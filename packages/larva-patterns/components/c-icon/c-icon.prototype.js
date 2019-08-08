603b6fa944552083093ede8843387ce9548ad01c
laras126 <laras126@gmail.com>
2019-08-08 18:06:53 -0400

    footer: feat: add utils and pattern for social footer

--- packages/larva-patterns/components/c-icon/c-icon.prototype.js
+++ packages/larva-patterns/components/c-icon/c-icon.prototype.js
@@ -1,5 +1,10 @@
-module.exports = {
-	"c_icon_name": "byline-plus",
-	"c_icon_url": "#",
-	"c_icon_classes": ""
-};
+const path = require( 'path' );
+
+let c_icon = {};
+
+c_icon.c_icon_link_classes = '';
+c_icon.c_icon_name = 'byline-plus';
+c_icon.c_icon_url = '#';
+c_icon.c_icon_classes = 'pmc-u-display-block pmc-u-width-16 pmc-u-height-16';
+c_icon.c_icon_rel_name = 'noopener noreferrer';
+module.exports = c_icon;
