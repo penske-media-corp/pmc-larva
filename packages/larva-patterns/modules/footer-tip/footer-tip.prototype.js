2b526bc26878f1cee2b17dfabfdaea4b2f1d23f9
laras126 <laras126@gmail.com>
2019-08-08 18:19:17 -0400

    footer: feat: add footer tip

---
+++ packages/larva-patterns/modules/footer-tip/footer-tip.prototype.js
@@ -0,0 +1,15 @@
+module.exports = {
+	"c_title": {
+		"c_title_classes": "pmc-u-margin-b-025 pmc-u-font-family-primary",
+		"c_title_text": "Have a Tip?"
+	},
+	"c_tagline": {
+		"c_tagline_classes": "pmc-u-font-size-14 u-font-style-italic pmc-u-font-family-basic",
+		"c_tagline_text": "We want to hear from you! Send us a tip using our annonymous form."
+	},
+	"c_button": {
+		"c_button_classes": "pmc-u-color-white pmc-u-color-grey-light:hover pmc-u-background-black pmc-u-font-size-12 pmc-u-font-weight-bold pmc-u-text-transform-uppercase a-icon-after a-icon-arrow-right a-icon-invert",
+		"c_button_url": "https://deadline.com/tip-line/",
+		"c_button_text": "Send Us a Tip"
+	}
+};
