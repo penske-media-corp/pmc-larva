0be7b74a620d8367a873610c19bc6a75007a7cde
laras126 <laras126@gmail.com>
2019-08-16 12:32:12 -0700

    PMCP-1402: fix: extend c_email_field for ghost

--- packages/larva-patterns/components/c-email-field/c-email-field.ghost.js
+++ packages/larva-patterns/components/c-email-field/c-email-field.ghost.js
@@ -1,10 +1,10 @@
-module.exports = {
-	"c_email_field_classes": "pmc-u-font-size-14",
-	"c_email_field_label_classes": "pmc-u-padding-r-050 pmc-u-font-weight-strong",
-	"c_email_field_label_for_attr": "",
-	"c_email_field_label_text": "Your Email",
-	"c_email_field_input_classes": "pmc-u-background-transparent pmc-u-border-a-0 pmc-u-color-currentColor pmc-u-padding-a-050 pmc-u-font-size-14",
-	"c_email_field_input_id_attr": "",
-	"c_email_field_input_name_attr": "",
-	"c_email_field_input_placeholder_attr": "you@email.com"
-};
+
+const path = require( 'path' );
+const clone = require( '@penskemediacorp/larva' ).clone;// This should be in this repo, probably
+const c_email_field = clone( path.resolve( __dirname, './c-email-field.prototype' ) );
+
+c_email_field.c_email_field_classes = "pmc-u-font-size-14";
+c_email_field.c_email_field_label_classes = "pmc-u-padding-r-050 pmc-u-font-weight-bold";
+c_email_field.c_email_field_input_classes = "pmc-u-background-transparent pmc-u-border-a-0 pmc-u-color-currentColor pmc-u-padding-a-050 pmc-u-font-size-14";
+
+module.exports = c_email_field;
