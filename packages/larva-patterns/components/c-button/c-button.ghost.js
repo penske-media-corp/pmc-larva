c81f8afbd6d399e666673fb895b059b552d86500
laras126 <laras126@gmail.com>
2019-08-16 16:39:14 -0700

    lists: feat: correct color in ghost button, support button or anchor tag

--- packages/larva-patterns/components/c-button/c-button.ghost.js
+++ packages/larva-patterns/components/c-button/c-button.ghost.js
@@ -2,7 +2,8 @@ const path = require( 'path' );
 const clone = require( '@penskemediacorp/larva' ).clone;
 const c_button = clone( path.resolve( __dirname, './c-button.prototype' ) );
 
-c_button.c_button_classes += "pmc-a-unstyle-button pmc-u-cursor-pointer pmc-u-border-a-3 pmc-u-border-radius-5 pmc-u-color-brand-primary pmc-u-background-grey:hover pmc-u-padding-tb-050 pmc-u-padding-lr-1 pmc-u-display-inline-flex pmc-u-color-grey-dark:hover pmc-u-font-weight-bold";
+c_button.c_button_classes += "pmc-a-unstyle-button pmc-u-cursor-pointer pmc-u-border-a-3 pmc-u-border-radius-5 pmc-u-color-brand-primary pmc-u-padding-tb-050 pmc-u-padding-lr-1 pmc-u-color-grey-dark:hover pmc-u-font-weight-bold pmc-u-display-inline-block";
 c_button.c_button_type_attr = "";
+c_button.c_button_url = "#";
 
 module.exports = c_button;
