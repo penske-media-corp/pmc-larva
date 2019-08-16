d07a45786ba546e403c6441c31079531d051f4ca
laras126 <laras126@gmail.com>
2019-08-16 12:26:34 -0700

    PMCP-1402: feat: o-email-capture-form pattern + minimal variation

---
+++ packages/larva-patterns/objects/o-email-capture-form/o-email-capture-form.minimal.js
@@ -0,0 +1,15 @@
+const path = require( 'path' );
+const clone = require( '@penskemediacorp/larva' ).clone;// This should be in this repo, probably
+const o_email_capture_form = clone( path.resolve( __dirname, './o-email-capture-form.prototype' ) );
+const c_button = clone( path.resolve( __dirname, '../../components/c-button/c-button.plain' ) );
+const c_email_field = clone( path.resolve( __dirname, '../../components/c-email-field/c-email-field.ghost' ) );
+
+c_button.c_button_classes += " a-icon-after a-icon-arrow-right";
+c_button.c_button_inner_classes = "a-screen-reader-only";
+c_button.c_button_type_attr = "submit";
+
+o_email_capture_form.c_button = c_button;
+o_email_capture_form.c_email_field = c_email_field;
+o_email_capture_form.o_email_capture_form_inner_classes = "pmc-u-flex pmc-u-align-items-center pmc-u-justify-content-center pmc-u-border-b-1 pmc-u-border-color-currentColor";
+
+module.exports = o_email_capture_form;
