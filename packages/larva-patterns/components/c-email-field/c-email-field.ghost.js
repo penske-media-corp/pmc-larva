0e60c97eb805313567e20a4d511d926584791f7a
laras126 <laras126@gmail.com>
2019-08-16 12:28:25 -0700

    PMCP-1402: feat: c-email-field and ghost variation

---
+++ packages/larva-patterns/components/c-email-field/c-email-field.ghost.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"c_email_field_classes": "pmc-u-font-size-14",
+	"c_email_field_label_classes": "pmc-u-padding-r-050 pmc-u-font-weight-strong",
+	"c_email_field_label_for_attr": "",
+	"c_email_field_label_text": "Your Email",
+	"c_email_field_input_classes": "pmc-u-background-transparent pmc-u-border-a-0 pmc-u-color-currentColor pmc-u-padding-a-050 pmc-u-font-size-14",
+	"c_email_field_input_id_attr": "",
+	"c_email_field_input_name_attr": "",
+	"c_email_field_input_placeholder_attr": "you@email.com"
+};
