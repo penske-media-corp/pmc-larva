96b51d0813a708f9c74041b9781ac87918372f83
laras126 <laras126@gmail.com>
2019-08-20 16:29:04 -0500

    tokens: feat: function to transform into kebab case

--- packages/larva-tokens/gulpfile.js
+++ packages/larva-tokens/gulpfile.js
@@ -20,7 +20,27 @@ gulp.task('default', ( done ) => {
 	gulp.src('src/brands/*.json')
 		.pipe(theo({
 			transform: { type: 'web' },
-			format: { type: 'html' }
+			format: { 
+				type: 'html',
+				options: {
+					transformPropName: ( name ) =>  {
+						let kebabCase = [];
+
+						// TODO: find a more concise way of turning a string into an iterable
+						for (let i = 0; i < name.length; i++) {
+							let letter = name[i];
+							kebabCase[i] = letter;
+						}
+
+						return kebabCase.reduce( ( a, b ) => {
+							if ( '_' === b ) {
+								return a.toLowerCase() + '-';
+							}
+							return a.toLowerCase() + b.toLowerCase();
+						});
+					}
+				} 
+			}
 		}))
 		.pipe(gulp.dest('style-guides'))
 
