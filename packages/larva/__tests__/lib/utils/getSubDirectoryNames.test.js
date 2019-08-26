262c48f98d731d91ee752e3531718eedd9950f44
laras126 <laras126@gmail.com>
2019-08-26 16:54:26 -0400

    pattern-nav: passing test, rename file

---
+++ packages/larva/__tests__/lib/utils/getSubDirectoryNames.test.js
@@ -0,0 +1,17 @@
+const assert = require( 'assert' );
+const getPatternsList = require( '../../../lib/utils/getSubDirectoryNames' );
+const appConfiguration = require( '../../../lib/utils/getAppConfiguration' )('patterns');
+const expectedObjectsList = [ 'o-crap', 'o-nav' ];
+const expectedComponentsList = [ 'c-nav-link' ];
+
+console.log( appConfiguration.projectPatternsDir );
+
+describe( 'getSubDirectoryNames', () => {
+	it( 'gets an array of subdir names for objects', () => {
+		assert.deepEqual( getPatternsList( appConfiguration.projectPatternsDir + '/objects' ), expectedObjectsList );
+	});
+
+	it( 'gets an array of subdir names for components', () => {
+		assert.deepEqual( getPatternsList( appConfiguration.projectPatternsDir + '/components' ), expectedComponentsList );
+	});
+});
