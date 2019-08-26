49c0c3c8e0cb5f2984fed84e37c83e257af2bb48
laras126 <laras126@gmail.com>
2019-08-26 14:13:03 -0400

    o-figure: fix: update o-figure classes

--- packages/larva-patterns/objects/o-figure/o-figure.prototype.js
+++ packages/larva-patterns/objects/o-figure/o-figure.prototype.js
@@ -1,8 +1,10 @@
 module.exports = {
-	"o_figure_permalink_url": "https://notlaura.com",
+	"o_figure_permalink_url": "#",
+	"o_figure_link_classes": "lrv-a-unstyle-link",
 	"o_figure_alt_attr": "Thumbnail image",
 	"o_figure_placeholder_image_url": "https://placehold.it/300x300",
 	"o_figure_image_url": "https://placehold.it/300x300",
+	"o_figure_image_classes": "lrv-u-width-100p lrv-u-display-block lrv-u-height-auto lrv-a-hover-effect lrv-a-hover-effect--fade",
 	"o_figure_caption_markup": "Here is some caption text",
 	"o_figure_credit_text": "Photo Credit",
 	"o_figure_screen_reader_text": "Text if there is no caption"
