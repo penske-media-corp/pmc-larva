e37ccd76b2d6282304c849d6312af1180c083ba0
laras126 <laras126@gmail.com>
2019-08-30 11:33:27 -0400

    o-icon-button: feat: add icon button

---
+++ packages/larva-patterns/objects/o-icon-button/o-icon-button.prototype.js
@@ -0,0 +1,18 @@
+const c_span_prototype = require( '../../components/c-span/c-span.prototype' );
+const c_icon_prototype = require( '../../components/c-icon/c-icon.prototype' );
+
+const c_span = Object.assign( {}, c_span_prototype );
+const c_icon = Object.assign( {}, c_icon_prototype );
+
+c_icon.c_icon_name = 'hamburger';
+c_icon.c_icon_url = false;
+
+c_span.c_span_text = 'Menu';
+c_span.c_span_classes = 'lrv-u-margin-l-050';
+c_span.c_span_url = false;
+
+module.exports = {
+	o_icon_button_classes: 'lrv-a-unstyle-button lrv-a-unstyle-link lrv-u-cursor-pointer lrv-u-flex',
+	c_icon: c_icon,
+	c_span: c_span,
+};
