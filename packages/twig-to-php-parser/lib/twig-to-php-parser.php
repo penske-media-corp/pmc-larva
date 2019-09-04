41fe5ac04ca42034bae66601bc5b7e70a83129dd
laras126 <laras126@gmail.com>
2019-09-04 17:25:05 -0400

    [support-twig-filters] feat: update mustache regex to incl. filters

--- packages/twig-to-php-parser/lib/twig-to-php-parser.php
+++ packages/twig-to-php-parser/lib/twig-to-php-parser.php
@@ -64,8 +64,8 @@ function twig_to_php_parser( $patterns_dir_path, $template_dir_path ) {
 		$loop_regex = '/({% for item in\s*)(\w*)/';
 
 		// Get matches for {{ name }}
-		// https://regex101.com/r/ACN0rE/1
-		$mustache_regex = '/({{\s*)(\w*)(\s*}})/';
+		// https://regex101.com/r/ACN0rE/2
+		$mustache_regex = '/({{\s*)(\w*?\|?\w*)(\s*}})/';
 
 		// Get matches for {% include "path/c-element.twig" with data %}
 		// https://regex101.com/r/ns5kBR/2
