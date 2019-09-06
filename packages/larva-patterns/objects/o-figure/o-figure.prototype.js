0b982dbb66e49bf461e4263e8f9c53d571cbf2bd
laras126 <laras126@gmail.com>
2019-09-06 10:58:24 -0400

    [o-figure] feat: add lazy srcset attrs

--- packages/larva-patterns/objects/o-figure/o-figure.prototype.js
+++ packages/larva-patterns/objects/o-figure/o-figure.prototype.js
@@ -3,6 +3,8 @@ module.exports = {
 	"o_figure_link_classes": "lrv-a-unstyle-link",
 	"o_figure_alt_attr": "Thumbnail image",
 	"o_figure_placeholder_image_url": "https://placehold.it/300x300",
+	"o_figure_lazy_srcset_url": "",
+	"o_figure_lazy_sizes_attr": "",
 	"o_figure_image_url": "https://placehold.it/300x300",
 	"o_figure_image_classes": "lrv-u-width-100p lrv-u-display-block lrv-u-height-auto lrv-a-hover-effect lrv-a-hover-effect--fade",
 	"o_figure_caption_markup": "Here is some caption text",
