52570ae2148efbde9c917b864ffeb48d5a6b77cc
laras126 <laras126@gmail.com>
2019-09-09 16:33:47 -0400

    [backstop] feat: add config to pmc-larva to test

--- larva.config.js
+++ larva.config.js
@@ -1,6 +1,19 @@
 const path = require( 'path' );
 
 module.exports = {
+	visual_regression_testing: {
+		pmcMainQaUrl: 'http://localhost:3000',
+		pmcTestPaths: [
+			'/larva/modules/footer'
+		],
+		pmcScenario: {
+			'delay': 1000,
+			'misMatchThreshold': 0.5,
+			'selectors': [
+				'.footer'
+			]
+		}
+	},
 
 	patterns: {
 		larvaPatternsDir: path.resolve( './packages/larva-patterns' ),
