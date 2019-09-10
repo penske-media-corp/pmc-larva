0459ea4ab92b01c0c3f663589918671090c033a0
laras126 <laras126@gmail.com>
2019-09-10 16:22:12 -0400

    [cli] feat: get args from cli

---
+++ packages/visual-regression-testing/__tests__/cli.test.js
@@ -0,0 +1,22 @@
+const assert = require( 'assert' );
+const path = require( 'path' );
+const { getCliArgs } = require( '../lib/backstop-utils' );
+// const getAppConfiguration = require( '@penskemediacorp/larva' ).config;
+
+const processMocker = {
+	argv: [
+		'/usr/local/Cellar/node/10.11.0/bin/node',
+		'/Users/laraschenck/Sites/pmc-deadline-2019/assets/node_modules/.bin/backstop',
+		'--configPath=node_modules/pmc-build-utils/lib/backstop.js',
+		'test',
+		'--modules=footer,mega-menu-content,social-share'
+	]
+};
+
+describe( 'cli for testing', function() {
+
+	it( 'gets a list of modules from the cli', () => {
+		assert.deepEqual( getCliArgs( processMocker.argv ), [ 'footer', 'mega-menu-content', 'social-share' ] );
+	});
+
+});
--- packages/visual-regression-testing/lib/backstop-utils.js
+++ packages/visual-regression-testing/lib/backstop-utils.js
@@ -7,5 +7,16 @@ module.exports = {
 
 		return configUrl;
 
+	},
+
+	getCliArgs: ( argv ) => {
+
+		if ( argv[4] && argv[4].includes( '--modules=' ) ) {
+			const modulesArg = argv[4].split( '=' );
+			return modulesArg[1].split( ',' );
+		}
+
+		console.log( chalk.red( 'You must provide an argument in the form of: --modules=footer,header' ) );
 	}
+
 };
