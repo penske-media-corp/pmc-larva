eb2013ecd4b847a53a01e48bb118bfbccdb9d51a
laras126 <laras126@gmail.com>
2019-09-10 16:22:54 -0400

    [backstop] fix: comment ad hiding script

--- packages/visual-regression-testing/backstop_data/engine_scripts/puppet/onReady.js
+++ packages/visual-regression-testing/backstop_data/engine_scripts/puppet/onReady.js
@@ -4,16 +4,17 @@ module.exports = async (page, scenario, vp) => {
 	await page.evaluate(() => {
 		let ids = [];
 
-		if ( typeof googletag !== typeof undefined ) {
-			ids.push( ... googletag.pubads().getSlots().map( slot => slot.getSlotElementId() ) );
-			ids.push( 'jpsuperheader', 'skin-ad-section' ); // IDs that are not part of googetag array
-		}
+		// Leave commented until we are testing production screenshots.
+		// if ( typeof googletag !== typeof undefined ) {
+		// 	ids.push( ... googletag.pubads().getSlots().map( slot => slot.getSlotElementId() ) );
+		// 	ids.push( 'jpsuperheader', 'skin-ad-section' ); // IDs that are not part of googetag array
+		// }
 
-		ids.forEach( id => {
-			if ( null !== document.getElementById( id ) ) {
-				document.getElementById( id ).style.cssText = 'display: none !important';
-			}
-		});
+		// ids.forEach( id => {
+		// 	if ( null !== document.getElementById( id ) ) {
+		// 		document.getElementById( id ).style.cssText = 'display: none !important';
+		// 	}
+		// });
 	});
 	
 	await require('./clickAndHoverHelper')(page, scenario);
