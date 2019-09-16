6433d87123c598364a2041e76c9f92777f1d30c9
laras126 <laras126@gmail.com>
2019-09-16 18:48:00 -0400

    [svg-in-parser] test: update test

--- packages/twig-to-php-parser/__tests__/fixtures/template-parts-expected/patterns/components/c-logo.php
+++ packages/twig-to-php-parser/__tests__/fixtures/template-parts-expected/patterns/components/c-logo.php
@@ -2,5 +2,5 @@
 // This is a generated file. Refer to the relevant Twig file for adjusting this markup.
 ?>
 <div>
-	<?php \PMC::render_template( CHILD_THEME_PATH . '/assets/build/icons/' . ( $c_logo_svg ?? '' ) . '.svg', [ ], true ); ?>
+	<?php \PMC::render_template( CHILD_THEME_PATH . '/assets/build/icons/' . ( $c_logo_svg ?? '' ) . '.svg', [], true ); ?>
 </div>
