b65c6df869a328d50254c6448feaf8ad7c686558
laras126 <laras126@gmail.com>
2019-09-18 11:02:01 -0400

    [backstop] refact: use getScenarios in  package index

--- packages/backstopjs-config/index.js
+++ packages/backstopjs-config/index.js
@@ -1,12 +1,12 @@
 const merge = require( 'webpack-merge' );
-const getAppConfiguration = require( '@penskemediacorp/larva' ).config;
-
-const path    = require( 'path' );
+const path = require( 'path' );
 const chalk = require( 'chalk' );
 const backstopUtils = require( './lib/utils' );
+const getScenarios = require( './lib/getScenarios' );
 
-const appConfiguration = getAppConfiguration( 'backstopjs' );
-const { pmcMainQaUrl, pmcTestPaths, pmcScenario, backstopApi } = appConfiguration;
+// Get app config from project
+const getAppConfiguration = require( '@penskemediacorp/larva' ).config;
+const appConfiguration = getAppConfiguration( 'backstop' );
 
 const cliArgs = ( function getCliArgs() {
 	if ( 'test' === process.env.NODE_ENV ) {
@@ -16,47 +16,27 @@ const cliArgs = ( function getCliArgs() {
 	return process.argv;
 }() );
 
-const urlBase = backstopUtils.maybeUseCliUrl( cliArgs, pmcMainQaUrl );
+const urlBase = backstopUtils.maybeUseCliUrl( cliArgs, appConfiguration.pmcMainQaUrl );
 const modulesFromCli = backstopUtils.getCliModuleArgs( cliArgs );
 const selectors = backstopUtils.prepareTestSelectors( modulesFromCli );
 
-console.log( modulesFromCli );
-
-let paths = backstopUtils.prepareTestPaths( modulesFromCli, 'larva' );
+let paths = backstopUtils.prepareTestPaths( appConfiguration.larvaModules, appConfiguration.pmcTestPaths );
 
 // Exit if no paths in config.
 if ( 0 === paths.length ) {
-	console.error( chalk.red.bold( '\nPlease specify paths to test in larva.config.js in the structure `backstop.pmcTestPaths`, or add an argument for --modules in the CLI.\n' ) );
+	console.error( chalk.red.bold( '\nPlease specify paths to test in larva.config.js in the structure `backstop.pmcTestPaths`, or add modules in `backstop.larvaModules`\n' ) );
 	process.exit( 1 );
 }
 
 // Exit if no URL from config or CLI.
-if ( undefined === pmcMainQaUrl && false === urlFromCli && null === modulesFromCli ) {
+if ( undefined === appConfiguration.pmcMainQaUrl && false === urlFromCli && null === modulesFromCli ) {
 	console.error( chalk.red.bold( '\nPlease specify a QA URL in pmc.config.js in `backstop.pmcMainQaUrl`, or pass in a full URL with the comman e.g. `npm run backstop -- test --url=https://example.com`\n' ) );
 	process.exit( 1 );
 }
 
+const scenarios = getScenarios( urlBase, paths, selectors,  appConfiguration.pmcScenario );
+
 console.log( chalk.blue( 'Testing paths: \n' + paths ) );
-
-let scenarios = [];
-
-for ( let i = 0; i < paths.length; i++ ) {
-	console.log( urlBase + paths[i] );
-	
-	scenarios.push( merge({
-		'label': paths[i],
-		'url': urlBase + paths[i],
-		'hideSelectors': [],
-		'removeSelectors': [],
-		'selectors': [
-			selectors[i]
-		],
-		'delay': 500,
-		'misMatchThreshold': 0.1,
-		'onBeforeScript': 'puppet/onBefore.js',
-		'onReadyScript': 'puppet/onReady.js'
-	}, pmcScenario ) );
-};
 
 module.exports = merge({
 	'id': 'backstop_test',
@@ -95,5 +75,5 @@ module.exports = merge({
 	'asyncCompareLimit': 50,
 	'debug': false,
 	'debugWindow': false
-}, backstopApi );
+}, appConfiguration.backstopApi );
 
