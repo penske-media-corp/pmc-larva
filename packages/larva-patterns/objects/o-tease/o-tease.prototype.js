12eaa887dfc723fbb5e73bb3498c1b987f8f39bb
laras126 <laras126@gmail.com>
2019-09-27 15:27:27 -0400

    [o-figure] refact: use o-figure defaults in o-tease

--- packages/larva-patterns/objects/o-tease/o-tease.prototype.js
+++ packages/larva-patterns/objects/o-tease/o-tease.prototype.js
@@ -1,3 +1,11 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const o_figure_prototype = require( '../o-figure/o-figure.prototype' );
+const o_figure = clonedeep( o_figure_prototype );
+
+o_figure.o_figure_crop_class = 'lrv-u-crop-2x3';
+o_figure.o_figure_alt_attr = 'Thumbnail image';
+
 module.exports = {
 	"o_tease_url": "#",
 	"o_tease_classes": "lrv-u-flex lrv-u-align-items-center",
@@ -15,10 +23,5 @@ module.exports = {
 		"c_tagline_classes": "",
 		"c_tagline_markup": "Tagline Text"
 	},
-	"o_figure": {
-		"o_figure_crop_class": "lrv-u-crop-2x3",
-		"o_figure_alt_attr": "Thumbnail image",
-		"o_figure_placeholder_image_url": "https://placehold.it/300x300",
-		"o_figure_caption_text": "Here in o-tease"
-	}
+	o_figure: o_figure
 };
