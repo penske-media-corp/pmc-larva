bbfa7271c3f757af12d970f595bbcc8cb168c207
laras126 <laras126@gmail.com>
2019-09-27 14:25:18 -0400

    [c-icon] refact: add screen reader text to c-icon, clean up data file

--- packages/larva-patterns/components/c-icon/c-icon.prototype.js
+++ packages/larva-patterns/components/c-icon/c-icon.prototype.js
@@ -1,10 +1,8 @@
-const path = require( 'path' );
-
-let c_icon = {};
-
-c_icon.c_icon_link_classes = '';
-c_icon.c_icon_name = 'byline-plus';
-c_icon.c_icon_url = '#';
-c_icon.c_icon_classes = 'lrv-u-display-block lrv-u-width-16 lrv-u-height-16';
-c_icon.c_icon_rel_name = 'noopener noreferrer';
-module.exports = c_icon;
+module.exports = {
+	c_icon_link_classes: '',
+	c_icon_name: 'byline-plus',
+	c_icon_link_screen_reader_text: 'Icon Link',
+	c_icon_url: false,
+	c_icon_classes: 'lrv-u-display-block lrv-u-width-16 lrv-u-height-16',
+	c_icon_rel_name: 'noopener noreferrer'
+};
