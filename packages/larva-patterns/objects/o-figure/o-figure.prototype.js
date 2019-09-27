bee803481909608828117725a1bdcf4a9fc142f1
laras126 <laras126@gmail.com>
2019-09-27 15:27:15 -0400

    [o-figure] refact: remove hover fade effect for now

--- packages/larva-patterns/objects/o-figure/o-figure.prototype.js
+++ packages/larva-patterns/objects/o-figure/o-figure.prototype.js
@@ -1,14 +1,14 @@
 module.exports = {
 	o_figure_permalink_url: false,
-	o_figure_classes: 'lrv-u-background-color-grey-light',
-	o_figure_link_classes: 'lrv-a-unstyle-link',
+	o_figure_classes: '',
+	o_figure_link_classes: 'lrv-a-unstyle-link ',
 	o_figure_crop_class: 'lrv-a-crop-16x9',
 	o_figure_alt_attr: 'Thumbnail image',
 	o_figure_placeholder_image_url: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
 	o_figure_lazy_srcset_url: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_m.jpg 240w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_n.jpg 320w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba.jpg 500w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg 640w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_b.jpg 1024w',
 	o_figure_lazy_sizes_attr: 'auto',
 	o_figure_image_url: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg',
-	o_figure_image_classes: 'lrv-u-width-100p lrv-u-display-block lrv-u-height-auto lrv-a-hover-effect lrv-a-hover-effect--fade',
+	o_figure_image_classes: 'lrv-u-background-color-grey-lightest lrv-u-width-100p lrv-u-display-block lrv-u-height-auto',
 	o_figure_caption_markup: false,
 	o_figure_credit_text: false,
 	o_figure_screen_reader_text: 'Image'
