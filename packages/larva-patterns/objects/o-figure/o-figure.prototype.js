2d4f68763b044d4af0542c7ee936af5046a3de41
laras126 <laras126@gmail.com>
2019-10-11 16:42:52 -0400

    [o-figure] refact: create c-figcaption, rm screen reader text

--- packages/larva-patterns/objects/o-figure/o-figure.prototype.js
+++ packages/larva-patterns/objects/o-figure/o-figure.prototype.js
@@ -1,5 +1,10 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const c_figcaption_prototype = require( '../../components/c-figcaption/c-figcaption.prototype' );
+const c_figcaption = clonedeep( c_figcaption_prototype );
+
 module.exports = {
-	c_button: false,
+	c_figcaption: c_figcaption,
 	o_figure_permalink_url: false,
 	o_figure_classes: '',
 	o_figure_link_classes: 'lrv-a-unstyle-link ',
@@ -11,9 +16,6 @@ module.exports = {
 	o_figure_lazy_srcset_url: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_m.jpg 240w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_n.jpg 320w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba.jpg 500w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg 640w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_b.jpg 1024w',
 	o_figure_lazy_sizes_attr: 'auto',
 	o_figure_image_classes: 'lrv-u-background-color-grey-lightest lrv-u-width-100p lrv-u-display-block lrv-u-height-auto',
-	o_figure_caption_markup: false,
-	o_figure_credit_text: false,
-	o_figure_screen_reader_text: 'Image',
 	o_figure_image_height_attr: '',
 	o_figure_image_width_attr: '',
 }
