03f5d4a5015a899a5f06c51e69755cdf930708ca
laras126 <laras126@gmail.com>
2019-10-14 14:58:56 -0400

    [o-figure] fix: naming corrections

--- packages/larva-patterns/objects/o-sponsored-by/o-sponsored-by.prototype.js
+++ packages/larva-patterns/objects/o-sponsored-by/o-sponsored-by.prototype.js
@@ -4,7 +4,7 @@ module.exports = {
 	"o_sponsored_by_text": "Sponsored by",
 	"o_figure": {
 		"o_figure_classes": "u-width-50",
-		"o_figure_permalink_url": "#",
+		"o_figure_link_url": "#",
 		"o_figure_alt_text": "Thumbnail image",
 		"o_figure_image_url": "https://picsum.photos/57/11"
 	}
