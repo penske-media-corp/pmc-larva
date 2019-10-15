e75d7f367faf550da2db88910c26f8481ff9937e
laras126 <laras126@gmail.com>
2019-10-15 17:31:00 -0400

    [o-figure] refact: remove lazy qualifier from names

--- packages/larva-patterns/components/c-lazy-image/c-lazy-image.prototype.js
+++ packages/larva-patterns/components/c-lazy-image/c-lazy-image.prototype.js
@@ -7,8 +7,8 @@ module.exports = {
 	c_lazy_image_alt_attr: 'Lazy loaded image',
 	c_lazy_image_src_url: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg',
 	c_lazy_image_placeholder_url: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
-	c_lazy_image_lazy_srcset_url: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_m.jpg 240w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_n.jpg 320w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba.jpg 500w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg 640w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_b.jpg 1024w',
-	c_lazy_image_lazy_sizes_attr: 'auto',
+	c_lazy_image_srcset_url: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_m.jpg 240w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_n.jpg 320w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba.jpg 500w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg 640w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_b.jpg 1024w',
+	c_lazy_image_sizes_attr: 'auto',
 	c_lazy_image_img_classes: 'lrv-u-background-color-grey-lightest lrv-u-width-100p lrv-u-display-block lrv-u-height-auto',
 	c_lazy_image_height_attr: '',
 	c_lazy_image_width_attr: ''
