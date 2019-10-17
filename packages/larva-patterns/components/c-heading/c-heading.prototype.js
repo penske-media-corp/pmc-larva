94983322cf416814adabdd23fb0ea20cc98371b2
laras126 <laras126@gmail.com>
2019-10-17 17:35:10 -0400

    [docs] fix: add default for primary heading

--- packages/larva-patterns/components/c-heading/c-heading.prototype.js
+++ packages/larva-patterns/components/c-heading/c-heading.prototype.js
@@ -3,5 +3,6 @@ module.exports = {
 	c_heading_url: '',
 	c_heading_link_classes: '',
 	c_heading_wrapper: false,
-	c_heading_wrapper_classes: ''
+	c_heading_wrapper_classes: '',
+	c_heading_is_primary_heading: false
 }
