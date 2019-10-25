0d46be6380b1d57d1c5ade144586069513b41b90
laras126 <laras126@gmail.com>
2019-10-25 15:40:59 -0400

    [misc] feat: add rel attr to c-button

--- packages/larva-patterns/components/c-button/c-button.prototype.js
+++ packages/larva-patterns/components/c-button/c-button.prototype.js
@@ -1,6 +1,7 @@
 module.exports = {
 	"c_button_classes": "lrv-a-unstyle-button lrv-u-cursor-pointer",
 	"c_button_target_attr": "",
+	"c_button_rel_attr": "",
 	"c_button_inner_classes": "",
 	"c_button_text": "Sign Up",
 	"c_button_url": "",
