00614d4047397ee6d1bd71590e5de2006aa3d257
laras126 <laras126@gmail.com>
2019-11-19 14:01:26 -0500

    [amh-footer] fix: clean up footer prototypes

--- packages/larva-patterns/modules/footer-social/footer-social.prototype.js
+++ packages/larva-patterns/modules/footer-social/footer-social.prototype.js
@@ -6,6 +6,7 @@ const o_social_list_icons = [];
 
 for (let i = 0; i < social.length; i++) {
 	let c_icon = clonedeep( c_icon_prototype );
+	c_icon.c_icon_url = '#';
 	c_icon.c_icon_name = social[i];
 	c_icon.c_icon_link_classes += ' lrv-u-display-block lrv-u-border-radius-50p lrv-u-color-grey lrv-u-margin-lr-050 lrv-u-padding-a-050 lrv-u-background-color-white lrv-u-color-grey-dark:hover';
 
