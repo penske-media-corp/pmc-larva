b88aecdbd21ce74518cc9688ca67e326f7b92c99
laras126 <laras126@gmail.com>
2019-11-25 18:03:14 -0500

    [o-card] fix: update project references

--- packages/larva-patterns/objects/o-span-group/o-span-group.prototype.js
+++ packages/larva-patterns/objects/o-span-group/o-span-group.prototype.js
@@ -1,13 +1,11 @@
-// Note: As of 10/28/19 o-span-group is currently not used anywhere in this project since the design it was created to accommodate changed. However, it might be useful later, so we are keeping it here. Remove this text when it is used, or remove o-span-group if it is October 2020 or later.
-
-const clondeep = require( 'lodash.clonedeep' );
-const c_span_prototype = require( '@penskemediacorp/larva-patterns/components/c-span/c-span.prototype' );
-const c_span = clondeep( c_span_prototype );
+const clonedeep = require( 'lodash.clonedeep' );
+const c_span_prototype = require( '../../components/c-span/c-span.prototype' );
+const c_span = clonedeep( c_span_prototype );
 
 module.exports = {
 	o_span_group_classes: '',
 	o_span_group_items: [
 		c_span,
-		clondeep( c_span )
+		c_span
 	]
 }
