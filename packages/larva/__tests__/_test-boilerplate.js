6f04840c02e9a8c900a2c7329717f39e7652467b
laras126 <laras126@gmail.com>
2019-11-27 13:04:41 -0500

    [write-json] docs: comment

--- packages/larva/__tests__/_test-boilerplate.js
+++ packages/larva/__tests__/_test-boilerplate.js
@@ -2,6 +2,7 @@ const assert = require( 'assert' );
 const path = require( 'path' );
 const fixture = path.join( __dirname, '../../fixtures' );
 
+// If needed:
 // const getAppConfiguration = require( '../../../lib/utils/getAppConfiguration' );
 
 describe( 'describe the test', () => {
