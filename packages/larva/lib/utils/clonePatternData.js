218a94c7f32d95b5d45a0b26e30542de9ac074c1
laras126 <laras126@gmail.com>
2019-11-27 17:23:21 -0500

    [write-json] fix: rm deprecated notice, too intrusive

--- packages/larva/lib/utils/clonePatternData.js
+++ packages/larva/lib/utils/clonePatternData.js
@@ -3,6 +3,5 @@ const chalk = require( 'chalk' );
 // TODO: this should be removed.
 module.exports = function clonePatternData( path ) {
 	const patternData = require( path );
-	console.warn( chalk.yellow.bold( 'clonePatternData is deprecated. Use lodash.clonedeep instead. Refer to o-email-capture-form.prototype.js in larva-patterns for an example.' ) );
 	return Object.assign( {}, patternData );
 }
