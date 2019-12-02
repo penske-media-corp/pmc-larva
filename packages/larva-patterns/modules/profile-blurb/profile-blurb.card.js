46bc0213e071188a393ea68a8657c31f592e2608
laras126 <laras126@gmail.com>
2019-12-02 17:40:20 -0500

    [PMCP-1653] refact: correct naming in variant

--- packages/larva-patterns/modules/profile-blurb/profile-blurb.card.js
+++ packages/larva-patterns/modules/profile-blurb/profile-blurb.card.js
@@ -1,25 +1,28 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
-const profile_blurb = clonedeep( require( './profile-blurb.prototype' ) );
+const profile_blurb_prototype = require( './profile-blurb.prototype' );
+const profile_blurb_card = clonedeep( profile_blurb_prototype );
+
 const c_button = clonedeep( require( '../../components/c-button/c-button.prototype' ) );
 
-profile_blurb.profile_blurb_classes = false;
-profile_blurb.c_lazy_image.c_lazy_image_classes = false;
-profile_blurb.profile_blurb_image_classes = false;
-profile_blurb.o_social_list = false;
+profile_blurb_card.profile_blurb_classes = false;
+profile_blurb_card.c_lazy_image.c_lazy_image_classes = false;
+profile_blurb_card.profile_blurb_image_classes = false;
+profile_blurb_card.o_social_list = false;
 
-profile_blurb.c_title.c_title_classes = 'lrv-u-font-family-primary lrv-u-text-transform-uppercase lrv-u-font-size-16 u-font-size-22@desktop-xl lrv-u-color-black';
-profile_blurb.c_link.c_link_classes = 'lrv-a-unstyle-link lrv-u-font-family-primary lrv-u-font-size-14@mobile-max lrv-u-font-size-16 lrv-u-font-size-18@desktop-xl u-color-light-desaturated-magenta';
-profile_blurb.c_span.c_span_classes = 'lrv-u-whitespace-nowrap lrv-u-font-size-14@mobile-max lrv-u-font-size-16 lrv-u-font-size-18@desktop-xl lrv-u-font-family-primary lrv-u-display-block';
-profile_blurb.c_dek.c_dek_classes = 'lrv-u-margin-tb-050 lrv-u-font-size-16 u-font-style-italic';
+profile_blurb_card.c_title.c_title_classes = 'lrv-u-font-family-primary lrv-u-text-transform-uppercase lrv-u-font-size-16 lrv-u-color-black';
+profile_blurb_card.c_link.c_link_classes = 'lrv-a-unstyle-link lrv-u-font-family-primary lrv-u-font-size-14@mobile-max lrv-u-font-size-16 lrv-u-font-size-18@desktop-xl';
+profile_blurb_card.c_span.c_span_classes = 'lrv-u-whitespace-nowrap lrv-u-font-size-14@mobile-max lrv-u-font-size-16 lrv-u-font-size-18@desktop-xl lrv-u-font-family-primary lrv-u-display-block';
+profile_blurb_card.c_dek.c_dek_classes = 'lrv-u-margin-tb-050 lrv-u-font-size-16 lrv-u-font-style-italic';
 
-profile_blurb.profile_blurb_c_button = 'lrv-u-text-align-center';
-profile_blurb.profile_blurb_detail_classes = 'lrv-u-margin-t-050';
+profile_blurb_card.profile_blurb_c_button = 'lrv-u-text-align-center';
+profile_blurb_card.profile_blurb_detail_classes = 'lrv-u-margin-t-050';
+
 c_button.c_button_text = 'View Profile';
 c_button.c_button_classes += ' lrv-u-display-inline-block lrv-u-margin-t-1 lrv-u-text-transform-uppercase lrv-u-font-size-14 lrv-u-background-color-black lrv-u-padding-lr-2 lrv-u-padding-tb-050 lrv-u-color-white lrv-u-color-grey-light:hover';
 c_button.c_button_url = '#';
-c_button.c_button_inner_classes = 'u-padding-lr-1@tablet';
+c_button.c_button_inner_classes = 'lrv-u-padding-lr-1@tablet';
 
-profile_blurb.c_button = c_button;
+profile_blurb_card.c_button = c_button;
 
 module.exports = profile_blurb;
