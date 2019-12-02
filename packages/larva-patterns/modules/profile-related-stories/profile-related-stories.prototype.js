c3d8571b5a31a5a31e71f5614de588ddd23ade08
laras126 <laras126@gmail.com>
2019-12-02 18:29:19 -0500

    [PMCP-1653] refact: move project classes out of larva

--- packages/larva-patterns/modules/profile-related-stories/profile-related-stories.prototype.js
+++ packages/larva-patterns/modules/profile-related-stories/profile-related-stories.prototype.js
@@ -5,24 +5,22 @@ const o_card_list = clonedeep( require( '../../objects/o-card-list/o-card-list.p
 
 c_heading.c_heading_text = 'Related Stories';
 c_heading.c_heading_classes = 'lrv-u-font-family-secondary lrv-u-font-size-28 lrv-u-font-size-40@tablet lrv-u-font-size-46@desktop-xl lrv-u-padding-r-1';
-c_heading.c_heading_outer = true;
-c_heading.c_heading_outer_classes = 'a-heading-border lrv-u-margin-b-1';
 
 o_card_list.o_card_list_classes += ' lrv-a-grid a-cols3@tablet';
 
 o_card_list.o_card_list_items.map( ( o_card ) => {
 
-	o_card.o_card_classes = 'lrv-u-flex u-flex-direction-column@tablet';
+	o_card.o_card_classes = 'lrv-u-flex lrv-u-flex-direction-column@tablet';
 
-	o_card.c_lazy_image.c_lazy_image_classes = 'u-width-30p@mobile-max lrv-u-flex-shrink-0 lrv-u-margin-r-1 lrv-u-margin-r-00@tablet';
+	o_card.c_lazy_image.c_lazy_image_classes = 'lrv-u-flex-shrink-0 lrv-u-margin-r-1 lrv-u-margin-r-00@tablet';
 	o_card.c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-2x3';
 
-	o_card.c_span.c_span_text = 'news';
-	o_card.c_span.c_span_classes += ' lrv-u-font-size-12@mobile-max lrv-u-font-family-primary lrv-u-font-weight-bold lrv-u-display-block lrv-u-padding-b-050 u-letter-spacing-012 u-text-transform-lowercase';
+	o_card.c_span.c_span_text = 'News';
+	o_card.c_span.c_span_classes += ' lrv-u-font-size-12@mobile-max lrv-u-font-family-primary lrv-u-font-weight-bold lrv-u-display-block lrv-u-padding-b-050';
 	o_card.c_span.c_span_url = '#';
 
 	o_card.c_title.c_title_text = 'Guggenheim’s Hilma af Klint Survey Is Most Popular Show in Its History';
-	o_card.c_title.c_title_classes = 'lrv-u-font-family-primary lrv-u-font-size-14@mobile-max lrv-u-font-size-16 u-font-size-22@desktop-xl lrv-u-font-weight-normal';
+	o_card.c_title.c_title_classes = 'lrv-u-font-family-primary lrv-u-font-size-14@mobile-max lrv-u-font-size-16 lrv-u-font-weight-normal';
 	o_card.c_title.c_title_link_classes = 'lrv-u-color-black';
 
 	o_card.c_timestamp = false;
