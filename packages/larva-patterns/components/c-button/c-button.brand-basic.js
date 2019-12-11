7585e98feac1c273848f9bddf7b8c6bc98b9f7fb
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-11 19:25:19 +0530

    Update c-button's prototype

--- packages/larva-patterns/components/c-button/c-button.brand-basic.js
+++ packages/larva-patterns/components/c-button/c-button.brand-basic.js
@@ -1,6 +1,8 @@
-const c_button_protoype = require( './c-button.prototype' );
-const c_button = Object.assign( {}, c_button_protoype );
+const clonedeep = require( 'lodash.clonedeep' );
 
-c_button.c_button_classes += " lrv-u-padding-tb-050 lrv-u-padding-lr-1 lrv-u-background-color-brand-primary lrv-u-color-white";
+const c_button_protoype = require( './c-button.prototype' );
+const c_button = clonedeep( c_button_protoype );
+
+c_button.c_button_classes += ' lrv-u-padding-tb-050 lrv-u-padding-lr-1 lrv-u-background-color-brand-primary lrv-u-color-white';
 
 module.exports = c_button;
--- packages/larva-patterns/components/c-button/c-button.stripped.js
+++ packages/larva-patterns/components/c-button/c-button.stripped.js
@@ -1,7 +1,8 @@
-const path = require( 'path' );
-const clone = require( '@penskemediacorp/larva' ).clone;
-const c_button = clone( path.resolve( __dirname, './c-button.prototype' ) );
+const clonedeep = require("lodash.clonedeep");
 
-c_button.c_button_classes += "  lrv-u-display-inline-flex";
+const c_button_protoype = require("./c-button.prototype");
+const c_button = clonedeep(c_button_protoype);
+
+c_button.c_button_classes += ' lrv-u-display-inline-flex';
 
 module.exports = c_button;
