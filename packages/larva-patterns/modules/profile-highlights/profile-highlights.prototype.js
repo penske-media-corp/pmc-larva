4c294fac86c7f522f0f3fa66a0718444f5703283
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-12 18:19:16 +0530

    Update profile-highlights module standards

--- packages/larva-patterns/modules/profile-highlights/profile-highlights.prototype.js
+++ packages/larva-patterns/modules/profile-highlights/profile-highlights.prototype.js
@@ -1,10 +1,7 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
-const profile_blurb_prototype = require( '../profile-blurb/profile-blurb.prototype' );
-const profile_blurb = clonedeep( profile_blurb_prototype );
-
-const c_heading_prototype = require( '../../components/c-heading/c-heading.prototype' );
-const c_heading = clonedeep( c_heading_prototype );
+const profile_blurb = clonedeep( require( '../profile-blurb/profile-blurb.prototype' ) );
+const c_heading = clonedeep( require( '../../components/c-heading/c-heading.prototype' ) );
 
 c_heading.c_heading_classes = 'lrv-u-text-align-center lrv-u-font-size-36 lrv-u-font-size-46@desktop-xl lrv-u-font-family-secondary lrv-u-padding-b-2';
 c_heading.c_heading_text = 'Replace This Text';
@@ -21,4 +18,4 @@ module.exports = {
 		profile_blurb,
 		profile_blurb,
 	]
-}
+};
