6f2416cd3bc77631d280c76891fa15e39df60836
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-12 18:39:18 +0530

    Update o-collection-item objects prototypes standards

--- packages/larva-patterns/objects/o-collection-item/o-collection-item.floated.js
+++ packages/larva-patterns/objects/o-collection-item/o-collection-item.floated.js
@@ -1,10 +1,8 @@
-const path = require( 'path' );
 const clonedeep = require( 'lodash.clonedeep' );
 
-const o_collection_item_prototype = require( './o-collection-item.prototype' );
-const o_collection_item = clonedeep( o_collection_item_prototype );
+const o_collection_item = clonedeep( require( './o-collection-item.prototype' ) );
 
-o_collection_item.o_collection_item_classes += " lrv-a-floated-parent";
-o_collection_item.o_figure.o_figure_classes += " lrv-a-floated-left";
+o_collection_item.o_collection_item_classes += ' lrv-a-floated-parent';
+o_collection_item.o_figure.o_figure_classes += ' lrv-a-floated-left';
 
 module.exports = o_collection_item;
--- packages/larva-patterns/objects/o-collection-item/o-collection-item.prototype.js
+++ packages/larva-patterns/objects/o-collection-item/o-collection-item.prototype.js
@@ -1,14 +1,8 @@
-const path = require( 'path' );
 const clonedeep = require( 'lodash.clonedeep' );
 
-const c_title_prototype = require( '../../components/c-title/c-title.prototype' );
-const c_title = clonedeep( c_title_prototype );
-
-const o_figure_prototype = require( '../o-figure/o-figure.prototype' );
-const o_figure = clonedeep( o_figure_prototype );
-
-const c_dek_prototype = require( '../../components/c-dek/c-dek.prototype' );
-const c_dek = clonedeep( c_dek_prototype );
+const c_title = clonedeep( require( '../../components/c-title/c-title.prototype' ) );
+const o_figure = clonedeep( require( '../o-figure/o-figure.prototype' ) );
+const c_dek = clonedeep( require( '../../components/c-dek/c-dek.prototype' ) );
 
 module.exports = {
 	c_title: c_title,
