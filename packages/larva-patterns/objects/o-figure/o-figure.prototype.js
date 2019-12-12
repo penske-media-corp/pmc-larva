6271e0cc3f27d8d35e2a1ffe1b7eed3935232231
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-12 18:40:10 +0530

    Update o-figure object prototypes standards

--- packages/larva-patterns/objects/o-figure/o-figure.prototype.js
+++ packages/larva-patterns/objects/o-figure/o-figure.prototype.js
@@ -1,10 +1,7 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
-const c_figcaption_prototype = require( '../../components/c-figcaption/c-figcaption.prototype' );
-const c_figcaption = clonedeep( c_figcaption_prototype );
-
-const c_lazy_image_prototype = require( '../../components/c-lazy-image/c-lazy-image.prototype' );
-const c_lazy_image = clonedeep( c_lazy_image_prototype );
+const c_figcaption = clonedeep( require( '../../components/c-figcaption/c-figcaption.prototype' ) );
+const c_lazy_image = clonedeep( require( '../../components/c-lazy-image/c-lazy-image.prototype' ) );
 
 module.exports = {
 	c_figcaption: c_figcaption,
@@ -14,4 +11,4 @@ module.exports = {
 	o_figure_link_classes: 'lrv-a-unstyle-link ',
 	o_figure_figcaption_outer: false,
 	o_figure_figcaption_outer_classes: '',
-}
+};
