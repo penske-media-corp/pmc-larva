c4caa40b849d771eb22cfc3b97e82f108bf4942d
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-12 18:18:31 +0530

    Update listicle-item module standards

--- packages/larva-patterns/modules/listicle-item/listicle-item.prototype.js
+++ packages/larva-patterns/modules/listicle-item/listicle-item.prototype.js
@@ -1,11 +1,9 @@
-const path = require( 'path' );
 const clonedeep = require( 'lodash.clonedeep' );
 
-const o_collection_item_prototype = require( '../../objects/o-collection-item/o-collection-item.prototype' );
-const o_collection_item = clonedeep( o_collection_item_prototype );
+const o_collection_item = clonedeep( require( '../../objects/o-collection-item/o-collection-item.prototype' ) );
 
-o_collection_item.o_collection_item_classes = "a-floated-parent";
-o_collection_item.o_figure.o_figure_classes = "a-floated-left@tablet lrv-u-padding-r-1@tablet";
+o_collection_item.o_collection_item_classes = 'a-floated-parent';
+o_collection_item.o_figure.o_figure_classes = 'a-floated-left@tablet lrv-u-padding-r-1@tablet';
 
 module.exports = {
 	listicle_item_classes: "lrv-a-wrapper",
