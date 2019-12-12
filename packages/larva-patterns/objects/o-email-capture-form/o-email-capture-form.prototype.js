372d3074bf410b8857edb705d620b2a9efc89e1f
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-12 18:39:57 +0530

    Update o-email-capture-form objects prototypes standards

--- packages/larva-patterns/objects/o-email-capture-form/o-email-capture-form.prototype.js
+++ packages/larva-patterns/objects/o-email-capture-form/o-email-capture-form.prototype.js
@@ -1,11 +1,7 @@
-const path = require( 'path' );
 const clonedeep = require( 'lodash.clonedeep' );
 
-const c_button_prototype = require( '../../components/c-button/c-button.prototype' );
-const c_button = clonedeep( c_button_prototype );
-
-const c_email_field_prototype = require( '../../components/c-email-field/c-email-field.prototype' );
-const c_email_field = clonedeep( c_email_field_prototype );
+const c_button = clonedeep( require( '../../components/c-button/c-button.prototype' ) );
+const c_email_field = clonedeep( require( '../../components/c-email-field/c-email-field.prototype' ) );
 
 module.exports = {
 	o_email_capture_form_classes: '',
