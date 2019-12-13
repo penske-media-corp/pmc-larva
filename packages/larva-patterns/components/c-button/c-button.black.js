ec3f61d3671bc13bbca744ec5822cab23d660134
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-14 00:16:47 +0530

    Remove extra utilities from u-button-black

--- packages/larva-patterns/components/c-button/c-button.black.js
+++ packages/larva-patterns/components/c-button/c-button.black.js
@@ -2,6 +2,6 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const c_button = clonedeep( require( './c-button.prototype' ) );
 
-c_button.c_button_classes = 'lrv-a-unstyle-button lrv-u-cursor-pointer lrv-u-padding-tb-050 lrv-u-padding-lr-2 lrv-u-background-color-black lrv-u-color-white lrv-u-border-radius-5 lrv-u-color-white:hover lrv-u-font-family-primary lrv-u-font-weight-bold lrv-u-font-size-20 u-box-shadow-dark ';
+c_button.c_button_classes += ' lrv-u-background-color-black lrv-u-color-white lrv-u-border-radius-5 lrv-u-color-white:hover';
 
 module.exports = c_button;
