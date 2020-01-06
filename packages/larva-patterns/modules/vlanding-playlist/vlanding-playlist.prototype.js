390b7ce17e2bfb518b4049fd6c17d5caba7332e7
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2020-01-06 20:37:25 +0530

    PMCP-1735: feat: Add vlanding-playlist

---
+++ packages/larva-patterns/modules/vlanding-playlist/vlanding-playlist.prototype.js
@@ -0,0 +1,34 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const o_header = clonedeep ( require( '../../objects/o-header/o-header.prototype' ) );
+const o_card = clonedeep ( require( '../../objects/o-card/o-card.prototype' ) );
+
+o_header.o_heading_classes = 'lrv-u-flex lrv-u-align-items-center lrv-u-border-b-1 lrv-u-border-color-grey-light lrv-u-margin-b-1';
+o_header.c_heading.c_heading_text = 'Behind the Lens';
+o_header.c_heading.c_heading_classes = 'lrv-u-font-size-32';
+
+o_header.c_button.c_button_classes += ' lrv-u-font-weight-bold';
+
+o_card.c_span = false;
+o_card.c_timestamp = false;
+o_card.o_card_link_url = '#';
+o_card.o_card_link_classes = 'lrv-a-unstyle-link';
+o_card.c_lazy_image.c_lazy_image_classes = 'lrv-u-margin-b-050';
+o_card.c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-4x3';
+o_card.c_title.c_title_url = false;
+
+const o_large_card = clonedeep ( o_card );
+
+o_card.c_title.c_title_classes = 'lrv-u-font-size-12';
+o_large_card.c_title.c_title_classes = 'lrv-u-font-size-20@desktop lrv-u-font-size-12';
+
+module.exports = {
+	o_header: o_header,
+	"large_card": o_large_card,
+	"small_card": [
+		o_card,
+		o_card,
+		o_card,
+		o_card
+	]
+};
--- packages/larva-patterns/objects/o-header/o-header.prototype.js
+++ packages/larva-patterns/objects/o-header/o-header.prototype.js
@@ -6,7 +6,7 @@ const c_heading = clonedeep( c_heading_prototype );
 const c_button_prototype = require( '../../components/c-button/c-button.prototype' );
 const c_button = clonedeep( c_button_prototype );
 
-c_button.c_button_classes = 'c-button--plain lrv-u-font-size-12 lrv-u-text-transform-uppercase lrv-a-icon-after lrv-a-icon-arrow-right lrv-u-margin-l-auto';
+c_button.c_button_classes = 'lrv-u-font-size-12 lrv-u-text-transform-uppercase lrv-a-icon-after lrv-a-icon-arrow-right lrv-u-margin-l-auto';
 c_button.c_button_text = 'Link';
 c_button.c_button_url = '#';
 
