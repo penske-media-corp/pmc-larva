5596c6245ba0483dbaa878152de69ad627c64d9f
laras126 <laras126@gmail.com>
2020-01-06 13:53:20 -0500

    [larva-scripts] feat: add eslint

---
+++ packages/larva/scripts/lint-js.js
@@ -0,0 +1,12 @@
+const path = require( 'path' );
+
+const eslintConfigPath = path.join( __dirname, './config/.eslintrc.json' );
+const spawnScript = require( '../lib/utils/spawnScript' );
+
+const args = [
+	'--c',
+	eslintConfigPath,
+	'./'
+];
+
+spawnScript( './node_modules/.bin/eslint', args );
