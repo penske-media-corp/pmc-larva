3514d3e42f1cba4f4251bb6e31bf1591311eb4c4
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2020-01-06 15:34:51 +0530

    PMCP-1735: chore: Use vlanding-video-card n big video module

--- packages/larva-patterns/modules/vlanding-big-video/vlanding-big-video.prototype.js
+++ packages/larva-patterns/modules/vlanding-big-video/vlanding-big-video.prototype.js
@@ -1,7 +1,10 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
+const vlanding_video_card = clonedeep( require( '../../modules/vlanding-video-card/vlanding-video-card.prototype' ) );
 const c_title = clonedeep( require( '../../components/c-title/c-title.prototype' ) );
 const c_tagline = clonedeep( require( '../../components/c-tagline/c-tagline.prototype' ) );
+
+vlanding_video_card.c_title = false;
 
 c_title.c_title_classes = 'lrv-u-font-size-24 lrv-u-font-size-32@desktop lrv-u-padding-b-1';
 c_title.c_title_link_classes = 'lrv-u-color-black';
@@ -11,21 +14,7 @@ c_tagline.c_tagline_classes = 'lrv-u-color-grey-dark lrv-u-margin-t-00';
 
 // TODO: Add social share module and o_video_card.
 module.exports = {
+	vlanding_video_card: vlanding_video_card,
 	c_title: c_title,
 	c_tagline: c_tagline,
-	"o_video_card": {
-		"o_video_card_is_player": true,
-		"o_video_card_link_data_attr": "data-video-showcase-trigger='rs2RlZQVXBU'",
-		"o_video_card_classes": "a-glue-parent c-play-badge-parent",
-		"o_video_card_permalink_url": "#",
-		"o_video_card_alt_attr": "Thumbnail image",
-		"o_video_card_image_url": "https://picsum.photos/300/300",
-		"o_video_card_image_classes": "is-to-be-hidden",
-		"o_video_card_crop_class": "pmc-u-crop-9x16",
-		"c_play_badge": {
-			"modifier_class": "",
-			"c_play_badge_classes": "a-glue a-glue--bl-0 pmc-u-margin-l-1 pmc-u-margin-l-1 pmc-u-margin-b-1 is-to-be-hidden"
-		},
-		"c_title": ""
-	},
 };
