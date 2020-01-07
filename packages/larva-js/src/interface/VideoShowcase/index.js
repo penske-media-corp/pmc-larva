3dcaa839b6ce5c859405cdfac8e015f0dc92170e
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2020-01-07 18:11:07 +0530

    PMCP-1745: chore: Add videoshowcase interface from deadline

---
+++ packages/larva-js/src/interface/VideoShowcase/index.js
@@ -0,0 +1,10 @@
+import './style.scss';
+import VideoShowcase from './VideoShowcase';
+
+export default function initVideoShowcase() {
+	const videoShowcases = [ ... document.querySelectorAll( '[data-video-showcase]' ) ];
+
+	if ( videoShowcases.length ) {
+		videoShowcases.forEach( el => el.pmcVideoShowcase = new VideoShowcase( el ) );
+	}
+}
