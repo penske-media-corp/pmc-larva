97eeef1d7043918e945e9e14d00d4178ef827505
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2020-01-07 14:41:13 +0530

    PMCP-1737: chore: Add more utilities

--- packages/larva-patterns/modules/social-share/social-share.prototype.js
+++ packages/larva-patterns/modules/social-share/social-share.prototype.js
@@ -1,67 +1,67 @@
 module.exports = {
 	social_share_classes: '',
 	social_share_prefix: false,
-	social_share_prefix_classes: 'u-display-none@mobile-max lrv-u-color-grey lrv-u-font-size-10 lrv-u-text-transform-uppercase lrv-u-margin-r-1@tablet lrv-u-margin-b-050@mobile-max',
+	social_share_prefix_classes: 'lrv-u-display-none@mobile-max lrv-u-color-grey lrv-u-font-size-10 lrv-u-text-transform-uppercase lrv-u-margin-r-1@tablet lrv-u-margin-b-050@mobile-max',
 	social_share_prefix_text: 'Share',
 	social_share_items_classes: 'lrv-a-space-children--1 lrv-a-space-children-horizontal',
 	primary: [
 		{
-			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex",
+			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex",
 			"c_icon_url": "#",
 			"c_icon_rel_name": "facebook",
 			"c_icon_name": "facebook",
-			"c_icon_classes": "u-width-16 u-height-16"
+			"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 		},
 		{
-			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex",
+			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex",
 			"c_icon_url": "#",
 			"c_icon_rel_name": "twitter",
 			"c_icon_name": "twitter",
-			"c_icon_classes": "u-width-16 u-height-16"
+			"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 		},
 		{
-			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex",
+			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex",
 			"c_icon_url": "#",
 			"c_icon_rel_name": "pinterest",
 			"c_icon_name": "pinterest",
-			"c_icon_classes": "u-width-16 u-height-16"
+			"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 		},
 		{
-			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex",
+			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex",
 			"c_icon_url": "#",
 			"c_icon_rel_name": "tumblr",
 			"c_icon_name": "tumblr",
-			"c_icon_classes": "u-width-16 u-height-16"
+			"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 		}
 	],
 	secondary: [
 		{
-			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex",
+			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex",
 			"c_icon_url": "#",
 			"c_icon_rel_name": "reddit",
 			"c_icon_name": "reddit",
-			"c_icon_classes": "u-width-16 u-height-16"
+			"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 		},
 		{
-			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex",
+			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex",
 			"c_icon_url": "#",
 			"c_icon_rel_name": "linkedin",
 			"c_icon_name": "linkedin",
-			"c_icon_classes": "u-width-16 u-height-16"
+			"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 		},
 		{
-			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex js-PrintTrigger",
+			"c_icon_link_classes": "lrv-a-unstyle-link lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex js-PrintTrigger",
 			"c_icon_url": "#",
 			"c_icon_rel_name": "print",
 			"c_icon_name": "print",
-			"c_icon_classes": "u-width-16 u-height-16"
+			"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 		},
 	],
 	plus_icon: {
-		"c_icon_link_classes": "lrv-u-border-color-grey-light lrv-u-display-block u-display-inline-flex u-color-black",
+		"c_icon_link_classes": "lrv-u-border-color-grey-light lrv-u-display-block lrv-u-display-inline-flex u-color-black",
 		"c_icon_url": "#",
 		"c_icon_rel_name": "plus",
 		"c_icon_name": "plus",
-		"c_icon_classes": "u-width-16 u-height-16"
+		"c_icon_classes": "lrv-u-width-16 lrv-u-height-16"
 	}
 };
