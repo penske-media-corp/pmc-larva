1aff9524608645ec1c5d289992f4d2ca56242c3b
laras126 <laras126@gmail.com>
2020-01-17 13:59:27 -0500

    [larva-ui] chore: add listener

--- packages/larva/entries/common.entry.js
+++ packages/larva/entries/common.entry.js
@@ -3,10 +3,10 @@ import initFlickity from '@larva-js/interface/Flickity';
 import initVideoShowcase from '@larva-js/interface/VideoShowcase';
 import initCollapsible from '@larva-js/interface/Collapsible';
 
-
 ajaxIconSprite( '/assets/build/svg/defs/sprite.defs.svg' );
 
-initCollapsible();
-initFlickity();
-initVideoShowcase();
-
+document.addEventListener( 'DOMContentLoaded', () => {
+	initCollapsible();
+	initFlickity();
+	initVideoShowcase();
+} );
