e8602aaf0965f187d3a8aae8edec3ca7995a82fa
laras126 <laras126@gmail.com>
2020-01-17 13:56:21 -0500

    [larva-ui] fix: init collapsible and move over css

--- packages/larva/entries/common.entry.js
+++ packages/larva/entries/common.entry.js
@@ -1,11 +1,12 @@
 import ajaxIconSprite from '@larva-js/utils/ajaxIconSprite';
 import initFlickity from '@larva-js/interface/Flickity';
 import initVideoShowcase from '@larva-js/interface/VideoShowcase';
+import initCollapsible from '@larva-js/interface/Collapsible';
 
 
 ajaxIconSprite( '/assets/build/svg/defs/sprite.defs.svg' );
 
+initCollapsible();
 initFlickity();
 initVideoShowcase();
 
-console.log( 'hi' );
