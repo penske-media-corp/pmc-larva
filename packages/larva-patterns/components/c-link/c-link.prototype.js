c2d3e611e7aebc039d9ed3d8e0e623bfb1e198cb
Mike Crantea <mike.crantea@xwp.co>
2020-02-18 14:00:15 +0200

    Add support for target=“_blank” on the link component

--- packages/larva-patterns/components/c-link/c-link.prototype.js
+++ packages/larva-patterns/components/c-link/c-link.prototype.js
@@ -2,5 +2,6 @@ module.exports = {
 	modifier_class: '',
 	c_link_classes: 'lrv-a-unstyle-link',
 	c_link_text: 'A plain text link',
-	c_link_url: '#'
+	c_link_url: '#',
+	c_link_is_external: false,
 };
