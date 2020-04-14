fc4e0ad2e4aa699465fa753c0fae62f37726dc4b
laras126 <laras126@gmail.com>
2020-04-14 14:54:06 -0400

    [ls-static-site] refact: rename, add test file

---
+++ packages/larva/__tests__/lib/generateStatic.test.js
@@ -0,0 +1,13 @@
+const path = require( 'path' );
+const fixture = path.join( __dirname, '../../fixtures' );
+
+const getAllPatternsObj = require( '../../../lib/utils/getAllPatternsObj' );
+const getPatternRoutes = require( '../../../lib/utils/getPatternRoutes' );
+
+describe( 'generateStatic', () => {
+
+	it( 'creates a build-html directory', () => {
+
+	} );
+
+} );
