947d79631a5f8b954d93ada70d8ef4727b4b8088
laras126 <laras126@gmail.com>
2020-04-14 15:40:19 -0400

    [ls-static-site] test: test for creating index file

--- packages/larva/__tests__/lib/generateStatic.test.js
+++ packages/larva/__tests__/lib/generateStatic.test.js
@@ -1,13 +1,59 @@
 const path = require( 'path' );
-const fixture = path.join( __dirname, '../../fixtures' );
+const fs = require( 'fs' );
+const mkdirp = require( 'mkdirp' );
+const exec = require( 'child_process' ).exec;
 
-const getAllPatternsObj = require( '../../../lib/utils/getAllPatternsObj' );
-const getPatternRoutes = require( '../../../lib/utils/getPatternRoutes' );
+const fixture = path.join( __dirname, '../fixtures' );
+
+const getAllPatternsObj = require( '../../lib/utils/getAllPatternsObj' );
+const getPatternRoutes = require( '../../lib/utils/getPatternRoutes' );
+
+const buildPath = path.join( fixture, './build-html' );
+
+function generateStatic ( directory ) {
+	const patternsObj = getAllPatternsObj( directory );
+
+	// navigate to path in server
+	// prepare the file extension
+	// write the output to a file
+
+	let dir = path.join( buildPath, 'components/c-nav-link/' );
+
+	// patternsObj.map( ( pattern ) => {
+
+	// } );
+
+	mkdirp.sync( dir );
+
+	fs.writeFileSync( dir + '/index.html', 'asdasd' );
+}
 
 describe( 'generateStatic', () => {
 
-	it( 'creates a build-html directory', () => {
+	beforeAll( ( done ) => {
+
+		exec( 'mkdir -p ' + buildPath, ( err ) => {
+			if ( err ) {
+				console.error( err );
+				done();
+			} else {
+				generateStatic( path.join( fixture, './src/patterns' ) );
+				done();
+			}
+		} );
 
 	} );
 
+	it( 'creates an index.html file for a prototype pattern', () => {
+		expect( fs.existsSync( path.join( buildPath, 'components/c-nav-link/index.html' ) ) ).toBe( true );
+	} );
+
+	afterAll( ( done ) => {
+		exec( 'rm -r ' + buildPath, ( err ) => {
+			if ( err ) {
+				console.error( err );
+				done();
+			}
+		} );
+	} );
 } );
