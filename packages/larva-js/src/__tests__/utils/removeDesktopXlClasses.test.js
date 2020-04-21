551fc2f781895853472c61fb8712454e8352439d
laras126 <laras126@gmail.com>
2020-04-21 10:23:34 -0400

    [variety-updates] feat: add skin tests and readme

---
+++ packages/larva-js/src/__tests__/utils/removeDesktopXlClasses.test.js
@@ -0,0 +1,28 @@
+const markup = require( '../fixtures/markup' );
+
+import removeDesktopXlClasses from '../../utils/removeDesktopXlClasses';
+
+function setup() {
+	document.querySelector( '.test' ).innerHTML = markup.markupWithXlClasses;
+	removeDesktopXlClasses();
+}
+
+describe( 'removeDesktopXlClasses', () => {
+	setup();
+
+	it( 'rebuilds the class name without @desktop-xl classes', () => {
+
+		const el = document.querySelector( '.module' );
+
+		expect( el.className ).toBe( ' module // u-margin-t-125 lrv-a-wrapper lrv-a-grid u-grid-gap-0' );
+
+	} );
+
+	it( 'removes all @desktop-xl classes', () => {
+		const desktopXlEls = [...document.querySelectorAll( '[class*="@desktop-xl"]' )];
+
+		expect( desktopXlEls.length ).toBe( 0 );
+	} );
+
+	// TODO: add teardown() if more test cases are added
+} );
---
+++ packages/larva-js/src/interface/SideSkinAd/README.md
@@ -0,0 +1,11 @@
+# Side Skins Ad JS
+
+To use this JS module in a consuming theme that uses skin ads, add the following to the common entry point:
+
+```
+import initSideSkinAd from '../src/js/interface/SideSkinAd';
+
+window.addEventListener( 'message', ( e ) => {
+	initSideSkinAd( e );
+} );
+```
