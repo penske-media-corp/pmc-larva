24bd95ff4cd46c1ca19855ba4e82175d67009da7
laras126 <laras126@gmail.com>
2020-04-24 11:09:08 -0400

    [ls-static-site] test: passing!

--- packages/larva/__tests__/lib/generateStatic.test.js
+++ packages/larva/__tests__/lib/generateStatic.test.js
@@ -35,34 +35,37 @@ describe( 'generateStatic', () => {
 
 	} );
 
-	// it( 'creates an html file for a pattern variant', () => {
-	// 	const routesArr = [
-	// 		'components/c-button/brand-basic'
-	// 	];
+	it( 'creates an html file for a pattern variant', ( done ) => {
+		const routesArr = [
+			'components/c-button/brand-basic'
+		];
 
-	// 	generateStatic( routesArr, buildPath );
+		generateStatic( routesArr, buildPath, () => {
+			expect(
+				fs.existsSync( path.join( buildPath, 'components/c-button/brand-basic/index.html' ) )
+			).toBe( true );
+			done();
+		} );
 
-	// 	expect(
-	// 		fs.existsSync( path.join( buildPath, 'components/c-button/brand-basic/index.html' ) )
-	// 	).toBe( true );
-	// } );
+	} );
 
-	// it( 'writes the pattern html to the file', () => {
-	// 	const routesArr = [
-	// 		'components/c-button/ghost',
-	// 		'components/c-button',
-	// 		'modules/footer',
-	// 	];
+	it( 'contains content for the pattern', ( done ) => {
+		const routesArr = [
+			'modules/footer',
+		];
 
-	// 	generateStatic( routesArr, buildPath );
-
-	// 	expect(
-	// 		fs.readFileSync(
-	// 			path.join( buildPath, routesArr[0] + '/index.html' ),
-	// 			{ encoding: 'utf8' }
-	// 		)
-	// 	).toBe( expect.stringContaining( 'class="c-nav-link' ));
-	// } );
+		generateStatic( routesArr, buildPath, () => {
+			expect(
+				// Hacky way to test that the response actually contains
+				// the content, not an error.
+				fs.readFileSync(
+					path.join( buildPath, routesArr[0] + '/index.html' ),
+					{ encoding: 'utf8' }
+				).indexOf( 'footer_classes' )
+			).not.toBe( -1 );
+			done();
+		} );
+	} );
 
 	afterEach( ( done ) => {
 		exec( 'rm -r ' + buildPath, ( err ) => {
