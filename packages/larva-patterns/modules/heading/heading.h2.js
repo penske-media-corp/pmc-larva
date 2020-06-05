dbd5e08f484bcd6d4254e39840d3f38ef4b2e29c
laras126 <laras126@gmail.com>
2020-06-05 14:44:25 -0400

    [GUT-60] refact: update defaults

--- packages/larva-patterns/modules/heading/heading.h2.js
+++ packages/larva-patterns/modules/heading/heading.h2.js
@@ -2,7 +2,7 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const heading = clonedeep( require( './heading.prototype' ) );
 
-heading.heading_classes = 'lrv-u-text-align-center a-font-accent-l';
+heading.heading_classes = 'a-font-accent-l';
 heading.heading_level_text = '2';
 
 module.exports = heading;
--- packages/larva-patterns/modules/heading/heading.h3.js
+++ packages/larva-patterns/modules/heading/heading.h3.js
@@ -2,7 +2,7 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const heading = clonedeep( require( './heading.prototype' ) );
 
-heading.heading_classes = 'lrv-u-text-align-center a-font-secondary-l lrv-u-color-brand-primary';
+heading.heading_classes = 'a-font-secondary-l lrv-u-color-brand-primary';
 heading.heading_level_text = '3';
 
 module.exports = heading;
