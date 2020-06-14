7ffe03c2c625f4d207f82e432970acba094b2be7
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2020-06-14 10:50:33 -0400

    add list module (#236)
    * add list module
    
    * use BEM naming

---
+++ packages/larva-patterns/modules/list/list.prototype.js
@@ -0,0 +1,7 @@
+module.exports = {
+	list_classes: 'a-font-body-m',
+	list_markup: 'Paragraph Text.',
+	list_type: 'u',
+	list_items: [ 'item_one', 'item_two', 'item_list'],
+	list_item_classes: ''
+};
