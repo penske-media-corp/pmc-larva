f6f0e167c1a0f6df2a87293a79e0abd831838e6d
laras126 <laras126@gmail.com>
2020-07-01 13:59:55 -0400

    [ls-build-html] fix: add try catch

--- packages/larva/lib/utils/getPatternDataPath.js
+++ packages/larva/lib/utils/getPatternDataPath.js
@@ -3,17 +3,16 @@ const chalk = require( 'chalk' );
 const path = require( 'path' );
 
 module.exports = function getPatternDataPath( patternsPath, params ) {
-	let ext = params.variant ? params.variant : 'prototype';
-
-	const protoPath = path.join( patternsPath, params.type, params.name, params.name + '.' + ext + '.js' );
+	const protoPath = path.join( patternsPath, params.type, params.name, params.name + '.' + params.variant + '.js' );
 	const jsonPath = path.join( patternsPath, params.type, params.name, params.name + '.json' )
 
-	if ( fs.existsSync( protoPath ) ) {
-		return protoPath;
-	} else if ( fs.existsSync( jsonPath ) ) {
-		return jsonPath;
+	try {
+		if ( fs.existsSync( protoPath ) ) {
+			return protoPath;
+		} else if ( fs.existsSync( jsonPath ) ) {
+			return jsonPath;
+		}
+	} catch( e ) {
+		return e;
 	}
-
-	console.log( chalk.yellow( `Couldn\'t find data file for ${params.name}.\nLooked in ${patternsPath}.` ) );
-	return undefined;
 }
