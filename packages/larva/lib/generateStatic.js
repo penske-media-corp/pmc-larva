3c8bf5d2f8a636354272fd44a652316a8e9de042
laras126 <laras126@gmail.com>
2020-07-07 13:06:39 -0400

    [ls-build-html] refact: put site gen fn in closure

--- packages/larva/lib/generateStatic.js
+++ packages/larva/lib/generateStatic.js
@@ -21,13 +21,69 @@ const axios = require( 'axios' );
  * @see {@link getPatternRoutes}.
  */
 
+
 module.exports = function generateStatic( routesArr, buildPath, done, urlBase = 'http://localhost:3000/larva' ) {
 	const errors = [];
 
+	const siteBuilder = () => {
+		try {
+			
+			const promises = routesArr.map( ( route ) => {
+
+				const dir = path.join( buildPath, route );
+				const url = `${urlBase}/${route}`;
+
+				return axios.get( url ).then( ( response ) => {
+
+					if ( 200 === response.status ) {
+						mkdirp.sync( dir );
+						fs.writeFileSync( `${dir}/index.html`, response.data );
+						console.log( `Built ${route}.` );
+					}
+
+					if ( 500 === response.status ) {
+						errors.push( url );
+					}
+
+				} ).catch( ( e ) => {
+
+					if ( 'ECONNREFUSED' === e.code ) {
+						process.exitCode = 1;
+					} else {
+						mkdirp.sync( dir );
+						fs.writeFileSync( `${dir}/index.html`, e.message );
+					}
+
+				});
+
+			} );
+
+			axios.all( promises ).then( () => {
+				
+				if ( errors.length > 0 ) {
+					console.log( errors );
+				}
+
+				done( chalk.green( `Successfully build static site to ${buildPath}` ) );			
+
+			} ).catch( ( e ) =>  {
+				
+				done( chalk.bold.red( e ) );
+			
+			});
+
+		} catch ( e ) {
+			console.error( e );
+		}
+	};
+
 	// TODO: must update to always read from static larva UI JS
-	const assetsDest = path.join( buildPath, './assets' );
+	const assetsDest = path.join( buildPath, './assets/build/js' );
 	const assetsBuildSrc = path.join( buildPath, '../js' );
 
+	console.log( assetsBuildSrc );
+	console.log( assetsDest );
+	
 	// get subdirectories - i dont even know rifhr noqaS
 	// const assetsDest = path.join( buildPath, './assets' );
 	// const assetsBuildSrc = path.join( buildPath, '../js' );
@@ -42,56 +98,8 @@ module.exports = function generateStatic( routesArr, buildPath, done, urlBase = 
 			}
 
 			console.log( `Copied assets.` );
+			siteBuilder();
 		}
 	);
 
-	try {
-		
-		const promises = routesArr.map( ( route ) => {
-
-			const dir = path.join( buildPath, route );
-			const url = `${urlBase}/${route}`;
-
-			return axios.get( url ).then( ( response ) => {
-
-				if ( 200 === response.status ) {
-					mkdirp.sync( dir );
-					fs.writeFileSync( `${dir}/index.html`, response.data );
-					console.log( `Built ${route}.` );
-				}
-
-				if ( 500 === response.status ) {
-					errors.push( url );
-				}
-
-			} ).catch( ( e ) => {
-
-				if ( 'ECONNREFUSED' === e.code ) {
-					process.exitCode = 1;
-				} else {
-					mkdirp.sync( dir );
-					fs.writeFileSync( `${dir}/index.html`, e.message );
-				}
-
-			});
-
-		} );
-
-		axios.all( promises ).then( () => {
-			
-			if ( errors.length > 0 ) {
-				console.log( errors );
-			}
-
-			done( chalk.green( `Successfully build static site to ${buildPath}` ) );			
-
-		} ).catch( ( e ) =>  {
-			
-			done( chalk.bold.red( e ) );
-		
-		});
-
-	} catch ( e ) {
-		console.error( e );
-	}
 }
