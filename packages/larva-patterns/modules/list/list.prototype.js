44b25aa993210f793db05e6c1411766ce80829b0
laras126 <laras126@gmail.com>
2020-08-19 15:47:14 -0400

    [GUT-77] feat: recursive list

--- packages/larva-patterns/modules/list/list.prototype.js
+++ packages/larva-patterns/modules/list/list.prototype.js
@@ -1,35 +1,50 @@
 module.exports = {
 	list_classes: 'a-font-body-m',
 	list_markup: 'Paragraph Text.',
-	list_type: 'u',
+	list_type_name: 'u',
 	list_items: [ 
-		'item_one', 
-		'item_two', 
-		'item_list',
+		{
+			list_item_markup: 'item_one', 
+		},
+		{
+			list_item_markup: 'item_two', 
+		},
 		{
 			list_classes: 'a-font-body-m',
 			list_markup: 'Paragraph Text.',
-			list_type: 'u',
+			list_type_name: 'u',
 			list_item_classes: '',
 			list_items: [
-				'sub_item_one',
-				'sub_item_two',
+				{
+					list_item_markup: 'sub_item_one', 
+				},
+				{
+					list_item_markup: 'sub_item_two', 
+				},
 				{
 					list_classes: 'a-font-body-m',
 					list_markup: 'Paragraph Text.',
-					list_type: 'u',
+					list_type_name: 'u',
 					list_item_classes: '',
 					list_items: [
-						'sub_sub_item_one',
-						'sub_sub_item_two',
+						{
+							list_item_markup: 'sub_sub_item_one', 
+						},
+						{
+							list_item_markup: 'sub_sub_item_two', 
+						},
 						{
 							list_classes: 'a-font-body-m',
 							list_markup: 'Paragraph Text.',
-							list_type: 'u',
+							list_type_name: 'u',
 							list_item_classes: '',
 							list_items: [
-								'sub_sub_sub_item_one',
-								'sub_sub_sub_item_two'
+								{
+									list_item_markup: 'sub_sub_sub_item_one', 
+								},
+								{
+									list_item_markup: 'sub_sub_sub_item_two', 
+								},
 							]
 						}
 						
