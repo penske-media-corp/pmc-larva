61dd406aa2aafb11ab7cbc69aa437ab77036231a
laras126 <laras126@gmail.com>
2020-08-20 09:28:01 -0400

    [GUT-77] feat: support array syntax in parser

--- packages/twig-to-php-parser/lib/twig-to-php-parser.php
+++ packages/twig-to-php-parser/lib/twig-to-php-parser.php
@@ -63,9 +63,9 @@ function twig_to_php_parser( $patterns_dir_path, $template_dir_path, $is_using_p
 		// https://regex101.com/r/lceCnz/2/
 		$loop_regex = '/({% for item in\s*)(\w*)/';
 
-		// Get matches for {{ name }}
-		// https://regex101.com/r/ACN0rE/2
-		$mustache_regex = '/({{\s*)(\w*?\|?\w*)(\s*}})/';
+		// Get matches for {{ name }}, {{ name|filter }}, {{ name['item'] }}
+		// https://regex101.com/r/ACN0rE/3
+		$mustache_regex = '/({{\s*)(\w*?\[?\'?\w*\'?\]?\|?\w*)(\s*}})/';
 
 		// Get matches for {% include "path/c-element.twig" with data %}
 		// https://regex101.com/r/ns5kBR/2
