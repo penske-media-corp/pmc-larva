e263cbb1748cd247c8a5e5d7e5b0cc8c07db6185
Dhaval Parekh <dmparekh007@gmail.com>
2020-10-16 19:35:25 +0530

    Add option for tag and id attribute for c-title components

--- packages/larva-patterns/components/c-title/c-title.prototype.js
+++ packages/larva-patterns/components/c-title/c-title.prototype.js
@@ -1,5 +1,7 @@
 module.exports = {
 	c_title_classes: '',
+	c_title_tag: '',
+	c_title_id_attr: 'title-of-a-story',
 	c_title_text: 'Title of a story',
 	c_title_markup: '',
 	c_title_link_classes: '',
