085f2414cfd77e4777969a6de7e61b672a6ef2f9
Sarah Smolen <ssmolen@pmc.com>
2020-11-11 17:31:48 -0500

    Create simple app

---
+++ packages/larva-designer-form/src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Form } from "./Form";
+
+export const App = () => (
+	<div className="App">
+		<h1>Update Larva Design Tokens</h1>
+		<Form />
+	</div>
+);
---
+++ packages/larva-designer-form/src/tokens.js
@@ -0,0 +1,23 @@
+export const baseTokens = [
+	"aliases",
+	"background-color",
+	"border-color",
+	"color",
+	"font-family",
+	"font-weight",
+	"line-height",
+	"spacing",
+];
+export const brandTokens = [
+	"artinamerica",
+	"artnews",
+	"deadline",
+	"dirt",
+	"indiewire",
+	"rollingstone",
+	"sheknows",
+	"sportico",
+	"variety-vip",
+	"variety",
+	"wwd",
+];
