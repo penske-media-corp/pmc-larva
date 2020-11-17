8d24c29a1af9184bc0f7113697e1772f86599150
laras126 <laras126@gmail.com>
2020-11-17 14:27:10 -0500

    feat: add stub for fetch

--- packages/larva-designer-form/src/components/UpdateToken.js
+++ packages/larva-designer-form/src/components/UpdateToken.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
-export const UpdateToken = (props) => (
-	<div>{`Update ${props.tokenType} token: ${props.selectedToken}`}</div>
-);
+export const UpdateToken = (props) => {
+
+	const [tokens, setTokens] = useState();
+
+	( async () => {
+		let url = 'https://gist.githubusercontent.com/laras126/20e75684dcbd65b4dc3fe84403a1243c/raw/a14143306c76c77cbe8771088429283c24a9c1d5/artnews.json';
+		let response = await fetch( url );
+
+		setTokens( await response.json() );
+	} )();
+
+	const Tokens = () => {
+		const tokensArr = [];
+
+		for ( const token in tokens ) {
+			tokensArr.push( <li key={token}>{token}: {tokens[token]}</li> );
+		}
+
+		return tokensArr;
+	}
+
+	return <Fragment>
+		{ tokens && <Tokens /> }
+		<div>{`Update ${props.tokenType} token: ${props.selectedToken}`}</div>
+	</Fragment>;
+};
