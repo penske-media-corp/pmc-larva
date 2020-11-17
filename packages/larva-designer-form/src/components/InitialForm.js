695291cc8ea87da598d89423e57044f109c7f6a4
laras126 <laras126@gmail.com>
2020-11-17 14:27:02 -0500

    chore: wording updates

--- packages/larva-designer-form/src/components/InitialForm.js
+++ packages/larva-designer-form/src/components/InitialForm.js
@@ -4,7 +4,7 @@ import { NewToken } from "./NewToken";
 import { UpdateToken } from "./UpdateToken";
 
 export const InitialForm = () => {
-	const [tokenType, setTokenType] = useState();
+	const [tokenType, setTokenType] = useState("Brand");
 	const [selectedToken, setSelectedToken] = useState();
 	const [newToken, setNewToken] = useState(false);
 	const [submitted, setSubmitted] = useState(false);
@@ -17,19 +17,8 @@ export const InitialForm = () => {
 	if (!submitted && !newToken) {
 		return (
 			<form onSubmit={handleSubmit}>
-				<h3>Would you like to update a brand or base token?</h3>
+				<h3>Which brand would you like to update? <br/>Select "Default" to update the default tokens.</h3>
 				<Fragment>
-					<div className="radio">
-						<label>
-							<input
-								type="radio"
-								value="Base"
-								checked={tokenType === "Base"}
-								onChange={() => setTokenType("Base")}
-							/>
-							Base
-						</label>
-					</div>
 					<div className="radio">
 						<label>
 							<input
@@ -41,11 +30,22 @@ export const InitialForm = () => {
 							Brand
 						</label>
 					</div>
+					<div className="radio">
+						<label>
+							<input
+								type="radio"
+								value="Default"
+								checked={tokenType === "Default"}
+								onChange={() => setTokenType("Default")}
+							/>
+							Generic
+						</label>
+					</div>
 				</Fragment>
 
 				{tokenType && (
 					<Fragment>
-						<h3>Select the token you would like to update:</h3>
+						<h3>Select the brand you would like to update:</h3>
 
 						<select
 							name="select"
@@ -67,21 +67,24 @@ export const InitialForm = () => {
 								  ))}
 						</select>
 
-						<p>
+						{/* <p>
 							Can't find the token you are looking for? Select
 							either 'Base' or 'Brand' above and click 'Create New
 							Token' below.
-						</p>
+						</p> */}
 					</Fragment>
 				)}
 
-				<button
+
+				{/* <button
 					className="btn"
 					onClick={() => setNewToken(true)}
 					disabled={!tokenType}
 				>
 					Create New Token
-				</button>
+				</button> */}
+
+
 				<button
 					className="btn"
 					type="submit"
