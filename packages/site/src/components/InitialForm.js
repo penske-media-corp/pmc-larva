4ed1c6e8a093f2092ada08b6119d426b9a8aeb45
laras126 <laras126@gmail.com>
2021-01-08 14:23:20 -0500

    wip: adding create and update versions

--- packages/site/src/components/InitialForm.js
+++ packages/site/src/components/InitialForm.js
@@ -28,42 +28,66 @@ export const InitialForm = () => {
 
 	if (!submitted) {
 		return (
-			<form onSubmit={handleSubmit} className="lrv-a-space-children lrv-a-space-children-vertical lrv-a-space-children--1">
-				<h3>
-					Which brand would you like to update?
-				</h3>
+			<Fragment>
+			<p className="lrv-u-font-size-18">Choose from the following options for working with Design Tokens.</p>
 
-				<h3>Select the brand you would like to update:</h3>
+			<form onSubmit={handleSubmit} className="lrv-a-grid lrv-a-cols2">
+				<div className="lrv-a-space-children-vertical lrv-a-space-children--1">
+					<h2>Update a Brand's Tokens</h2>
+					<p className="lrv-u-font-size-18">Select the brand you would like to update:</p>
+					<select
+						className="lrv-u-display-block lrv-u-padding-a-050 lrv-u-margin-b-2 lrv-u-border-a-1 lrv-u-border-radius-5"
+						name="select"
+						onChange={(e) => {
+							setSelectedToken({
+								...selectedToken,
+								brand: e.target.value,
+							});
+						}}
+					>
+						<option key="select">Select</option>
+						{
+							selectedToken.type === "Brand"
+							&& brands.map((brand) => (
+									<option key={brand} value={brand}>
+										{brand}
+									</option>
+							))
+						}
+					</select>
 
-				<select
-					className="lrv-u-display-block lrv-u-padding-a-050 lrv-u-margin-b-2 lrv-u-border-a-1 lrv-u-border-radius-5"
-					name="select"
-					onChange={(e) => {
-						setSelectedToken({
-							...selectedToken,
-							brand: e.target.value,
-						});
-					}}
-				>
-					<option key="select">Select</option>
-					{
-						selectedToken.type === "Brand"
-						&& brands.map((brand) => (
-								<option key={brand} value={brand}>
-									{brand}
-								</option>
-						))
-					}
-				</select>
+					<button
+						className="ui primary button lrv-u-display-inline-block"
+						type="submit"
+						disabled={!(selectedToken.type && selectedToken.brand)}
+					>
+						Submit
+					</button>
+				</div>
+				<div className="lrv-a-space-children-vertical lrv-a-space-children--1">
+					<h2>Create Tokens for a New Brand</h2>
+					<p className="lrv-u-font-size-18">Enter the name of the brand in all lowercase, one word text:</p>
 
-				<button
-					className="ui primary button lrv-u-display-inline-block"
-					type="submit"
-					disabled={!(selectedToken.type && selectedToken.brand)}
-				>
-					Submit
-				</button>
+					<div className="ui input">
+						<input
+							type="text"
+							placeholder="artnews"
+						/>
+					</div>
+					<p className="lrv-u-color-grey">e.g. billboard, variety, rollingstone, artinamerica, sheknows</p>
+
+					<div className="lrv-u-display-block">
+						<button
+							className="ui primary button lrv-u-display-inline-block"
+							type="submit"
+							disabled={!(selectedToken.type && selectedToken.brand)}
+						>
+							Submit
+						</button>
+					</div>
+				</div>
 			</form>
+			</Fragment>
 		);
 	} else if (selectedToken.action === "create") {
 		return <NewToken tokenType={selectedToken.type} />;
@@ -71,20 +95,20 @@ export const InitialForm = () => {
 
 		return (
 			<Fragment>
-			<div className="lrv-a-grid lrv-a-cols3">
-				<section className="lrv-a-span2">
-					<UpdateToken
-						tokenType={selectedToken.type}
-						selectedToken={selectedToken.brand}
-						tokens={tokens}
-					/>
-				</section>
-				<section className="lrv-u-width-100p lrv-u-overflow-auto">
-					<JsonOutput
-						tokensJson={tokens}
-					/>
-				</section>
-			</div>
+				<div className="lrv-a-grid lrv-a-cols3">
+					<section className="lrv-a-span2">
+						<UpdateToken
+							tokenType={selectedToken.type}
+							selectedToken={selectedToken.brand}
+							tokens={tokens}
+						/>
+					</section>
+					<section className="lrv-u-width-100p lrv-u-overflow-auto">
+						<JsonOutput
+							tokensJson={tokens}
+						/>
+					</section>
+				</div>
 			</Fragment>
 		);
 	}
