e504ff7521190c6774d75d425cb0f209d31a19b6
laras126 <laras126@gmail.com>
2021-01-08 14:09:48 -0500

    feat: add basic routing

--- packages/site/src/components/App.js
+++ packages/site/src/components/App.js
@@ -1,9 +1,35 @@
 import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
+
 import { InitialForm } from "./InitialForm";
 
 export const App = () => (
-	<div className="App">
-		<h1 className="lrv-u-margin-b-1 lrv-u-padding-tb-1 lrv-u-border-b-1">Update Larva Design Tokens</h1>
-		<InitialForm />
-	</div>
+	<Router>
+		<div className="App lrv-a-space-children-vertical lrv-a-space-children--1">
+			<header className="lrv-u-flex lrv-u-align-items-center lrv-u-padding-tb-1 lrv-u-border-b-1">
+				<h1 className="lrv-u-margin-r-1">Larva Helpers Site</h1>
+				<ul className="ui basic buttons lrv-a-unstyle-list lrv-u-flex lrv-a-space-children-horizontal lrv-a-space-children--1">
+					<li className="ui button">
+						<Link to="/">Home</Link>
+					</li>
+					<li className="ui button">
+						<Link to="/tokens">Design Tokens</Link>
+					</li>
+				</ul>
+			</header>
+			<Switch>
+				<Route path="/tokens">
+					<InitialForm />
+				</Route>
+				<Route path="/">
+					<p>Welcome to the Larva helpers site! So far, this site contains a tool for helping to create design tokens.</p>
+				</Route>
+			</Switch>
+		</div>
+	</Router>
 );
