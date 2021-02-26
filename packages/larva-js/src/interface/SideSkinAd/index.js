b6cfd7531708ba46b8eb8dc153ae362c3b487e82
Yahil Madakiya <yahilmadakiya@gmail.com>
2021-02-26 19:37:18 +0530

    Fix side skin ad

--- packages/larva-js/src/interface/SideSkinAd/index.js
+++ packages/larva-js/src/interface/SideSkinAd/index.js
@@ -6,6 +6,10 @@ export default function initSideSkinAd ( e ) {
 	const parametersMessagePattern = 'pmcadm:dfp:skinad:parameters';
 	let serializedParameters = '';
 
+	if ( undefined !== window.pmc && undefined !== window.pmc.skinAds ) {
+		window['pmc_dfp_skin'] = window.pmc.skinAds;
+	}
+
 	if ( 'string' === typeof e.data && 'object' === typeof window.pmc_dfp_skin ) { // eslint-disable-line camelcase
 		if ( parametersMessagePattern === e.data.substring( 0, parametersMessagePattern.length ) ) {
 			serializedParameters = e.data.substring( parametersMessagePattern.length );
