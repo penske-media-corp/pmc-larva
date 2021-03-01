be9fda28bf92f08fba86550dc433ecd50e126867
laras126 <laras126@gmail.com>
2021-03-01 18:14:55 -0500

    Fix: update test

--- packages/larva/__tests__/lib/utils/getAppConfiguration.test.js
+++ packages/larva/__tests__/lib/utils/getAppConfiguration.test.js
@@ -1,6 +1,4 @@
-const assert = require( 'assert' );
 const path = require( 'path' );
-const fs = require( 'fs' );
 const fixture = path.join( __dirname, '../../fixtures' );
 
 const getAppConfiguration = require( '../../../lib/utils/getAppConfiguration' );
@@ -9,7 +7,7 @@ const expectedLarvaConfig = require( fixture + '/larva.config.js' );
 describe( 'getAppConfiguration', () => {
 
 	it( 'Get Larva Configuration', () => {
-		assert.equal( getAppConfiguration( 'backstop' ), expectedLarvaConfig.backstop );
+		expect(getAppConfiguration( 'backstop' )).toStrictRqual( expectedLarvaConfig.backstop );
 	});
 
 });
