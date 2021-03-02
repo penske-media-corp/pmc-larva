4ee9be8d114434e26bdc75db1415caf3320270ac
laras126 <laras126@gmail.com>
2021-03-02 11:35:22 -0500

    chore: remove legacy style guide

--- packages/larva-patterns/__tests__/style-guide/style-guide.prototype.js
+++
@@ -1,9 +0,0 @@
-const clonedeep = require( 'lodash.clonedeep' );
-
-const c_button_brand_basic = clonedeep( require( '../../components/c-button/c-button.brand-basic' ) );
-const c_button_black = clonedeep( require( '@penskemediacorp/larva-patterns/components/c-button/c-button.black' ) );
-
-module.exports = {
-	c_button_brand_basic,
-	c_button_black
-};
