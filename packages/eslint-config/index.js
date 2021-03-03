5b3c4a3e33cd17eba95fa0b718b81e7c1759579b
laras126 <laras126@gmail.com>
2021-03-03 11:19:23 -0500

    Fix: Add config for root pattern paths

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,12 +1,3 @@
 module.exports = {
-	'extends': [ 'plugin:@wordpress/eslint-plugin/recommended' ],
-	"overrides": [ {
-		"files": [
-			"**/*.js"
-		],
-		"rules": {
-			"jsdoc/check-tag-names": "off",
-			"jsdoc/require-returns-description": "off"
-		}
-	} ]
+	'extends': [ 'plugin:@wordpress/eslint-plugin/recommended' ]
 };
