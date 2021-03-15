6de120a318f1998600401e0da5c0d6c37e45bdcf
Hau Vong <hvong@pmc.com>
2021-03-15 09:54:35 -0700

    Filter empty ga tracking data

--- packages/larva-patterns/components/c-button/c-button.prototype.js
+++ packages/larva-patterns/components/c-button/c-button.prototype.js
@@ -1,13 +1,6 @@
 module.exports = {
 	c_button_classes: 'lrv-a-unstyle-button lrv-u-cursor-pointer',
-	c_button_ga_tracking: {
-		'details' : false,
-		'category': '',
-		'id': true, // post id
-		'label': '',
-		'location': true, // canonical url
-		'url': true // link url
-	},
+	c_button_ga_tracking: {}, // by default we do not want to enable any ga tracking
 	c_button_inner_classes: '',
 	c_button_rel_attr: '',
 	c_button_screen_reader_text: 'Sign Up',
