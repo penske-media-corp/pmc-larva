98b12f18df83c2a070b46512b47ff23081ac8a84
Hau Vong <hvong@pmc.com>
2021-03-15 09:23:24 -0700

    Only include ga tracking related to c button in prototype

--- packages/larva-patterns/components/c-button/c-button.prototype.js
+++ packages/larva-patterns/components/c-button/c-button.prototype.js
@@ -1,23 +1,18 @@
 module.exports = {
 	c_button_classes: 'lrv-a-unstyle-button lrv-u-cursor-pointer',
-	c_button_target_attr: '',
-	c_button_rel_attr: '',
-	c_button_inner_classes: '',
-	c_button_text: 'Sign Up',
-	c_button_screen_reader_text: 'Sign Up',
-	c_button_url: '',
 	c_button_ga_tracking: {
-		'details' : false, // text
-		'category': '',  // Amazon Inline Text
-		'label': '',  // eg. buy-now
-		'product': {
-			'id': '',
-			'name': '', // title
-			'brand': '',
-			'category': '',
-			'price': '', // numeric string
-		},
-		'url': false,
+		'details' : false,
+		'category': '',
+		'id': true, // post id
+		'label': '',
+		'location': true, // canonical url
+		'url': true // link url
 	},
-	c_button_type_attr: 'submit'
+	c_button_inner_classes: '',
+	c_button_rel_attr: '',
+	c_button_screen_reader_text: 'Sign Up',
+	c_button_target_attr: '',
+	c_button_text: 'Sign Up',
+	c_button_type_attr: 'submit',
+	c_button_url: ''
 }
