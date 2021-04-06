f385001f0b569acc1cfafc790bf0ee39c9e1b7fa
laras126 <laras126@gmail.com>
2021-04-06 12:14:25 -0400

    [GUT-87] WIP: cover block

---
+++ src/patterns/modules/cover-image/cover-image.prototype.js
@@ -0,0 +1,9 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const c_lazy_image = require( '../../../../packages/larva-patterns/components/c-lazy-image/c-lazy-image.prototype' );
+const paragraph = require( '../../../../packages/larva-patterns/modules/paragraph/paragraph.prototype' );
+
+module.exports = {
+	c_lazy_image,
+	paragraph,
+}
