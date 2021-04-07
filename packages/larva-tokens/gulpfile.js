d0cf9c5327391e8780fc19dfb17993f66a5ced89
laras126 <laras126@gmail.com>
2021-04-07 14:17:19 -0400

    [typography-tokens] feat: add clean to tokens build

--- packages/larva-tokens/gulpfile.js
+++ packages/larva-tokens/gulpfile.js
@@ -1,10 +1,23 @@
-// gulpfile.js
 const gulp = require('gulp')
 const theo = require('gulp-theo')
+const gulpClean = require( 'gulp-clean' );
+const { mkdirpSync } = require( 'fs-extra' );
 
 const { kebabify } = require( './lib/utils' );
 
 const formats = [ 'map.scss', 'custom-properties.css', 'json', 'raw.json' ];
+
+/**
+ * Prepare destination directory.
+ *
+ * @param {Function} done Function called upon completion.
+ */
+ const clean = ( dest, done ) => {
+	gulp.src( dest, { read: false } ).pipe( gulpClean() );
+
+	mkdirpSync( dest );
+	done();
+};
 
 const basicTokenBuild = ( format, dest = './build' ) => {
 	gulp.src( [
@@ -20,21 +33,26 @@ const basicTokenBuild = ( format, dest = './build' ) => {
 };
 
 gulp.task('default', ( done ) => {
-	formats.forEach( format => {
-		basicTokenBuild( format )
+
+	clean( './build', () => {
+		formats.forEach( format => {
+			basicTokenBuild( format )
+		});
 	});
 
-	gulp.src('src/brands/*.json')
-		.pipe(theo({
-			transform: { type: 'web' },
-			format: {
-				type: 'html',
-				options: {
-					transformPropName: ( name ) => kebabify( name )
+	clean( './style-guides', () => {
+		gulp.src('src/brands/*.json')
+			.pipe(theo({
+				transform: { type: 'web' },
+				format: {
+					type: 'html',
+					options: {
+						transformPropName: ( name ) => kebabify( name )
+					}
 				}
-			}
-		}))
-		.pipe(gulp.dest('./style-guides'));
+			}))
+			.pipe(gulp.dest('./style-guides'));
+	});
 
 	done();
 });
