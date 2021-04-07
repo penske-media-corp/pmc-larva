4ccc2197750690a2a1a6cfae67eca3bd991bd6e6
laras126 <laras126@gmail.com>
2021-04-07 13:03:23 -0400

    [typography-tokens] fix: todo

--- packages/larva-tokens/lib/utils.js
+++ packages/larva-tokens/lib/utils.js
@@ -1,11 +1,5 @@
 export const kebabify = ( name ) => {
-	let kebabCase = [];
-
-	// TODO: find a more concise way of turning a string into an iterable
-	for (let i = 0; i < name.length; i++) {
-		let letter = name[i];
-		kebabCase[i] = letter;
-	}
+	const kebabCase = name.split('');
 
 	return kebabCase.reduce( ( a, b ) => {
 		if ( '_' === b ) {
