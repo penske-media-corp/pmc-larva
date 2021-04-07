7feaf4603cc1f995b41bd126c71e589537062297
laras126 <laras126@gmail.com>
2021-04-07 16:51:46 -0400

    [typography-tokens] refact: use gulp series

--- packages/larva-tokens/gulpfile.js
+++ packages/larva-tokens/gulpfile.js
@@ -1,7 +1,6 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 const theo = require('gulp-theo')
 const del = require( 'del' );
-const { mkdirpSync } = require( 'fs-extra' );
 
 const { kebabify } = require( './lib/utils' );
 
@@ -12,17 +11,15 @@ const formats = [ 'map.scss', 'custom-properties.css', 'json', 'raw.json' ];
  *
  * @param {Function} done Function called upon completion.
  */
- const clean = ( dest, done ) => {
-	del( dest );
-
-	mkdirpSync( dest );
+ const clean = ( done ) => {
+	del( [ 'build', 'style-guides' ] );
 	done();
 };
 
 const basicTokenBuild = (
 	format,
 	done,
-	dest = './build'
+	dest = 'build'
 ) => {
 	gulp.src( [
 		'src/brands/*.json',
@@ -38,29 +35,27 @@ const basicTokenBuild = (
 		done();
 };
 
-gulp.task('default', ( done ) => {
+const build = ( done ) => {
 
-	clean( './build', () => {
-		formats.forEach( format => {
-			basicTokenBuild( format, () => {
-				console.log( `Built tokens in ${format}`);
-			} )
-		});
+	formats.forEach( format => {
+		basicTokenBuild( format, () => {
+			console.log( `Built ${format} format.`);
+		} )
 	});
 
-	clean( './style-guides', () => {
-		gulp.src('src/brands/*.json')
-			.pipe(theo({
-				transform: { type: 'web' },
-				format: {
-					type: 'html',
-					options: {
-						transformPropName: ( name ) => kebabify( name )
-					}
+	gulp.src('src/brands/*.json')
+		.pipe(theo({
+			transform: { type: 'web' },
+			format: {
+				type: 'html',
+				options: {
+					transformPropName: ( name ) => kebabify( name )
 				}
-			}))
-			.pipe(gulp.dest('./style-guides'));
-	});
+			}
+		}))
+		.pipe(gulp.dest('style-guides'));
 
 	done();
-});
+};
+
+exports.default = gulp.series( clean, build )
