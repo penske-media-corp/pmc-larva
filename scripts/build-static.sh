8c2c6598c5903eac4649d05643ef801f76db1853
laras126 <laras126@gmail.com>
2021-04-12 14:09:18 -0400

    [GUT-151] feat: build larva site in vercel

---
+++ scripts/build-static.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build React site and copy to to build/html
+mkdir -p build/html/site
+npm run build --prefix=packages/site
+cp -r packages/site/build/* build/html
+
+# Generate Larva static site
+nohup node ./packages/larva/lib/start.js > /dev/null 2>&1 &
+npm run build-html
