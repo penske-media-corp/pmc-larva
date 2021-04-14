50a8e59388389a111abb7743788f4d79244d931c
laras126 <laras126@gmail.com>
2021-04-14 19:14:57 -0400

    [GUT-151] fix: update test

--- packages/larva-css/__tests__/fixtures/expected.js
+++ packages/larva-css/__tests__/fixtures/expected.js
@@ -11,8 +11,6 @@ module.exports = {
 	--font-weight-mobile: var(--is-desktop) var( --primary-xxl-font-weight-mobile);
 	--font-style-desktop: var( --primary-xxl-font-style-desktop, var( --primary-xxl-font-style-mobile ) );
 	--font-style-mobile: var(--is-desktop) var( --primary-xxl-font-style-mobile);
-	--font-family-desktop: var( --primary-xxl-font-family-desktop, var( --primary-xxl-font-family-mobile ) );
-	--font-family-mobile: var(--is-desktop) var( --primary-xxl-font-family-mobile);
 
 	// Define this token locally; it can be defined via larva-tokens, if needed in the future.
 	--font-family: var( --font-family-primary );
@@ -38,10 +36,6 @@ module.exports = {
 	font-style: var(
 		--font-style-mobile,
 		var(--font-style-desktop)
-	);
-	font-family: var(
-		--font-family-mobile,
-		var(--font-family-desktop)
 	);
 }\n`,
 };
