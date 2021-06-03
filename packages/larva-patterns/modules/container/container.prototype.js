0d13689039fafc5abc7ac062107cd6739628592c
tinuola <tinuola@users.noreply.github.com>
2021-06-03 12:49:01 -0700

    Updating paragraph and container max width settings

--- packages/larva-patterns/modules/container/container.prototype.js
+++ packages/larva-patterns/modules/container/container.prototype.js
@@ -1,4 +1,4 @@
 module.exports = {
 	container_markup: '<p>hello markup</p>',
-	container_classes: ''
+	container_classes: 'a-font-body-m lrv-u-width-75p lrv-u-margin-lr-auto'
 }
--- packages/larva-patterns/modules/paragraph/paragraph.prototype.js
+++ packages/larva-patterns/modules/paragraph/paragraph.prototype.js
@@ -1,4 +1,4 @@
 module.exports = {
-	paragraph_classes: 'a-font-body-m',
+	paragraph_classes: 'a-font-body-m lrv-u-width-75p lrv-u-margin-lr-auto',
 	paragraph_markup: 'Paragraph Text.',
 };
