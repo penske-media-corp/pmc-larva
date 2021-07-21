839f9fa23b2cc1a7a3a654556f1362c356e5b05d
tinuola <tinuola@users.noreply.github.com>
2021-07-20 17:46:56 -0700

    WIP GUT-163: Working draft of carousel-grid overlay algorithm

--- packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
+++ packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
@@ -1,3 +1,9 @@
 module.exports = {
-    modifier_classes: ''
+    modifier_classes1: 'lrv-a-carousel-grid1__primary--overlay',
+    modifier_classes2: 'lrv-a-carousel-grid1__secondary--flex',
+    modifier_classes3: 'lrv-a-carousel-grid1__overlay-img',
+    modifier_classes4: 'lrv-a-carousel-grid1__overlay-content',
+    modifier_classes5: 'lrv-a-carousel-grid1__overlay',
+    modifier_classes6: 'lrv-u-flex',
+    modifier_classes7: 'lrv-u-width-75p'
 }
