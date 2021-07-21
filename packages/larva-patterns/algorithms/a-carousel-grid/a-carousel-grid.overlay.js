13c08b68c015a7e523caa9a2c7b15e2e2393c0fc
tinuola <tinuola@users.noreply.github.com>
2021-07-20 18:15:39 -0700

    WIP GUT-163: Created carousel overlay algorithm, deleted 4up

--- packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
+++ packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
@@ -1,9 +1,16 @@
 module.exports = {
-    modifier_classes1: 'lrv-a-carousel-grid1__primary--overlay',
-    modifier_classes2: 'lrv-a-carousel-grid1__secondary--flex',
-    modifier_classes3: 'lrv-a-carousel-grid1__overlay-img',
-    modifier_classes4: 'lrv-a-carousel-grid1__overlay-content',
-    modifier_classes5: 'lrv-a-carousel-grid1__overlay',
-    modifier_classes6: 'lrv-u-flex',
-    modifier_classes7: 'lrv-u-width-75p'
+    
+    modifier_class1: 'lrv-a-carousel-grid__overlay',
+    
+    modifier_class2: 'lrv-a-carousel-grid__primary--overlay',
+    
+    modifier_class3: 'lrv-a-carousel-grid__overlay-img',
+
+    modifier_class4: 'lrv-a-carousel-grid__overlay-content',
+
+    modifier_class5: 'lrv-a-carousel-grid__secondary--flex',
+
+    modifier_class6: 'lrv-u-flex',
+    
+    modifier_class7: 'lrv-u-width-75p'
 }
