067b3f35957ba0652f03a9b3f697307d21900e48
laras126 <laras126@gmail.com>
2021-07-22 11:08:59 -0400

    [GUT-155] fix: remove unused color classes

--- packages/larva-patterns/modules/heading/heading.h1.js
+++ packages/larva-patterns/modules/heading/heading.h1.js
@@ -2,7 +2,7 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const heading = clonedeep( require( './heading.prototype' ) );
 
-heading.heading_classes = 'lrv-a-font-primary-xl lrv-u-color-brand-primary';
+heading.heading_classes = 'lrv-a-font-primary-xl';
 heading.heading_level_text = '1';
 
 module.exports = heading;
