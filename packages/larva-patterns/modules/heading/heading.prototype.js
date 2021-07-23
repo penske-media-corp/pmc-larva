297577ec59a094dfb16fda3b48b5c37d5b51e2d4
laras126 <laras126@gmail.com>
2021-07-23 10:09:07 -0400

    [GUT-155] chore: add alignment and color class

--- packages/larva-patterns/modules/heading/heading.prototype.js
+++ packages/larva-patterns/modules/heading/heading.prototype.js
@@ -1,5 +1,7 @@
 module.exports = {
 	heading_level_text: '3',
 	heading_classes: 'lrv-a-font-primary-m',
+	heading_color_class: '',
+	heading_alignment_class: '',
 	heading_markup: 'Heading Text'
 };
