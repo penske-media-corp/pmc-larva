b7a8e574afdf76d30fcc3e936f950cba6b149f6f
laras126 <laras126@gmail.com>
2021-07-23 12:00:48 -0400

    [GUT-155] chore: fix paragraph text classes

--- packages/larva-patterns/modules/paragraph/paragraph.prototype.js
+++ packages/larva-patterns/modules/paragraph/paragraph.prototype.js
@@ -1,4 +1,6 @@
 module.exports = {
-	paragraph_classes: 'a-font-body-m',
+	paragraph_classes: 'lrv-a-font-body-m',
+	paragraph_alignment_class: '',
+	paragraph_color_class: '',
 	paragraph_markup: 'Paragraph Text.',
 };
