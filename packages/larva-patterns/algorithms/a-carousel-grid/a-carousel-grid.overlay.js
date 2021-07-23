9fc9ee778d24a7f8f97a9717416fc09d08c6b265
tinuola <tinuola@users.noreply.github.com>
2021-07-22 17:43:55 -0700

    (WIP) GUT-163: Update carousel-grid pattern css classes

--- packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
+++ packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
@@ -8,9 +8,9 @@ module.exports = {
 
     modifier_class4: 'lrv-a-carousel-grid__overlay-content',
 
-    modifier_class5: 'lrv-a-carousel-grid__secondary--flex',
+    modifier_class5: 'lrv-a-carousel-grid__secondary--grid',
 
-    modifier_class6: 'lrv-u-flex',
+    modifier_class6: 'lrv-u-flex lrv-u-align-items-center lrv-u-margin-b-1@mobile-max',
     
     modifier_class7: 'lrv-u-width-75p'
 }
