9827536eb4cf8293d893151b4ddc75f13a869d22
laras126 <laras126@gmail.com>
2021-07-26 16:43:19 -0400

    [GUT-155] chore: change alignment to text_align

--- packages/larva-patterns/modules/container/container.prototype.js
+++ packages/larva-patterns/modules/container/container.prototype.js
@@ -3,5 +3,5 @@ module.exports = {
 	container_classes: 'lrv-a-font-body-m lrv-u-width-75p lrv-u-margin-lr-auto',
 	container_background_color_class: '',
 	container_color_class: '',
-	container_alignment_class: '',
+	container_text_align_class: '',
 }
