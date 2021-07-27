076b94ed953fcbcc7777539e57f313afc85493b6
Erick Hitter <git-contrib@ethitter.com>
2021-07-27 14:55:51 -0700

    GUT-179: fix padding and fonts

--- packages/larva-patterns/modules/story-grid/story-grid.prototype.js
+++ packages/larva-patterns/modules/story-grid/story-grid.prototype.js
@@ -2,6 +2,10 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const story_card = clonedeep( require( '../story/story.vertical' ) );
 
+story_card.story_nav_classes = 'lrv-u-font-size-12 lrv-u-font-family-secondary';
+story_card.story_grid_secondary_classes = 'lrv-a-span2';
+story_card.c_dek.c_dek_classes += ' lrv-u-font-family-body';
+
 module.exports = {
 	story_grid_classes: '',
 	story_grid_grid_classes: 'lrv-a-grid lrv-a-cols2 lrv-a-cols4@desktop',
