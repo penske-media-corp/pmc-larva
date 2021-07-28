8214cf9d8e62b899ca373d23789985ec38967092
tinuola <tinuola@users.noreply.github.com>
2021-07-28 15:13:11 -0700

    Task: Update carousel grid pattern css

--- packages/larva-patterns/modules/carousel-grid/carousel-grid.prototype.js
+++ packages/larva-patterns/modules/carousel-grid/carousel-grid.prototype.js
@@ -5,7 +5,7 @@ const o_card_prototype = require( '../../objects/o-card/o-card.prototype' );
 const o_card = clonedeep( o_card_prototype );
 
 o_card.o_card_classes = 'lrv-u-margin-b-2';
-o_card.o_card_content_classes = 'lrv-u-margin-t-1';
+o_card.o_card_content_classes = ' lrv-u-margin-t-050';
 
 o_card.c_lazy_image.c_lazy_image_classes += ' ';
 o_card.c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-2x3';
@@ -38,8 +38,7 @@ o_card_large.o_author.c_timestamp.c_timestamp_classes = 'lrv-a-icon-before lrv-a
 o_card.o_author = false;
 
 o_card_large.o_card_classes = '';
-o_card_large.o_card_content_classes = 'lrv-u-padding-a-1 lrv-u-padding-a-2@desktop';
-// o_card_large.c_title.c_title_classes = 'lrv-a-font-primary-s@desktop-xl lrv-a-font-primary-s@tablet lrv-a-font-primary-xs@mobile-max lrv-u-margin-tb-050';
+o_card_large.o_card_content_classes = 'lrv-u-padding-a-1';
 o_card_large.c_title.c_title_classes = 'lrv-a-font-primary-xs lrv-u-margin-tb-050';
 
 module.exports = {
