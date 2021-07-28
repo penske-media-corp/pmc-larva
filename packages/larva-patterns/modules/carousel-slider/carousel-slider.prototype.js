884966d26142ab16297736f9d977b052eed4bc35
tinuola <tinuola@users.noreply.github.com>
2021-07-28 13:59:02 -0700

    Task: Create carousel slider pattern

---
+++ packages/larva-patterns/modules/carousel-slider/carousel-slider.prototype.js
@@ -0,0 +1,57 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const c_heading_prototype = require( '../../components/c-heading/c-heading.prototype' );
+const c_heading = clonedeep( c_heading_prototype );
+
+const o_card_prototype = require( '../../objects/o-card/o-card.prototype' );
+const o_card = clonedeep( o_card_prototype );
+
+const o_indicator_prototype = require( '../../objects/o-indicator/o-indicator.prototype' );
+const o_indicator = clonedeep( o_indicator_prototype );
+
+o_indicator.c_icon = false;
+o_indicator.c_span.c_span_text = 'Photos';
+o_indicator.c_span.c_span_classes += ' lrv-u-text-transform-uppercase lrv-u-font-family-primary lrv-u-font-size-12 a-icon-gallery lrv-a-icon-after';
+o_indicator.o_indicator_classes += ' lrv-u-background-color-brand-primary lrv-u-color-white lrv-u-padding-a-050';
+
+o_card.o_indicator = o_indicator;
+
+o_card.o_card_link_url = '#';
+o_card.o_card_link_classes = 'lrv-a-unstyle-link lrv-u-display-block';
+o_card.c_lazy_image.c_lazy_image_classes = 'lrv-u-margin-b-050@mobile-max lrv-u-margin-b-1';
+o_card.c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-16x9';
+o_card.c_lazy_image.c_lazy_image_image_classes = 'u-box-shadow-medium';
+o_card.c_lazy_image.c_lazy_image_placeholder_url = 'https://live.staticflickr.com/4685/24635253357_483728d136_c.jpg';
+o_card.o_card_image_wrap_classes = 'lrv-a-glue-parent';
+o_card.c_lazy_image.c_lazy_image_caption_markup = false;
+o_card.c_lazy_image.c_lazy_image_credit_text = false;
+o_card.c_span = false;
+o_card.c_timestamp = false;
+o_card.c_title.c_title_link_classes = 'lrv-a-unstyle-link';
+o_card.c_title.c_title_classes = 'lrv-a-font-primary-xxs lrv-u-text-align-center';
+o_card.c_title.c_title_url = false;
+
+const titles = [
+	'Banksy Painting Sells for Record-Demolishing $12.2 M. at Sotheby’s London',
+	'Famed Art Historian Germano Celant to Curate KAWS Exhibition in Qatar',
+	'Yayoi Kusama ‘Infinity Room’ Heads to Toledo Museum of Art as Part of ‘Multi-Sensory Experience’ Push',
+	'After the Flood: Artists Fill Amsterdam Gallery with Wet Clay, Establish Safety Fund',
+	'Frieze London Sales Accumulate, Museum Boards Go Under the Scope, and More—Morning Links from October 3, 2019',
+	'KAWS Hit With Criticism in China for Mao-Based Artwork—Artist Responds [Updated]',
+	'Deborah Marrow, Game-Changing Getty Foundation Leader, Has Died',
+	'As Brexit Looms at Frieze London, $6.5 M. Twombly and $5 M. Guston Sell, While $32 M. Botticelli Awaits Buyer',
+	'German Museum to Give Walid Raad Art Prize Despite City Government’s Objection: Report',
+	'Pace Gallery Plans Paris Presence'
+];
+
+let galleries = [];
+
+for (let i = 0; i < titles.length; i++) {
+	let card = clonedeep( o_card );
+	card.c_title.c_title_text = titles[i];
+	galleries.push( card );
+}
+
+module.exports = {
+	galleries: galleries,
+};
