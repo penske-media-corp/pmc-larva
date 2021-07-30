9ec2511303391bb4e3a8ab0c83d174c482c846fc
tinuola <tinuola@users.noreply.github.com>
2021-07-30 09:17:44 -0700

    Task: Updates to modifier class names

--- packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
+++ packages/larva-patterns/algorithms/a-carousel-grid/a-carousel-grid.overlay.js
@@ -1,12 +1,12 @@
 module.exports = {
     
-    carousel_grid_overlay_layout_classes: 'lrv-a-carousel-grid__overlay',
+    carousel_grid_overlay_layout_class: 'lrv-a-carousel-grid__overlay',
     
-    modifier_class2: 'lrv-a-carousel-grid__overlay--primary',
+    carousel_grid_overlay_content_class: 'lrv-a-carousel-grid__overlay--primary',
     
-    modifier_class3: 'lrv-a-carousel-grid__overlay--primary-img',
+    carousel_grid_overlay_img_class: 'lrv-a-carousel-grid__overlay--primary-img',
 
-    modifier_class4: 'lrv-a-carousel-grid__overlay--primary-content',
+    carousel_grid_overlay_content_class: 'lrv-a-carousel-grid__overlay--primary-content',
 
-    carousel_grid_overlay_secondary_classes: 'lrv-a-carousel-grid__overlay--secondary'
+    carousel_grid_overlay_secondary_class: 'lrv-a-carousel-grid__overlay--secondary'
 }
