bf7d861e64250d861baa79cc470d9fd158f1d0b8
laras126 <laras126@gmail.com>
2021-08-04 13:03:28 -0400

    [GUT-165] chore: add query string to sprite and build

--- entries/common.entry.js
+++ entries/common.entry.js
@@ -3,7 +3,7 @@ import initFlickity from '@larva-js/interface/Flickity';
 import initVideoShowcase from '@larva-js/interface/VideoShowcase';
 import initCollapsible from '@larva-js/interface/Collapsible';
 
-ajaxIconSprite( '/assets/build/svg/defs/sprite.defs.svg' );
+ajaxIconSprite( '/assets/build/svg/defs/sprite.defs.svg?v=080421' );
 
 // eslint-disable-next-line @wordpress/no-global-event-listener
 document.addEventListener( 'DOMContentLoaded', () => {
