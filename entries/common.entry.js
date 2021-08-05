bef2531ef249510070ac15f2c9daba93bc8ff0d2
laras126 <laras126@gmail.com>
2021-08-05 12:02:54 -0400

    [GUT-165] feat: add larva uri for icon url

--- entries/common.entry.js
+++ entries/common.entry.js
@@ -3,7 +3,17 @@ import initFlickity from '@larva-js/interface/Flickity';
 import initVideoShowcase from '@larva-js/interface/VideoShowcase';
 import initCollapsible from '@larva-js/interface/Collapsible';
 
-ajaxIconSprite( '/assets/build/svg/defs/sprite.defs.svg?v=080421' );
+const spritePath = ( () => {
+	if ( undefined !== window.pmc_common_urls ) {
+		return (
+			window.pmc_common_urls.pmc_larva_uri +
+			'/build/svg/defs/sprite.defs.svg'
+		);
+	}
+	return '/assets/build/svg/defs/sprite.defs.svg';
+} )();
+
+ajaxIconSprite( spritePath );
 
 // eslint-disable-next-line @wordpress/no-global-event-listener
 document.addEventListener( 'DOMContentLoaded', () => {
