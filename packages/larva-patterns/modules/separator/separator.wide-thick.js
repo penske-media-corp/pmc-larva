b9d541a0e60873e25b21d41f50fb8717f3e7d783
Erick Hitter <git-contrib@ethitter.com>
2021-09-22 13:32:52 -0700

    Introduce thicker separators, port `wide` from IndieWire

---
+++ packages/larva-patterns/modules/separator/separator.wide-thick.js
@@ -0,0 +1,7 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const separator = clonedeep( require( './separator.wide' ) );
+
+separator.separator_classes = 'lrv-u-margin-tb-1 lrv-u-border-a-3 lrv-u-margin-lr-2 lrv-u-margin-lr-4@desktop';
+
+module.exports = separator;
