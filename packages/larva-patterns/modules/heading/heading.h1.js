c3c91a900a5faf2121f6b6a51078a07498a775dc
Erick Hitter <git-contrib@ethitter.com>
2021-09-29 10:25:18 -0700

    CR

--- packages/larva-patterns/modules/heading/heading.h1.js
+++ packages/larva-patterns/modules/heading/heading.h1.js
@@ -2,7 +2,7 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const heading = clonedeep( require( './heading.prototype' ) );
 
-heading.heading_classes = 'lrv-a-font-primary-xl';
+heading.heading_typography_class = 'lrv-a-font-primary-xl';
 heading.heading_level_text = '1';
 
 module.exports = heading;
--- packages/larva-patterns/modules/heading/heading.h2.js
+++ packages/larva-patterns/modules/heading/heading.h2.js
@@ -2,7 +2,7 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const heading = clonedeep( require( './heading.prototype' ) );
 
-heading.heading_classes = 'lrv-a-font-accent-l';
+heading.heading_typography_class = 'lrv-a-font-accent-l';
 heading.heading_level_text = '2';
 
 module.exports = heading;
