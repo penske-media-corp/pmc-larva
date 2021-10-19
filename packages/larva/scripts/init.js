5c68d196c47e2c614f014d5c8c8e05e70c6be0ac
laras126 <laras126@gmail.com>
2021-10-19 09:26:09 -0400

    [init-updates] chore: formatting

--- packages/larva/scripts/init.js
+++ packages/larva/scripts/init.js
@@ -7,11 +7,14 @@ const copyTo = process.cwd();
 
 copySync( copyFrom, copyTo );
 
-console.log(chalk.green( 'Copied folder structure. Now, add the following scripts to package.json:' ));
-console.log(`
+console.log(
+	chalk.green( 'Copied folder structure. Now, add the following scripts to package.json:' )
+);
+
+console.log( `
 "larva": "larva server --watch src -e twig,js,html",
 "write-json": "larva write-json",
 "parser": "./node_modules/.bin/twig-to-php-parser",
 "backstop": "backstop --config=node_modules/@penskemediacorp/backstopjs-config",
 "build-html": "larva build-html"
-`);
+` );
