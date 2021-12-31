15438ea929880aa31e11e0e01a97915ec8f4acea
laras126 <laras126@gmail.com>
2021-12-31 11:54:17 -0500

    [GUT-229] feat: add utility for current color hover

--- packages/larva-patterns/modules/button/button.prototype.js
+++ packages/larva-patterns/modules/button/button.prototype.js
@@ -1,6 +1,6 @@
 module.exports = {
 	button_url: null,
-	button_classes: 'lrv-u-padding-tb-050 lrv-u-padding-lr-1',
+	button_classes: 'lrv-u-padding-tb-050 lrv-u-padding-lr-1 lrv-u-color-currentColor:hover',
 	button_markup: 'Button Text',
 	button_background_color_class: 'lrv-u-background-color-brand-primary',
 	button_color_class: 'lrv-u-color-white'
