37a672a2cf31581fd20becfd9de70d0832ce1e56
Saniyat <saniyat@gmail.com>
2022-02-23 12:17:16 +0100

    Pullquote module

---
+++ packages/larva-patterns/modules/pullquote/pullquote.prototype.js
@@ -0,0 +1,8 @@
+module.exports = {
+	pullquote_classes: 'lrv-u-width-75p',
+	pullquote_typography_class: 'lrv-a-font-body-m',
+	pullquote_text_align_class: 'lrv-u-text-align-center',
+	pullquote_color_class: '',
+	pullquote_background_color_class: '',
+	pullquote_markup: 'Pullquote Text.',
+};
