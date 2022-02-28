5eddf3aab021edc021a78a8dc2a94556f86769a5
laras126 <laras126@gmail.com>
2022-02-28 09:11:57 -0500

    [PMCP-3738] WIP updating dot notation - brittle

--- packages/larva-patterns/modules/story-grid/story-grid.prototype.js
+++ packages/larva-patterns/modules/story-grid/story-grid.prototype.js
@@ -6,7 +6,8 @@ story_card.c_dek.c_dek_classes = 'lrv-a-font-body-s lrv-a-hidden@mobile-max lrv-
 story_card.c_title.c_title_classes = 'lrv-a-font-primary-xxs lrv-u-display-block lrv-u-margin-b-050';
 story_card.story_grid_secondary_classes = 'lrv-a-span2';
 story_card.story_links_classes += ' lrv-u-margin-t-050';
-story_card.story_nav_classes = 'lrv-a-font-secondary-m';
+story_card.o_author.o_author_classes = 'lrv-a-font-secondary-m';
+story_card.o_author.story_nav_layout_classes = 'lrv-u-flex lrv-u-align-items-center lrv-a-space-children-horizontal lrv-a-space-children--2 lrv-a-hidden@mobile-max lrv-u-margin-t-050';
 
 module.exports = {
 	story_grid_classes: '',
--- packages/larva-patterns/modules/story/story.prototype.js
+++ packages/larva-patterns/modules/story/story.prototype.js
@@ -4,11 +4,13 @@ const o_card = __experimentalCloneWithFallback( 'objects/o-card/o-card.story' );
 
 module.exports = {
 	story_classes: '',
-	story_nav_classes: 'lrv-u-font-size-12 lrv-u-font-family-primary lrv-u-text-transform-uppercase lrv-u-font-weight-bold lrv-u-color-black',
-	story_nav_layout_classes: 'lrv-u-flex lrv-u-align-items-center lrv-a-space-children-horizontal lrv-a-space-children--2 lrv-a-hidden@mobile-max lrv-u-margin-t-050',
-	story_grid_classes: 'lrv-a-grid lrv-a-cols3',
-	story_grid_primary_classes: 'lrv-u-margin-r-2@desktop-xl lrv-u-margin-r-00 lrv-u-margin-r-1@desktop lrv-a-glue-parent lrv-u-flex lrv-u-flex-direction-column lrv-u-height-100p lrv-u-justify-content-start',
-	story_grid_secondary_classes: 'lrv-a-glue-parent u-padding-t-1@mobile-max lrv-u-flex lrv-u-flex-direction-column lrv-u-justify-content-start lrv-u-margin-r-2@desktop-xl lrv-u-margin-r-00 lrv-u-margin-r-1@desktop lrv-a-span2 lrv-u-margin-r-2@desktop-xl lrv-u-margin-r-00 lrv-u-margin-r-1@desktop',
 	story_links_classes: '',
-	o_card
+	o_card: {
+		... o_card,
+		story_nav_classes: 'lrv-u-font-size-12 lrv-u-font-family-primary lrv-u-text-transform-uppercase lrv-u-font-weight-bold lrv-u-color-black',
+		story_nav_layout_classes: 'lrv-u-flex lrv-u-align-items-center lrv-a-space-children-horizontal lrv-a-space-children--2 lrv-a-hidden@mobile-max lrv-u-margin-t-050',
+		story_grid_classes: 'lrv-a-grid lrv-a-cols3',
+		story_grid_primary_classes: 'lrv-u-margin-r-2@desktop-xl lrv-u-margin-r-00 lrv-u-margin-r-1@desktop lrv-a-glue-parent lrv-u-flex lrv-u-flex-direction-column lrv-u-height-100p lrv-u-justify-content-start',
+		story_grid_secondary_classes: 'lrv-a-glue-parent u-padding-t-1@mobile-max lrv-u-flex lrv-u-flex-direction-column lrv-u-justify-content-start lrv-u-margin-r-2@desktop-xl lrv-u-margin-r-00 lrv-u-margin-r-1@desktop lrv-a-span2 lrv-u-margin-r-2@desktop-xl lrv-u-margin-r-00 lrv-u-margin-r-1@desktop',
+	}
 };
