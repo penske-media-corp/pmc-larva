f4847442aa18d282fbb885bb0720135bf4441a95
laras126 <laras126@gmail.com>
2022-03-01 13:14:34 -0500

    [hide-larva-ui] chore: whitespace

--- packages/backstopjs-config/backstop_data/engine_scripts/puppet/onReady.js
+++ packages/backstopjs-config/backstop_data/engine_scripts/puppet/onReady.js
@@ -2,11 +2,9 @@ module.exports = async (page, scenario, vp) => {
 	console.log('SCENARIO > ' + scenario.label);
 
 	await page.evaluate(() => {
-
 		document.querySelectorAll('.js-LarvaUiToggle-panel').forEach( el => {
 			el.classList.add( 'lrv-a-hidden' );
 		});
-
 	});
 
 	await require('./clickAndHoverHelper')(page, scenario);
