7c224a1bcc852cd6e5627de053f4cf9da33e8c2c
laras126 <laras126@gmail.com>
2022-03-01 12:49:37 -0500

    [hide-larva-ui] feat: hide larva ui pieces

--- packages/backstopjs-config/backstop_data/engine_scripts/puppet/onReady.js
+++ packages/backstopjs-config/backstop_data/engine_scripts/puppet/onReady.js
@@ -1,21 +1,13 @@
 module.exports = async (page, scenario, vp) => {
 	console.log('SCENARIO > ' + scenario.label);
-	
+
 	await page.evaluate(() => {
-		let ids = [];
 
-		// Leave commented until we are testing production screenshots.
-		// if ( typeof googletag !== typeof undefined ) {
-		// 	ids.push( ... googletag.pubads().getSlots().map( slot => slot.getSlotElementId() ) );
-		// 	ids.push( 'jpsuperheader', 'skin-ad-section' ); // IDs that are not part of googetag array
-		// }
+		document.querySelectorAll('.js-LarvaUiToggle-panel').forEach( el => {
+			el.classList.add( 'lrv-a-hidden' );
+		});
 
-		// ids.forEach( id => {
-		// 	if ( null !== document.getElementById( id ) ) {
-		// 		document.getElementById( id ).style.cssText = 'display: none !important';
-		// 	}
-		// });
 	});
-	
+
 	await require('./clickAndHoverHelper')(page, scenario);
 };
