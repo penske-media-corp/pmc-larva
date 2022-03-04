b7c1ba47f22b6e1eae4833bec0ef4b86f163ced2
Vinod Tella <vtella@pmc.com>
2022-03-04 09:30:13 -0800

    Updating tests

--- packages/larva-css/__tests__/fixtures/expected.js
+++ packages/larva-css/__tests__/fixtures/expected.js
@@ -1,30 +1,31 @@
 module.exports = {
 	ruleset: `
 .lrv-a-font-primary-xxl {
+	// Define this token locally; it can be defined via larva-tokens, if needed in the future.
+	--font-family: var( --font-family-primary );
+
 	font-family: var( --font-family );
 	font-size: var( --primary-xxl-font-size-mobile );
+	line-height: var( --primary-xxl-line-height-mobile );
+	letter-spacing: var( --primary-xxl-letter-spacing-mobile );
 	font-weight: var( --primary-xxl-font-weight-mobile );
 	font-style: var( --primary-xxl-font-style-mobile );
-	line-height: var(--primary-xxl-line-height-mobile );
-	letter-spacing: var( --primary-xxl-letter-spacing-mobile );
-
 
 	@media (min-width: 800px) {
-		font-family: var( --font-family );
 		font-size: var( --primary-xxl-font-size-desktop );
+		line-height: var( --primary-xxl-line-height-desktop );
+		letter-spacing: var( --primary-xxl-letter-spacing-desktop );
 		font-weight: var( --primary-xxl-font-weight-desktop );
 		font-style: var( --primary-xxl-font-style-desktop );
-		line-height: var(--primary-xxl-line-height-desktop );
-		letter-spacing: var( --primary-xxl-letter-spacing-desktop );
-	}
 
+	}
 	@media (min-width: 1200px) {
-		font-family: var( --font-family );
-		font-size: var( --primary-xxl-font-size-desktopxxl );
-		font-weight: var( --primary-xxl-font-weight-desktopxxl );
-		font-style: var( --primary-xxl-font-style-desktopxxl );
-		line-height: var(--primary-xxl-line-height-desktopxxl );
-		letter-spacing: var( --primary-xxl-letter-spacing-desktopxxl );
+		font-size: var( --primary-xxl-font-size-desktopxl );
+		line-height: var( --primary-xxl-line-height-desktopxl );
+		letter-spacing: var( --primary-xxl-letter-spacing-desktopxl );
+		font-weight: var( --primary-xxl-font-weight-desktopxl );
+		font-style: var( --primary-xxl-font-style-desktopxl );
+
 	}
 }\n`,
 };
