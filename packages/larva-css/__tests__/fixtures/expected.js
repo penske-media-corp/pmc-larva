a893d8e6844d16bc67dd29072eeb4c0633b04ec9
Vinod Tella <vtella@pmc.com>
2022-03-04 09:11:41 -0800

    feature/GUT-152 Adding DesktopXL break point and updating the logic for css generator to use media queries instead of var function

--- packages/larva-css/__tests__/fixtures/expected.js
+++ packages/larva-css/__tests__/fixtures/expected.js
@@ -1,41 +1,30 @@
 module.exports = {
 	ruleset: `
 .lrv-a-font-primary-xxl {
-	--font-size-desktop: var( --primary-xxl-font-size-desktop, var( --primary-xxl-font-size-mobile ) );
-	--font-size-mobile: var(--is-desktop) var( --primary-xxl-font-size-mobile);
-	--line-height-desktop: var( --primary-xxl-line-height-desktop, var( --primary-xxl-line-height-mobile ) );
-	--line-height-mobile: var(--is-desktop) var( --primary-xxl-line-height-mobile);
-	--letter-spacing-desktop: var( --primary-xxl-letter-spacing-desktop, var( --primary-xxl-letter-spacing-mobile ) );
-	--letter-spacing-mobile: var(--is-desktop) var( --primary-xxl-letter-spacing-mobile);
-	--font-weight-desktop: var( --primary-xxl-font-weight-desktop, var( --primary-xxl-font-weight-mobile ) );
-	--font-weight-mobile: var(--is-desktop) var( --primary-xxl-font-weight-mobile);
-	--font-style-desktop: var( --primary-xxl-font-style-desktop, var( --primary-xxl-font-style-mobile ) );
-	--font-style-mobile: var(--is-desktop) var( --primary-xxl-font-style-mobile);
-
-	// Define this token locally; it can be defined via larva-tokens, if needed in the future.
-	--font-family: var( --font-family-primary );
-
 	font-family: var( --font-family );
+	font-size: var( --primary-xxl-font-size-mobile );
+	font-weight: var( --primary-xxl-font-weight-mobile );
+	font-style: var( --primary-xxl-font-style-mobile );
+	line-height: var(--primary-xxl-line-height-mobile );
+	letter-spacing: var( --primary-xxl-letter-spacing-mobile );
 
-	font-size: var(
-		--font-size-mobile,
-		var(--font-size-desktop)
-	);
-	line-height: var(
-		--line-height-mobile,
-		var(--line-height-desktop)
-	);
-	letter-spacing: var(
-		--letter-spacing-mobile,
-		var(--letter-spacing-desktop)
-	);
-	font-weight: var(
-		--font-weight-mobile,
-		var(--font-weight-desktop)
-	);
-	font-style: var(
-		--font-style-mobile,
-		var(--font-style-desktop)
-	);
+
+	@media (min-width: 800px) {
+		font-family: var( --font-family );
+		font-size: var( --primary-xxl-font-size-desktop );
+		font-weight: var( --primary-xxl-font-weight-desktop );
+		font-style: var( --primary-xxl-font-style-desktop );
+		line-height: var(--primary-xxl-line-height-desktop );
+		letter-spacing: var( --primary-xxl-letter-spacing-desktop );
+	}
+
+	@media (min-width: 1200px) {
+		font-family: var( --font-family );
+		font-size: var( --primary-xxl-font-size-desktopxxl );
+		font-weight: var( --primary-xxl-font-weight-desktopxxl );
+		font-style: var( --primary-xxl-font-style-desktopxxl );
+		line-height: var(--primary-xxl-line-height-desktopxxl );
+		letter-spacing: var( --primary-xxl-letter-spacing-desktopxxl );
+	}
 }\n`,
 };
