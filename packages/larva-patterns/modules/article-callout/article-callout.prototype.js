24cfb17f99077d0d3afc768b21a61edc074c9a56
Rich Salvucci <rsalvucci@pmc.com>
2022-03-10 15:42:00 -0500

    Add Article Callout Module

---
+++ packages/larva-patterns/modules/article-callout/article-callout.prototype.js
@@ -0,0 +1,5 @@
+module.exports = {
+    article_callout_classes: '',
+    article_callout_title: 'Update',
+    article_callout_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+};
