c2bab20912c357e8305ed800dc51357acdb6077e
Akash Dhawade <akash.dhawade@multidots.com>
2022-04-08 18:04:19 +0530

    Feedback changes

--- packages/larva-patterns/modules/content-showcase/content-showcase.prototype.js
+++ packages/larva-patterns/modules/content-showcase/content-showcase.prototype.js
@@ -1,23 +1,29 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
-const c_lazy_image = clonedeep( require( '@penskemediacorp/larva-patterns/components/c-lazy-image/c-lazy-image.prototype' ) );
-const c_title = clonedeep( require( '@penskemediacorp/larva-patterns/components/c-title/c-title.prototype' ) );
-const c_dek = clonedeep( require( '@penskemediacorp/larva-patterns/components/c-dek/c-dek.prototype' ) );
-const c_link = clonedeep( require( '@penskemediacorp/larva-patterns/components/c-link/c-link.prototype' ) );
+const o_card = clonedeep( require( '@penskemediacorp/larva-patterns/objects/o-card/o-card.prototype' ) );
+const c_dek =  clonedeep( require( '@penskemediacorp/larva-patterns/components/c-dek/c-dek.prototype' ) );
 
-c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-1x1';
-c_lazy_image.modifier_class = 'u-width-100p lrv-u-margin-l-auto';
+o_card.o_card_classes = 'lrv-u-display-inline-flex lrv-u-flex-basis-100p@desktop-xl lrv-u-flex-basis-100p@desktop lrv-u-padding-tb-075@mobile-max';
+o_card.o_card_link_classes = 'lrv-u-flex';
+o_card.o_card_link_url = '#';
+o_card.o_card_image_wrap_classes = 'lrv-a-flex-basis-40p@desktop lrv-a-flex-basis-40p@desktop-xl lrv-a-flex-basis-25p@mobile-max';
+o_card.o_card_content_classes = 'lrv-u-margin-l-1 lrv-u-color-brand-primary:hover lrv-a-flex-basis-60p@desktop lrv-a-flex-basis-60p@desktop-xl lrv-a-flex-basis-55p@mobile-max';
 
-c_link.c_link_classes = 'a-font-body-l lrv-a-floated-left lrv-a-hover-effect-text-decoration--underline lrv-u-margin-b-00'
+o_card.c_lazy_image.c_lazy_image_link_url = '';
+o_card.c_lazy_image.c_lazy_image_classes = 'lrv-u-margin-l-auto';
+o_card.c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-1x1';
 
-c_title.c_title_url = '';
-c_title.c_title_tag_text = 'h1';
-c_title.c_title_classes += ' lrv-u-text-transform-uppercase a-font-basic-s';
+o_card.o_span_group = false;
+o_card.c_span = false;
+o_card.c_title.c_title_url = '';
+o_card.c_timestamp = false;
+o_card.c_tagline = false;
+o_card.o_author = false;
+
+o_card.c_dek = c_dek;
+o_card.c_dek.c_dek_text = 'Perfection is rarely achieved in movies, but this heaven-sent concert doc hits the sweet spot.';
 
 module.exports = {
-    content_showcase_item_classes: 'content-showcase-item lrv-u-display-inline-flex lrv-u-flex-basis-100p@desktop-xl lrv-u-flex-basis-100p@desktop lrv-u-padding-tb-075@mobile-max',  
-    c_lazy_image,
-    c_link,
-    c_title,
-    c_dek,
+    content_showcase_items_classes: 'lrv-u-flex lrv-u-flex-direction-column@mobile-max lrv-u-padding-tb-1@desktop lrv-u-width-100p lrv-u-padding-lr-125@mobile-max',
+    content_showcase_items: [ o_card, o_card, o_card ]
 }
