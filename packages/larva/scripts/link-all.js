a19f5e83859036646c01b82536ec5378af3368c8
James Burke <james.walter.burke@gmail.com>
2022-04-28 09:44:44 -0500

    Add script for linking packages in Larva

---
+++ packages/larva/scripts/link-all.js
@@ -0,0 +1,6 @@
+var shell = require( 'shelljs' );
+
+shell.exec( 'npm link @penskemediacorp/larva' );
+shell.exec( 'npm link @penskemediacorp/larva-patterns' );
+shell.exec( 'npm link @penskemediacorp/larva-css' );
+shell.exec( 'npm link @penskemediacorp/larva-js' );
