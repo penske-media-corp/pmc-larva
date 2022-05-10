73e29ec20d937a06c6815ba57f2b24629e7905db
Ebonie <104318630+eboniebutler@users.noreply.github.com>
2022-05-10 15:29:48 -0400

    Hotfix: Use proper variable naming to apply appropriate escaping (#541)
    * hotfix
    
    * update var names
    
    * npm run prod
    
    * deconflict naming
    
    * add code comment
    
    * update naming

--- packages/larva-patterns/objects/o-text-input-group/o-text-input-group.prototype.js
+++ packages/larva-patterns/objects/o-text-input-group/o-text-input-group.prototype.js
@@ -1,9 +1,12 @@
+ /**
+ * Properties intentionally not prefixed with module name to avoid twig-to-php parser issues.
+ */
 const clonedeep = require( 'lodash.clonedeep' );
 const c_text_input = clonedeep(require('@penskemediacorp/larva-patterns/components/c-text-input/c-text-input.prototype'));
 
 module.exports = {
-	o_text_input_group_classes: '',
-	o_text_input_group_items: [
+	input_group_class: '',
+	input_group_items: [
 		c_text_input,
 		c_text_input,
 		c_text_input
