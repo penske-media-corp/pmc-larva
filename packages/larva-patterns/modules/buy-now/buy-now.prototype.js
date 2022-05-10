ce5a8da71dca50696c167d1321300e879aba4965
David Parsons <djp424@gmail.com>
2022-05-10 16:48:13 -0600

    “test”

--- packages/larva-patterns/modules/buy-now/buy-now.prototype.js
+++ packages/larva-patterns/modules/buy-now/buy-now.prototype.js
@@ -7,13 +7,12 @@ const c_lazy_image = clonedeep( c_lazy_image_prototype );
 
 c_lazy_image.c_lazy_image_srcset_attr = false;
 c_lazy_image.c_lazy_image_sizes_attr = false;
-c_lazy_image.c_lazy_image_classes += ' lrv-u-width-185';
 c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-1x1';
 
 // Button
 const button = clonedeep( require( '@penskemediacorp/larva-patterns/components/c-button/c-button.bordered' ) );
 
-button.c_button_classes += ' a-buy-button';
+button.c_button_classes += ' lrv-a-buy-now-button';
 button.c_button_screen_reader_text = 'Buy Now';
 button.c_button_text = 'Buy Now';
 
