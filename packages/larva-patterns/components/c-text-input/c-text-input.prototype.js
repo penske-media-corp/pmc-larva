6315bae8753ea68ea2ee43ef7b05b6b985faea4a
Ebonie Butler <ebutler@pmc.com>
2022-05-10 13:32:19 -0400

    update var names

--- packages/larva-patterns/components/c-text-input/c-text-input.prototype.js
+++ packages/larva-patterns/components/c-text-input/c-text-input.prototype.js
@@ -1,8 +1,8 @@
 module.exports = {
-	c_text_input_id: 'inputExample',
+	c_text_input_id_attr: 'inputExample',
 	c_text_input_label_classes: 'lrv-a-screen-reader-only',
 	c_text_input_label_text: 'Input Example',
 	c_text_input_classes: '',
 	c_text_input_name: 'input_example',
-	c_text_input_placeholder: 'Enter text.',
+	c_text_input_placeholder_attr: 'Enter text.',
 };
