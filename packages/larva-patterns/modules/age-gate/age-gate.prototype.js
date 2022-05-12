032810790074da9ecf2e90f8ca5b25cc971352a6
Ebonie Butler <ebutler@pmc.com>
2022-05-12 08:58:07 -0400

    add font classes and optional svg footer

--- packages/larva-patterns/modules/age-gate/age-gate.prototype.js
+++ packages/larva-patterns/modules/age-gate/age-gate.prototype.js
@@ -1,20 +1,21 @@
 const clonedeep = require('lodash.clonedeep');
 
-const c_svg = clonedeep(require('@penskemediacorp/larva-patterns/components/c-svg/c-svg.prototype'));
+const c_svg_prototype = require('@penskemediacorp/larva-patterns/components/c-svg/c-svg.prototype');
 const c_heading = clonedeep(require('@penskemediacorp/larva-patterns/components/c-heading/c-heading.prototype'));
 const o_input_group = clonedeep(require('@penskemediacorp/larva-patterns/objects/o-input-group/o-input-group.prototype'));
 const c_button = clonedeep(require('@penskemediacorp/larva-patterns/components/c-button/c-button.prototype'));
 const c_dek = clonedeep(require('@penskemediacorp/larva-patterns/components/c-dek/c-dek.prototype'));
 
-c_svg.c_svg_name = 'brand-logo-white';
-c_svg.c_svg_classes = 'lrv-u-width-150 lrv-u-margin-lr-auto lrv-u-margin-b-2';
+const c_svg_header = clonedeep(c_svg_prototype);
+c_svg_header.c_svg_name = 'brand-logo-white';
+c_svg_header.c_svg_classes = 'lrv-u-width-150 lrv-u-margin-lr-auto lrv-u-margin-b-2';
 
-c_heading.c_heading_classes += ' lrv-u-margin-b-2';
+c_heading.c_heading_classes += ' lrv-a-font-primary-l lrv-u-margin-b-2';
 c_heading.c_heading_text = 'You must be 21 to enter.';
 
 o_input_group.o_input_group_class = 'lrv-u-margin-b-2';
 o_input_group_item = clonedeep( o_input_group.o_input_group_items[0] );
-o_input_group_item.c_input_class = 'lrv-u-font-size-32 lrv-u-color-white lrv-u-background-color-grey-darkest lrv-u-padding-lr-050 lrv-u-padding-tb-150 lrv-u-width-100 lrv-u-text-align-center';
+o_input_group_item.c_input_class = 'lrv-a-font-primary-m lrv-u-color-white lrv-u-background-color-grey-darkest lrv-u-padding-lr-050 lrv-u-padding-tb-150 lrv-u-width-100 lrv-u-text-align-center';
 o_input_group_item.c_input_size_attr = 2;
 o_input_group_item.c_input_maxlength_attr = 4;
 
@@ -46,7 +47,8 @@ o_input_group.o_input_group_items = [
 	o_input_year_item
 ];
 
-c_button.c_button_classes = ' lrv-u-color-white lrv-u-background-color-brand-primary lrv-u-padding-lr-2 lrv-u-padding-tb-075 lrv-u-margin-b-2 lrv-u-text-align-center lrv-u-text-transform-uppercase lrv-u-font-size-32 lrv-u-font-size-18@mobile-max';
+c_button.c_button_text = 'View';
+c_button.c_button_classes = ' lrv-u-color-white lrv-u-background-color-brand-primary lrv-u-padding-lr-2 lrv-u-padding-tb-075 lrv-u-margin-b-2 lrv-u-text-align-center lrv-u-text-transform-uppercase lrv-a-font-primary-m';
 
 c_dek.c_dek_text = 'Legal text.'
 
@@ -58,7 +60,7 @@ module.exports = {
 	form_wrapper_class: '',
 	age_gate_footer_class: 'lrv-u-border-t-1 lrv-u-margin-t-025',
 	c_heading,
-	c_svg,
+	c_svg_header,
 	o_input_group,
 	c_button,
 	c_dek
