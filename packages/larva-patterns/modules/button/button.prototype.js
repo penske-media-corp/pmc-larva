7aca5ed4d8795606bf61aebcae7220ce0cae9533
David Parsons <djp424@gmail.com>
2022-05-12 13:43:47 -0600

    restore button module

---
+++ packages/larva-patterns/modules/button/button.prototype.js
@@ -0,0 +1,9 @@
+module.exports = {
+	button_url: null,
+	button_classes: 'lrv-u-padding-tb-050 lrv-u-padding-lr-1',
+	button_markup: 'Button Text',
+	button_background_color_class: 'lrv-u-background-color-brand-primary',
+	button_color_class: 'lrv-u-color-white',
+	button_typography_class: '',
+	button_width_class: '',
+};
