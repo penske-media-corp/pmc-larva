5b3ad6add185fdbcd37d3288ba636f94f5bd70a5
David Parsons <djp424@gmail.com>
2022-05-18 15:10:25 -0600

    font family update

--- packages/larva-patterns/modules/buy-now/buy-now.prototype.js
+++ packages/larva-patterns/modules/buy-now/buy-now.prototype.js
@@ -1,14 +1,5 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
-// Buy now options
-const buy_now = {
-	div_container_classes: 'lrv-a-buy-now',
-	hr_top_classes: 'lrv-a-buy-now__hr lrv-a-buy-now__hr--top',
-	img_container_classes: 'lrv-a-buy-now__image',
-	hr_bottom_classes: 'lrv-a-buy-now__hr lrv-a-buy-now__hr--bottom',
-	p_container_classes: 'lrv-a-buy-now__p',
-}
-
 // Image
 const c_lazy_image_prototype = require( '@penskemediacorp/larva-patterns/components/c-lazy-image/c-lazy-image.prototype' );
 
@@ -21,7 +12,7 @@ c_lazy_image.c_lazy_image_crop_class = 'lrv-a-crop-1x1';
 // Button
 const button = clonedeep( require( '@penskemediacorp/larva-patterns/components/c-button/c-button.bordered' ) );
 
-button.c_button_classes += ' lrv-a-buy-now__button';
+button.c_button_classes += ' lrv-a-buy-now__button lrv-u-font-size-14 lrv-u-font-weight-bold';
 button.c_button_screen_reader_text = 'Buy Now';
 button.c_button_text = 'Buy Now';
 button.c_button_target_attr = '_blank';
@@ -29,7 +20,11 @@ button.c_button_rel_attr = 'nofollow';
 
 // Export
 module.exports = {
-	buy_now,
+	buy_now_div_container_classes: 'lrv-a-buy-now lrv-u-font-family-basic',
+	buy_now_hr_top_classes: 'lrv-a-buy-now__hr lrv-a-buy-now__hr--top',
+	buy_now_img_container_classes: 'lrv-a-buy-now__image',
+	buy_now_hr_bottom_classes: 'lrv-a-buy-now__hr lrv-a-buy-now__hr--bottom',
+	buy_now_p_container_classes: 'lrv-a-buy-now__p lrv-u-font-family-basic lrv-u-font-size-14',
 	c_lazy_image,
 	button,
 };
