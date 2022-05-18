1872364fc48820c56496ade96f70cb4347dac871
jomurgel <>
2022-05-18 11:06:44 -0600

    concatonates narrow classname rather than replacing

--- packages/larva-patterns/modules/paragraph/paragraph.narrow.js
+++ packages/larva-patterns/modules/paragraph/paragraph.narrow.js
@@ -3,6 +3,6 @@ const clonedeep = require('lodash.clonedeep');
 const paragraph_prototype = require( './paragraph.prototype' );
 const paragraph_narrow = clonedeep( paragraph_prototype );
 
-paragraph_narrow.paragraph_classes = 'lrv-u-width-75p';
+paragraph_narrow.paragraph_classes += 'lrv-u-width-75p';
 
 module.exports = paragraph_narrow;
