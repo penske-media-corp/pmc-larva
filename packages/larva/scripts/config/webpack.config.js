49f9802c647272fbf94982a793ca1e72991ad8fa
Erick Hitter <git-contrib@ethitter.com>
2022-06-03 09:32:24 -0700

    Support webpack externals in Larva config
    If a Larva script will use a package provided by WordPress, such as lodash or
    any `@wordpress`-prefixed package, it should be declared as an external in the
    Larva config otherwise webpack will either error because it can't resolve the
    module or will require that we bundle one of those packages in the module. By
    allowing consuming projects to specify externals, those packages can be
    sourced from WordPress.

--- packages/larva/scripts/config/webpack.config.js
+++ packages/larva/scripts/config/webpack.config.js
@@ -14,6 +14,8 @@ const aliases = {
 	... getConfig( 'webpack' ).aliases
 };
 
+const externals = getConfig( 'webpack' ).externals ?? {};
+
 const eslintConfig = getConfig( 'eslint', true );
 const eslintConfigFile = undefined !== eslintConfig ? eslintConfig.configFile : null;
 
@@ -71,6 +73,7 @@ const config = {
 	resolve: {
 		alias: aliases
 	},
+	externals,
 	optimization: {
 		minimize: true
 	}
