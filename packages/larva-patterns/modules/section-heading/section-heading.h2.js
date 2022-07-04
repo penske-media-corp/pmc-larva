8595880e45783c7dbd3a7f5b59790d4ac8108035
Ebonie Butler <ebutler@pmc.com>
2022-07-04 13:35:07 -0400

    update larva patterns

--- packages/larva-patterns/modules/section-heading/section-heading.h2.js
+++ packages/larva-patterns/modules/section-heading/section-heading.h2.js
@@ -2,6 +2,8 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const section_heading = clonedeep( require( './section-heading.prototype' ) );
 
+section_heading.section_heading_class = 'lrv-u-border-t-3 lrv-u-padding-t-1';
 section_heading.c_dek = false;
+
 
 module.exports = section_heading;
--- packages/larva-patterns/modules/section-heading/section-heading.h3.js
+++ packages/larva-patterns/modules/section-heading/section-heading.h3.js
@@ -2,6 +2,7 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const section_heading = clonedeep( require( './section-heading.prototype' ) );
 
+section_heading.section_heading_class = 'lrv-u-border-t-3 lrv-u-padding-t-1';
 section_heading.section_heading_level_text = '3';
 section_heading.section_heading_typography_class = 'lrv-a-font-secondary-xl';
 section_heading.c_dek = false;
