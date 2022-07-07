0ca23408c137780043078ab4aeeeea826baaede2
Ramiz Manked <ramiz.manked@gmail.com>
2022-07-07 14:49:04 +0530

    Add table module

---
+++ packages/larva-patterns/modules/table/table.prototype.js
@@ -0,0 +1,8 @@
+module.exports = {
+	table_classes: '',
+	table_typography_class: '',
+	table_text_align_class: '',
+	table_color_class: '',
+	table_background_color_class: '',
+	table_markup: 'Table content.',
+};
