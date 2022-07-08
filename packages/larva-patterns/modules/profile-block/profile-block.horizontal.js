ae3ba28af57cf6a13fc911c4a841cf8f11f61009
Ebonie Butler <ebutler@pmc.com>
2022-07-08 10:13:17 -0400

    profile block WIP

--- packages/larva-patterns/modules/profile-block/profile-block.horizontal.js
+++ packages/larva-patterns/modules/profile-block/profile-block.horizontal.js
@@ -1,6 +1,10 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
-const profile_block_horizontal = clonedeep( require( './profile-blcok.prototype' ) );
+const profile_block_horizontal = clonedeep( require( './profile-block.prototype' ) );
+
+profile_block_horizontal.profile_block_classes = 'lrv-u-flex@tablet';
+
+profile_block_horizontal.profile_block_image_classes = 'lrv-u-margin-b-150@mobile-max lrv-u-margin-a-00@tablet lrv-u-width-25p@tablet'
 
 
 module.exports = profile_block_horizontal;
