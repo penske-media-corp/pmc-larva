7525e51312110f3b28d8eda5261ad40b9c301069
Jo Murgel <dev@jomurgel.com>
2022-07-19 14:46:40 -0600

    updates text output

--- packages/twig-to-php-parser/__tests__/fixtures/template-parts-expected/patterns/components/c-byline.php
+++ packages/twig-to-php-parser/__tests__/fixtures/template-parts-expected/patterns/components/c-byline.php
@@ -8,6 +8,5 @@
 ?>
 <p class="c-byline <?php echo esc_attr( $modifier_class ?? '' ); ?> <?php echo esc_attr( $c_byline_classes ?? '' ); ?>">
 	<?php echo esc_html( $c_byline_by_text ?? '' ); ?>
-
 	<?php echo wp_kses_post( $c_byline_markup ?? '' ); ?>
 </p>
