f8b86efc442deb755dd40fe3ec9dcd2da0551968
Jo Murgel <dev@jomurgel.com>
2022-07-19 14:58:09 -0600

    removes whitespace

--- packages/twig-to-php-parser/__tests__/fixtures/template-parts-expected/patterns/components/c-byline.php
+++ packages/twig-to-php-parser/__tests__/fixtures/template-parts-expected/patterns/components/c-byline.php
@@ -4,7 +4,6 @@
  *
  * Refer to the relevant Twig file for adjusting this markup.
  */
-
 ?>
 <p class="c-byline <?php echo esc_attr( $modifier_class ?? '' ); ?> <?php echo esc_attr( $c_byline_classes ?? '' ); ?>">
 	<?php echo esc_html( $c_byline_by_text ?? '' ); ?>
