5629a769be6b86eecf91d927826a423e226dce9c
Ramiz Manked <ramiz.manked@gmail.com>
2022-07-22 20:03:16 +0530

    Add utility classes for overflow, word-break

--- packages/larva-patterns/modules/table/table.prototype.js
+++ packages/larva-patterns/modules/table/table.prototype.js
@@ -1,8 +1,7 @@
 module.exports = {
 	table_background_color_class: '',
-	table_classes: '',
+	table_classes: 'lrv-u-overflow-x-auto',
 	table_color_class: '',
-	table_layout_class: '',
 	table_markup: 'Table content.',
 	table_text_align_class: '',
 };
