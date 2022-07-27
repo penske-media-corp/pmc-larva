ce1cce1e0cda02b454783d08ce16d4efebb5dcfb
Deepak Lalwani <deepak.lalwani81@gmail.com>
2022-07-27 15:14:03 +0530

    Add custom event for first video play

--- packages/larva-js/src/interface/VideoShowcase/VideoShowcase.js
+++ packages/larva-js/src/interface/VideoShowcase/VideoShowcase.js
@@ -282,6 +282,9 @@ export default class VideoShowcase {
 	// Remove any trigger-related data attributes and hide any elements that are not relevant for the player.
 	onFirstTimePlay() {
 		if ( false === this.state.isPlayerSetup ) {
+
+			document.dispatchEvent( new CustomEvent('firstVideoPlay') );
+
 			this.elementsToHide.forEach( e => e.setAttribute( 'hidden', '' ) );
 			this.attributesToRemoveFromPlayer.forEach( attr => this.player.parentNode.removeAttribute( attr ) );
 			this.state.isPlayerSetup = true;
