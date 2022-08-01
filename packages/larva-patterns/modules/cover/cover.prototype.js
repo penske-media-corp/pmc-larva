9ec76320e993797c38b2821ecdc80f1f601ed504
Ramiz Manked <ramiz.manked@gmail.com>
2022-08-01 20:25:30 +0530

    Remove cover wrapper classes

--- packages/larva-patterns/modules/cover/cover.prototype.js
+++ packages/larva-patterns/modules/cover/cover.prototype.js
@@ -1,4 +1,4 @@
 module.exports = {
-	cover_classes: 'lrv-u-align-items-center lrv-u-background-position-50p lrv-u-background-size-cover lrv-u-flex lrv-u-justify-content-center lrv-u-margin-lr-auto lrv-u-min-height-430 lrv-u-padding-a-1 lrv-u-position-relative lrv-u-width-100plrv-u-margin-lr-auto',
+	cover_classes: '',
 	cover_markup: 'Cover markup.',
 };
