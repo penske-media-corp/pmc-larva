1847ff3aa10f65881d9ddb8eb5a9ec8284c9b693
Rich Salvucci <rsalvucci@pmc.com>
2022-08-04 13:31:21 -0400

    Updating packages

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,13 @@
 module.exports = {
 	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	"overrides": [{
+		"files": [
+			"**/*.js"
+		],
+		"rules": {
+			"jsdoc/require-returns-description": "off",
+			"jsdoc/check-line-alignment": "always",
+			"@wordpress/no-global-event-listener": "off"
+		},
+	}],
 };
