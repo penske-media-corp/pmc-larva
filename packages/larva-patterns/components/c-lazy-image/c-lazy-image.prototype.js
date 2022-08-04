614b9e7dd2d7312d7547c3de5beedbd1bcaeaaac
ldanielgiuliani <ldanielgiuliani@gmail.com>
2022-08-04 09:57:10 +0800

    Add image decoding attribute support

--- packages/larva-patterns/components/c-lazy-image/c-lazy-image.prototype.js
+++ packages/larva-patterns/components/c-lazy-image/c-lazy-image.prototype.js
@@ -13,5 +13,23 @@ module.exports = {
 	c_lazy_image_img_classes: 'lrv-u-background-color-grey-lightest lrv-u-width-100p lrv-u-display-block lrv-u-height-auto',
 	c_lazy_image_height_attr: '',
 	c_lazy_image_width_attr: '',
+	c_lazy_image_in_initial_viewport: false,
+	c_lazy_image_decoding_attr: 'async'
+}
+module.exports = {
+	c_lazy_image_classes: '',
+	c_lazy_image_link_url: false,
+	c_lazy_image_link_classes: 'lrv-a-unstyle-link',
+	c_lazy_image_crop_class: 'lrv-a-crop-16x9',
+	c_lazy_image_crop_style_attr: false,
+	c_lazy_image_link_tab_index_attr: false,
+	c_lazy_image_alt_attr: 'Lazy loaded image',
+	c_lazy_image_src_url: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg',
+	c_lazy_image_placeholder_url: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+	c_lazy_image_srcset_attr: 'https://farm5.staticflickr.com/4078/5441060528_31db7838ba_m.jpg 240w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_n.jpg 320w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba.jpg 500w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_z.jpg 640w,https://farm5.staticflickr.com/4078/5441060528_31db7838ba_b.jpg 1024w',
+	c_lazy_image_sizes_attr: 'auto',
+	c_lazy_image_img_classes: 'lrv-u-background-color-grey-lightest lrv-u-width-100p lrv-u-display-block lrv-u-height-auto',
+	c_lazy_image_height_attr: '',
+	c_lazy_image_width_attr: '',
 	c_lazy_image_in_initial_viewport: false
 }
