247e014cc2d888989120d53b32a4c5bd28c5ce25
Ramiz Manked <ramiz.manked@gmail.com>
2022-08-11 23:49:44 +0530

    Feature/PMCP-4050: Create Larva Pattern for `core/gallery` Block. (#650)
    * Use grid system instead flex on gallery
    
    * Update changelog

--- packages/larva-patterns/modules/gallery/gallery.prototype.js
+++ packages/larva-patterns/modules/gallery/gallery.prototype.js
@@ -1,5 +1,5 @@
 module.exports = {
-	gallery_classes: 'lrv-u-flex lrv-u-flex-wrap-wrap lrv-u-margin-lr-auto',
+	gallery_classes: 'lrv-a-grid',
 	gallery_align_class: '',
 	gallery_columns_class: '',
 	gallery_markup: 'Gallery markup.',
