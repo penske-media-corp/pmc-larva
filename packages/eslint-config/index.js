42e4370c6a4b19936abb7e124974573263e17892
Pattie Reaves <preaves@pmc.com>
2022-08-18 12:39:40 -0700

    Get to the eslint parsing error.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,13 +1,17 @@
 module.exports = {
 	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
-	"overrides": [{
-		"files": [
-			"**/*.js"
-		],
-		"rules": {
-			"jsdoc/require-returns-description": "off",
-			"jsdoc/check-line-alignment": 1,
-			"@wordpress/no-global-event-listener": "off"
+	parser: "@babel/eslint-parser",
+	requireConfigFile:" false",
+	ecmaVersion: 8,
+	overrides: [
+		{
+			files: [ '**/*.js' ],
+			rules: {
+				'jsdoc/require-returns-description': 'off',
+				'jsdoc/check-line-alignment': 1,
+				'@wordpress/no-global-event-listener': 'off',
+			},
 		},
-	}],
+	],
+	root: true,
 };
