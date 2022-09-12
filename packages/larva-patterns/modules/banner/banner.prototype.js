ed82ee18fd04dfef9b921abf91d80102556b5a05
rutviksavsani <savsanirutviklivetolearn@gmail.com>
2022-09-13 01:16:12 +0530

    Update Banner module to accomodate c_tatgline.

--- packages/larva-patterns/modules/banner/banner.prototype.js
+++ packages/larva-patterns/modules/banner/banner.prototype.js
@@ -3,8 +3,12 @@ const clonedeep = require( 'lodash.clonedeep' );
 const c_lazy_image_prototype = require( '../../components/c-lazy-image/c-lazy-image.prototype' );
 const c_lazy_image = clonedeep( c_lazy_image_prototype );
 
+const c_tagline_prototype = require( '@penskemediacorp/larva-patterns/components/c-tagline/c-tagline.prototype' );
+const c_tagline = clonedeep( c_tagline_prototype );
+
 module.exports = {
 	banner_classes: '',
 	c_lazy_image,
+	c_tagline,
 	screen_reader_text: 'Page Banner',
 };
