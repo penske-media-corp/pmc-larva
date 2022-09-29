8438e7d2f4f2cb8b06f861be373ef9e519e0b603
Jordan Pakrosnis <jpakrosnis@pmc.com>
2022-09-29 22:11:25 +0000

    Populate default tagline_terms in footer-newsletter prototype

--- packages/larva-patterns/modules/footer-newsletter/footer-newsletter.prototype.js
+++ packages/larva-patterns/modules/footer-newsletter/footer-newsletter.prototype.js
@@ -6,9 +6,10 @@ const o_email_capture_form_minimal = clonedeep(
 const c_title = clonedeep(
 	require( '../../components/c-title/c-title.prototype' )
 );
-const c_tagline = clonedeep(
-	require( '../../components/c-tagline/c-tagline.prototype' )
-);
+
+const c_tagline_prototype = require( '../../components/c-tagline/c-tagline.prototype' );
+const c_tagline = clonedeep( c_tagline_prototype );
+const c_tagline_terms = clonedeep( c_tagline_prototype );
 
 c_title.c_title_url = false;
 c_title.c_title_text = 'Stay In The Know';
@@ -19,6 +20,8 @@ c_tagline.c_tagline_text = 'Sign up for our daily newsletter';
 c_tagline.c_tagline_classes =
 	'lrv-u-font-size-14 u-font-style-italic lrv-u-font-family-basic lrv-u-text-align-center';
 
+c_tagline_terms.c_tagline_text = '';
+
 o_email_capture_form_minimal.c_button.c_button_classes += ' lrv-a-icon-invert';
 
 module.exports = {
@@ -26,5 +29,6 @@ module.exports = {
 		'lrv-u-text-align-center lrv-u-color-white lrv-u-background-color-black lrv-u-flex lrv-u-flex-direction-column lrv-u-align-items-center',
 	c_title,
 	c_tagline,
+	c_tagline_terms,
 	o_email_capture_form: o_email_capture_form_minimal,
 };
