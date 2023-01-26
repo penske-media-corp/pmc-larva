999bd490a99d980bf9e1d9d34e481911d38b9796
Nayeon Kim <2328709+nayeonk@users.noreply.github.com>
2023-01-26 12:32:19 -0800

    Add hubs variant for pullquote module

---
+++ packages/larva-patterns/modules/pullquote/pullquote.hubs.js
@@ -0,0 +1,6 @@
+const clonedeep = require( 'lodash.clonedeep' );
+const pullquote = clonedeep( require( './pullquote.prototype' ) );
+
+pullquote.pullquote_classes = 'hubs-pullquote lrv-u-width-100p lrv-u-margin-a-00';
+
+module.exports = pullquote;
