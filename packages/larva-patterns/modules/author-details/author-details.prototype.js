1d1b1b4455b06be721cbcfdba3a9409aa1e7f449
Nayeon Kim <2328709+nayeonk@users.noreply.github.com>
2023-04-11 12:05:42 -0700

    Reorder css class names per wordpress css standards

--- packages/larva-patterns/modules/author-details/author-details.prototype.js
+++ packages/larva-patterns/modules/author-details/author-details.prototype.js
@@ -29,18 +29,18 @@ for ( let i = 0; i < storyTitles.length; i++ ) {
 }
 
 c_title.c_title_classes =
-	'lrv-u-font-size-13 lrv-u-font-weight-bold lrv-u-margin-r-auto lrv-u-text-transform-uppercase lrv-u-width-100';
+	'lrv-u-margin-r-auto lrv-u-width-100 lrv-u-font-size-13 lrv-u-font-weight-bold lrv-u-text-transform-uppercase';
 c_title.c_title_url = '#';
 c_title.c_title_text = 'PATRICIA MESCHINO';
 c_title.c_title_link_classes = 'lrv-u-color-black';
 
 o_nav.o_nav_classes =
-	'lrv-u-border-t-1 lrv-u-width-100p lrv-u-margin-t-075 lrv-u-padding-t-025 ';
+	'lrv-u-margin-t-075 lrv-u-border-t-1 lrv-u-padding-t-025 lrv-u-width-100p ';
 o_nav.o_nav_title_text = '';
 o_nav.o_nav_title_classes =
 	'lrv-u-padding-t-050 lrv-u-padding-b-025 lrv-u-font-family-primary lrv-u-font-size-20';
 o_nav.o_nav_list_classes =
-	' lrv-u-font-size-13 lrv-u-border-t-1 lrv-u-border-b-1 lrv-u-padding-l-1 lrv-u-padding-t-075  lrv-u-list-style-type-disclosure-closed';
+	' lrv-u-border-t-1 lrv-u-border-b-1 lrv-u-padding-t-075 lrv-u-padding-l-1 lrv-u-font-size-13 lrv-u-list-style-type-disclosure-closed';
 o_nav.o_nav_list_item_classes = 'lrv-u-padding-b-075';
 
 c_icon_twitter.c_icon_name = 'twitter';
@@ -61,13 +61,13 @@ c_icon_mail.c_icon_link_classes = 'lrv-u-flex';
 
 c_link_view_all = clonedeep( c_link_prototype );
 c_link_view_all.c_link_classes =
-	'lrv-a-unstyle-link lrv-u-font-size-10 lrv-u-text-transform-uppercase';
+	'lrv-u-font-size-10 lrv-u-text-transform-uppercase lrv-a-unstyle-link';
 c_link_view_all.c_link_text = 'View All Stories ▶';
 c_link_view_all.c_link_url = '#';
 
 module.exports = {
 	author_details_classes:
-		'lrv-u-padding-lr-125 lrv-u-padding-t-125 lrv-u-padding-b-1 lrv-u-width-215 lrv-u-flex lrv-u-flex-wrap-wrap lrv-u-background-color-white lrv-u-border-a-2 lrv-a-glue-parent lrv-a-icon-after-arrow-top-left lrv-u-margin-t-1',
+		'lrv-u-flex lrv-u-flex-wrap-wrap lrv-u-margin-t-1 lrv-u-padding-lr-125 lrv-u-padding-t-125 lrv-u-padding-b-1 lrv-u-border-a-2 lrv-u-width-215 lrv-u-background-color-white lrv-a-glue-parent lrv-a-icon-after-arrow-top-left',
 	author_details_icons_classes:
 		'lrv-u-flex lrv-u-align-items-center lrv-u-margin-l-auto',
 	author_details_view_link_classes: ' lrv-u-flex lrv-u-margin-l-auto',
