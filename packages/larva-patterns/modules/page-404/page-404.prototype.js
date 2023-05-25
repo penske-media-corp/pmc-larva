c217b819bddaa668097d8194e4fde7886481d18a
Ramiz Manked <ramiz.manked@gmail.com>
2023-05-26 01:02:09 +0530

    Add smart-404 as a separate variant

--- packages/larva-patterns/modules/page-404/page-404.prototype.js
+++ packages/larva-patterns/modules/page-404/page-404.prototype.js
@@ -1,62 +1,49 @@
 const clonedeep = require( 'lodash.clonedeep' );
 
 const c_heading_prototype = require( '@penskemediacorp/larva-patterns/components/c-heading/c-heading.prototype' );
-const c_svg_prototype = require( '../../components/c-svg/c-svg.prototype.js' );
-const c_title_prototype = require( '@penskemediacorp/larva-patterns/components/c-title/c-title.prototype' );
-const c_tagline_prototype = require( '@penskemediacorp/larva-patterns/components/c-tagline/c-tagline.prototype' );
-const suggested_link_items_prototype = require( '@penskemediacorp/larva-patterns/modules/list/list.prototype' );
-const search_form_prototype = require( '@penskemediacorp/larva-patterns/modules/search-form/search-form.prototype' );
-
 const c_heading = clonedeep( c_heading_prototype );
-const c_suggestion_title = clonedeep( c_title_prototype );
-const c_search_title = clonedeep( c_title_prototype );
-const c_svg = clonedeep( c_svg_prototype );
-const c_title = clonedeep( c_title_prototype );
-const c_tagline = clonedeep( c_tagline_prototype );
-const suggested_link_items = clonedeep( suggested_link_items_prototype );
-const search_form = clonedeep( search_form_prototype );
 
+const c_svg_prototype = require( '../../components/c-svg/c-svg.prototype.js' );
+const c_svg = clonedeep( c_svg_prototype );
+
+const c_title_prototype = require( '@penskemediacorp/larva-patterns/components/c-title/c-title.prototype' );
+const c_title = clonedeep( c_title_prototype );
+
+const c_button_prototype = require( '@penskemediacorp/larva-patterns/components/c-button/c-button.prototype' );
+const c_button_home = clonedeep( c_button_prototype );
+const c_button_search = clonedeep( c_button_prototype );
+
+c_heading.c_heading_text = '404';
 c_heading.c_heading_classes = 'lrv-a-screen-reader-only';
 c_heading.c_heading_is_primary_heading = true;
-c_heading.c_heading_text = '404';
 
 c_svg.c_svg_name = 'logo-404';
 c_svg.c_svg_classes = 'lrv-u-max-width-100p';
 
 c_title.c_title_text = 'OOPS! Something went wrong here!';
-c_title.c_title_classes = 'lrv-a-font-secondary-l lrv-u-text-transform-uppercase lrv-u-margin-b-125';
+c_title.c_title_classes =
+	'lrv-a-font-secondary-l lrv-u-text-transform-uppercase lrv-u-margin-b-125';
 c_title.c_title_url = false;
 
-c_suggestion_title.c_title_classes = 'lrv-u-text-align-left lrv-u-margin-b-075';
-c_suggestion_title.c_title_text = 'Here are some suggestions that might be the page you were looking for:';
-c_suggestion_title.c_title_url = false;
+c_button_home.c_button_text = 'Back To Homepage';
+c_button_home.c_button_url = '/';
+c_button_home.c_button_classes +=
+	' lrv-u-background-color-black lrv-u-margin-lr-075 lrv-u-text-transform-uppercase lrv-u-font-weight-bold u-background-color-brand-primary-dark:hover lrv-u-color-white lrv-u-color-white:hover lrv-u-padding-tb-050 lrv-u-padding-lr-1';
 
-suggested_link_items.list_classes = 'lrv-u-text-align-left lrv-u-padding-b-1';
-suggested_link_items.list_items = [
-	{ list_markup: 'item_one' },
-	{ list_markup: 'item_two' },
-];
-
-c_search_title.c_title_classes = 'lrv-u-text-align-left';
-c_search_title.c_title_text = 'Or try searching for it here...';
-c_search_title.c_title_url = false;
-
-search_form.search_form_classes = 'lrv-u-text-align-left lrv-u-margin-t-1 search-form-404';
-search_form.search_form_input_classes = 'lrv-u-border-a-1 lrv-u-width-100p';
-search_form.search_form_action_url = '/?s=';
+c_button_search.c_button_text = 'Head To Search';
+c_button_search.c_button_url = '#';
+c_button_search.c_button_classes +=
+	' lrv-u-background-color-black lrv-u-margin-lr-075 lrv-u-text-transform-uppercase lrv-u-font-weight-bold u-background-color-brand-primary-dark:hover lrv-u-color-white lrv-u-color-white:hover lrv-u-padding-tb-050 lrv-u-padding-lr-1 lrv-u-margin-t-050@mobile-max';
 
 module.exports = {
-	button_outer_classes:
-		'lrv-u-flex lrv-u-justify-content-center lrv-u-align-items-center lrv-u-flex-direction-column@mobile-max',
 	c_heading,
-	c_suggestion_title,
-	c_search_title,
-	c_svg,
-	c_tagline,
 	c_title,
-	page_404_inner_classes: 'lrv-u-margin-lr-auto lrv-u-padding-lr-1',
-	search_form,
-	suggested_link_items,
+	c_button_home,
+	c_button_search,
+	c_svg,
 	wrapper_classes:
 		'lrv-u-text-align-center lrv-u-padding-t-1 lrv-u-padding-b-2 lrv-u-margin-b-2 lrv-u-max-width-830 lrv-u-margin-lr-auto',
+	page_404_inner_classes: 'lrv-u-margin-lr-auto lrv-u-padding-lr-1',
+	button_outer_classes:
+		'lrv-u-flex lrv-u-justify-content-center lrv-u-align-items-center lrv-u-flex-direction-column@mobile-max',
 };
