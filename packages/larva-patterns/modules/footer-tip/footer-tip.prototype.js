0ff42c6dddc8ce444aa574c23ea55e9b0c9c6e98
Jordan Pakrosnis <jpak@jordanpak.com>
2023-08-01 15:33:54 -0400

    Reorder footer-tip component classnames

--- packages/larva-patterns/modules/footer-tip/footer-tip.prototype.js
+++ packages/larva-patterns/modules/footer-tip/footer-tip.prototype.js
@@ -11,7 +11,7 @@ const c_tagline = clonedeep(
 );
 
 c_button.c_button_classes +=
-	'lrv-u-color-white lrv-u-color-grey-light:hover lrv-u-background-color-black lrv-u-font-size-12 lrv-u-font-weight-bold lrv-u-text-transform-uppercase lrv-a-icon-after lrv-a-icon-arrow-right lrv-a-icon-invert';
+	'lrv-u-font-size-12 lrv-u-text-transform-uppercase lrv-u-font-weight-bold lrv-u-color-white lrv-u-color-grey-light:hover lrv-u-background-color-black lrv-a-icon-after lrv-a-icon-arrow-right lrv-a-icon-invert';
 c_button.c_button_text = 'Send Us a Tip';
 c_button.c_button_url = 'https://google.com';
 c_button.c_button_inner_classes =
@@ -21,7 +21,7 @@ c_title.c_title_classes = 'lrv-u-margin-b-025 lrv-u-font-family-primary';
 c_title.c_title_text = 'Have a Tip?';
 
 c_tagline.c_tagline_classes =
-	'lrv-u-font-size-14 u-font-style-italic lrv-u-font-family-basic';
+	'lrv-u-font-size-14 lrv-u-font-family-basic u-font-style-italic';
 c_tagline.c_tagline_text =
 	'We want to hear from you! Send us a tip using our anonymous form.';
 
