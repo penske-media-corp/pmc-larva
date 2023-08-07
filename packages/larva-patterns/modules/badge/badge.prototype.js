1b741d4439108b4df6cdf19463178dd87c7d9b35
Nayeon Kim <2328709+nayeonk@users.noreply.github.com>
2023-08-07 16:58:45 -0700

    Reorder css classes for modules that start with 'b'

--- packages/larva-patterns/modules/badge/badge.prototype.js
+++ packages/larva-patterns/modules/badge/badge.prototype.js
@@ -4,7 +4,7 @@ const c_span_prototye = require( '../../components/c-span/c-span.prototype' );
 const c_span = clonedeep( c_span_prototye );
 
 c_span.c_span_classes +=
-	' lrv-u-background-color-grey-light lrv-u-font-size-12 lrv-u-padding-a-025';
+	' lrv-u-padding-a-025 lrv-u-background-color-grey-light lrv-u-font-size-12';
 c_span.c_span_text = 'Sponsored Video';
 
 module.exports = {
