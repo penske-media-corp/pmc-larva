bb0a1d4fcb8a96c7bcf4905fc181075d5179d14c
Jordan Pakrosnis <jpak@jordanpak.com>
2024-04-05 16:57:33 -0400

    Add helper for getting social-platforms JSON dataset

---
+++ packages/larva-patterns/utils/getSocialPlatforms.js
@@ -0,0 +1,10 @@
+/**
+ * Get social platform information
+ *
+ * @param {Array} platforms "name" property values of desired platforms (see ../_data/social-platforms.json).
+ */
+module.exports = function( platforms ) {
+	return require( './getDataSet' )( 'social-platforms', 'name',
+		undefined === platforms ? [ 'instagram', 'pinit', 'twitter', 'facebook', 'youtube' ] : platforms
+	);
+}
