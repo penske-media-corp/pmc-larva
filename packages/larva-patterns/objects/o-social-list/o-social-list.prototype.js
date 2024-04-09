878ee599b773b24e8046a8cc6a93f4dff8c82ce1
Jordan Pakrosnis <jpak@jordanpak.com>
2024-04-09 13:39:30 -0400

    PASE-2019: Add social (X) logos, social platform dataset, and improve icon accessibility (#842)
    * Remove unneeded ID attributes from social platform SVGs
    
    * Clean up PMC SVGs: Remove unneeded attributes and groups, inline CSS, etc
    
    * Swap twitter SVG with X logo
    
    * Rebuild SVG defs sprite
    
    * run prod
    
    * Rebuild SVGs (+ remove fill="none" from Twitter)
    
    * Re-run prod with non-filled X SVG
    
    * Add Mastodon, Threads, Tiktok, and Twitch logo SVGs
    
    * Rebuild SVGs
    
    * Rebuild sprites/CSS
    
    * Add getDataSet helper for _data JSON files
    
    * Add helper for getting social-platforms JSON dataset
    
    * Get social platforms from dataset and set proper human-readable label attributes
    
    * Add facebook-circle icon variant + remove "(Formerly Twitter)" from X label
    
    * Update readme
    
    * Adjust c-icon condition to work with PMC twig → PHP parser
    
    * Remove weird conditions from <a> screen reader span
    
    * Rebuild everything again whatever
    
    * Switch author-details social icon c_icon_screen_reader_text to c_icon_link_screen_reader_text
    
    * Add data-label attrs to hard-to-debug c-icon screen reader stuff
    
    * Rebuild everything again

--- packages/larva-patterns/objects/o-social-list/o-social-list.prototype.js
+++ packages/larva-patterns/objects/o-social-list/o-social-list.prototype.js
@@ -2,20 +2,22 @@ const clonedeep = require( 'lodash.clonedeep' );
 
 const c_icon_prototype = require( '../../components/c-icon/c-icon.prototype' );
 
-const social = [ 'instagram', 'pinit', 'twitter', 'facebook', 'youtube' ];
+const socialPlatforms = require( '../../utils/getSocialPlatforms' )();
 const o_social_list_icons = [];
 
-for ( let i = 0; i < social.length; i++ ) {
+socialPlatforms.forEach( ( { name, label } ) => {
 	const c_icon = clonedeep( c_icon_prototype );
 
 	c_icon.c_icon_url = '#';
-	c_icon.c_icon_name = social[ i ];
+	c_icon.c_icon_name = name;
 	c_icon.c_icon_link_classes +=
 		' lrv-u-display-block lrv-u-border-radius-50p lrv-u-color-grey lrv-u-margin-lr-050 lrv-u-padding-a-050 lrv-u-background-color-white lrv-u-color-grey-dark:hover';
 	c_icon.c_icon_rel_name = 'noopener noreferrer';
+	c_icon.c_icon_screen_reader_text = label;
+	c_icon.c_icon_screen_reader_title_attr = label;
 
 	o_social_list_icons.push( c_icon );
-}
+} );
 
 module.exports = {
 	o_social_list_classes: 'lrv-a-unstyle-list',
---
+++ packages/larva-patterns/utils/getDataSet.js
@@ -0,0 +1,24 @@
+/**
+ * Helper function to get larva-patterns/_data array by property
+ *
+ * @param {String} filename    JSON filenames in ../_data.
+ * @param {String} orderKey    Object property name to sort/order by.
+ * @param {Array}  orderValues Desired items from data file(s) (by orderKey property).
+ */
+module.exports = function( filename, orderKey, orderValues ) {
+	const data = require( `../_data/${ filename }.json` );
+
+	if ( orderKey && orderValues ) {
+		const sortedData = [];
+
+		orderValues.forEach( ( orderValue ) => {
+			sortedData.push(
+				data.find( ( values ) => values[ orderKey ] === orderValue )
+			);
+		} );
+
+		return sortedData;
+	} else {
+		return data;
+	}
+};
