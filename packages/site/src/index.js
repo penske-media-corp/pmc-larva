9852a2aa08784b2fde66017258528760293927e2
Jordan Pakrosnis <jpak@jordanpak.com>
2024-09-10 14:44:25 -0400

    Revert "Swap ReactDOM.render with React client createRoot"
    This reverts commit ea6639e26de949265bf4058ff635248c8c5bedf3.

--- packages/site/src/index.js
+++ packages/site/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { App } from './components/App';
 
-const container = document.getElementById( 'root' );
-const root = createRoot( container );
-root.render( <App /> );
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
