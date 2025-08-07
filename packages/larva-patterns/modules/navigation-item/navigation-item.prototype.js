d837dcf2e683f28f365ba3637e955c7adfc6bf45
Allan Collins <collinsinternet@gmail.com>
2025-08-07 10:38:13 -0600

    Update navigation-item.prototype.js
    Remove `u-` prefixed classes.

--- packages/larva-patterns/modules/navigation-item/navigation-item.prototype.js
+++ packages/larva-patterns/modules/navigation-item/navigation-item.prototype.js
@@ -8,7 +8,7 @@ const c_link__child = clonedeep( c_link );
 
 c_link__parent.c_link_text = 'Parent item';
 c_link__parent.c_link_classes =
-	'lrv-u-color-brand-primary:hover a-font-accent u-font-size-14 u-line-height-22 lrv-u-color-currentColor lrv-u-text-transform-uppercase u-letter-spacing-0187@mobile-max u-letter-spacing-0200@tablet lrv-a-hover-effect lrv-a-unstyle-link lrv-u-whitespace-nowrap lrv-u-display-inline-block u-font-weight-bold';
+	'lrv-u-color-brand-primary:hover lrv-u-color-currentColor lrv-u-text-transform-uppercase lrv-a-hover-effect lrv-a-unstyle-link lrv-u-whitespace-nowrap lrv-u-display-inline-block';
 
 c_link__child.c_link_text = 'Child item';
 c_link__child.c_link_classes =
@@ -28,7 +28,7 @@ module.exports = {
 		c_link__child,
 	],
 	navigation_child_list_classes:
-		'lrv-u-display-none lrv-a-unstyle-list a-font-accent lrv-a-glue lrv-a-glue--l-0 lrv-u-width-200 lrv-u-background-color-white u-max-width-330 u-max-width-300@mobile-max lrv-u-overflow-auto  u-box-shadow-03945 lrv-u-border-a-1 lrv-u-padding-lr-050 lrv-u-padding-tb-025 lrv-u-border-color-brand-accent u-box-shadow-03945',
+		'lrv-u-display-none lrv-a-unstyle-list lrv-a-glue lrv-a-glue--l-0 lrv-u-width-200 lrv-u-background-color-white lrv-u-overflow-auto lrv-u-border-a-1 lrv-u-padding-lr-050 lrv-u-padding-tb-025 lrv-u-border-color-brand-accent',
 	navigation_parent_list_classes:
 		'lrv-u-flex lrv-u-margin-lr-050 lrv-a-glue-parent lrv-u-margin-tb-025',
 	parent_list_item_classes: '',
