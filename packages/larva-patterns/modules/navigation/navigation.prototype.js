e23282ef80b883697fc808114b0d6718f65327d6
Allan Collins <collinsinternet@gmail.com>
2025-08-07 10:41:33 -0600

    Update navigation.prototype.js
    Remove `u-` prefixed classes.

--- packages/larva-patterns/modules/navigation/navigation.prototype.js
+++ packages/larva-patterns/modules/navigation/navigation.prototype.js
@@ -13,9 +13,9 @@ navigation_item.navigation_parent_list_classes =
 navigation_item.parent_list_item_heading_classes =
 	'lrv-u-flex lrv-u-justify-content-center lrv-u-padding-tb-050';
 navigation_item.parent_list_item_classes =
-	'lrv-u-width-100p@mobile-max lrv-u-justify-content-center u-border-color-wild-strawberry';
+	'lrv-u-width-100p@mobile-max lrv-u-justify-content-center';
 c_heading.c_heading_classes =
-	'lrv-a-glue-parent has-text-color u-font-weight-bold a-font-accent u-font-size-14 u-line-height-22 u-font-weight-bold u-color-wild-strawberry lrv-u-width-100p lrv-u-border-a-1 u-border-color-wild-strawberry u-letter-spacing-01875 lrv-js-MobileHeightToggle-trigger u-display-none@tablet lrv-u-padding-tb-050 lrv-u-justify-content-center lrv-u-text-transform-uppercase  lrv-a-icon-after a-icon-after-caret-down a-icon-height-8 a-icon-width-13';
+	'lrv-a-glue-parent has-text-color lrv-u-width-100p lrv-u-border-a-1 lrv-js-MobileHeightToggle-trigger lrv-u-padding-tb-050 lrv-u-justify-content-center lrv-u-text-transform-uppercase  lrv-a-icon-after a-icon-after-caret-down a-icon-height-8 a-icon-width-13';
 c_heading.c_heading_text = 'All';
 
 module.exports = {
@@ -29,5 +29,5 @@ module.exports = {
 	],
 	main_toggle_button_classes: 'lrv-a-hidden@tablet',
 	nav_content_classes:
-		'lrv-js-MobileHeightToggle-target a-children-border-color-current lrv-a-space-children-horizontal@tablet a-space-children--350 u-border-lr-1@mobile-max u-border-color-wild-strawberry lrv-u-border-b-1@mobile-max lrv-a-children-border-vertical@mobile-max lrv-u-flex-direction-column@mobile-max lrv-u-align-items-center lrv-a-unstyle-list lrv-u-flex lrv-u-justify-content-center lrv-u-flex-wrap-wrap u-justify-content-start@mobile-max u-border-lr-1@mobile-max',
+		'lrv-js-MobileHeightToggle-target a-children-border-color-current lrv-a-space-children-horizontal@tablet a-space-children--350 lrv-u-border-b-1@mobile-max lrv-a-children-border-vertical@mobile-max lrv-u-flex-direction-column@mobile-max lrv-u-align-items-center lrv-a-unstyle-list lrv-u-flex lrv-u-justify-content-center lrv-u-flex-wrap-wrap',
 };
